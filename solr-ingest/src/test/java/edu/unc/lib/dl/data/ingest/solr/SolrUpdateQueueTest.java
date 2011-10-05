3e68788aa7f0584b4abb19f96013f4cf0283afa0
bbpennel <bbpennel@email.unc.edu>
2011-10-05 11:42:13 -0400

    Queue test class

---
+++ solr-ingest/src/test/java/edu/unc/lib/dl/data/ingest/solr/SolrUpdateQueueTest.java
@@ -0,0 +1,11 @@
+/*package edu.unc.lib.dl.data.ingest.solr;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/service-context-unit.xml" })
+public class SolrUpdateQueueTest {
+
+}*/
