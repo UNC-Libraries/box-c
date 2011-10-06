e92caaac5d86e1cea45a3f207f52ad76ed273348
bbpennel <bbpennel@email.unc.edu>
2011-10-06 16:18:35 -0400

    Adding authentication

--- sword-server/src/main/java/edu/unc/lib/dl/sword/server/managers/ServiceDocumentManagerImpl.java
+++ sword-server/src/main/java/edu/unc/lib/dl/sword/server/managers/ServiceDocumentManagerImpl.java
@@ -67,7 +67,6 @@ public class ServiceDocumentManagerImpl extends AbstractFedoraManager implements
 				workspace.addCollection(collection);
 			}
 			sd.addWorkspace(workspace);
-			sd.setMaxUploadSize(999999999);
 			
 			return sd;
 		} catch (Exception e) {
