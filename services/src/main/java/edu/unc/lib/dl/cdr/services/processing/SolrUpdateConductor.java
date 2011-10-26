16e862f6f492b0836c0567da1cabe7416d25fa89
bbpennel <bbpennel@email.unc.edu>
2011-10-26 16:33:07 -0400

    Fleshed out SolrUpdateConductor to generate SolrUpdateService messages and add them to the queue.  SolrUpdateMessageFilter populated as well

--- services/src/main/java/edu/unc/lib/dl/cdr/services/processing/SolrUpdateConductor.java
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/processing/SolrUpdateConductor.java
@@ -1,15 +1,54 @@
 package edu.unc.lib.dl.cdr.services.processing;
 
 import edu.unc.lib.dl.cdr.services.model.PIDMessage;
+import edu.unc.lib.dl.cdr.services.util.JMSMessageUtil;
+import edu.unc.lib.dl.data.ingest.solr.SolrUpdateAction;
+import edu.unc.lib.dl.data.ingest.solr.SolrUpdateRequest;
 import edu.unc.lib.dl.data.ingest.solr.SolrUpdateService;
 
 public class SolrUpdateConductor extends SolrUpdateService implements MessageConductor {
-	public static final String identifier = "SOLRUPDATE";
+	public static final String identifier = "SOLR_UPDATE";
 
 	@Override
 	public void add(PIDMessage message) {
-		// TODO Auto-generated method stub
-		//this.off
+		String namespace = message.getNamespace();
+		String action = message.getAction();
+		if (namespace != null && action != null){
+			if (namespace.equals(SolrUpdateAction.namespace)){
+				SolrUpdateAction actionEnum = SolrUpdateAction.getAction(action);
+				if (action != null){
+					this.offer(message.getPIDString(), actionEnum);
+				}
+			} else {
+				if (JMSMessageUtil.FedoraActions.PURGE_OBJECT.equals(action)){
+					this.offer(message.getPIDString(), SolrUpdateAction.DELETE_SOLR_TREE);
+				} else if (JMSMessageUtil.CDRActions.MOVE.equals(action) || JMSMessageUtil.CDRActions.ADD.equals(action)
+						|| JMSMessageUtil.CDRActions.REORDER.equals(action)){
+					message.generateCDRMessageContent();
+					if (JMSMessageUtil.CDRActions.MOVE.equals(action) || JMSMessageUtil.CDRActions.ADD.equals(action)){
+						//Move and add are both recursive adds of all subjects, plus a nonrecursive update for reordered children.
+						for (String pidString: message.getCDRMessageContent().getSubjects()){
+							this.offer(new SolrUpdateRequest(pidString, SolrUpdateAction.RECURSIVE_ADD));
+						}
+					}
+					// Reorder is a non-recursive add.
+					for (String pidString: message.getCDRMessageContent().getReordered()){
+						this.offer(new SolrUpdateRequest(pidString, SolrUpdateAction.ADD));
+					}
+				} else if (JMSMessageUtil.CDRActions.REINDEX.equals(action)){
+					//Determine which kind of reindex to perform based on the mode
+					message.generateCDRMessageContent();
+					if (message.getCDRMessageContent().getMode().equals("inplace")){
+						this.offer(new SolrUpdateRequest(message.getCDRMessageContent().getParent(), SolrUpdateAction.RECURSIVE_REINDEX));
+					} else {
+						this.offer(new SolrUpdateRequest(message.getCDRMessageContent().getParent(), SolrUpdateAction.CLEAN_REINDEX));
+					}
+				} else {
+					//For all other message types, do a single record update
+					this.offer(message.getPIDString());
+				}
+			}
+		}
 	}
 
 	@Override
--- services/src/main/java/edu/unc/lib/dl/cdr/services/processing/SolrUpdateMessageFilter.java
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/processing/SolrUpdateMessageFilter.java
@@ -1,9 +1,25 @@
 package edu.unc.lib.dl.cdr.services.processing;
 
 import edu.unc.lib.dl.cdr.services.model.PIDMessage;
+import edu.unc.lib.dl.cdr.services.util.JMSMessageUtil;
+import edu.unc.lib.dl.data.ingest.solr.SolrUpdateAction;
 
 public class SolrUpdateMessageFilter extends MessageFilter {
+	
+	public SolrUpdateMessageFilter(){
+		MessageFilter.conductor = SolrUpdateConductor.identifier;
+	}
+	
 	public boolean filter(PIDMessage msg) {
+		if (msg.getNamespace() != null && msg.getNamespace().equals(SolrUpdateAction.namespace)){
+			return true;
+		}
+		String action = msg.getAction();
+		if (JMSMessageUtil.CDRActions.MOVE.equals(action) || JMSMessageUtil.CDRActions.ADD.equals(action)
+				|| JMSMessageUtil.CDRActions.REORDER.equals(action) || JMSMessageUtil.CDRActions.REINDEX.equals(action)
+				|| JMSMessageUtil.FedoraActions.PURGE_OBJECT.equals(action)) {
+			return true;
+		}
 		return false;
 	}
 }
