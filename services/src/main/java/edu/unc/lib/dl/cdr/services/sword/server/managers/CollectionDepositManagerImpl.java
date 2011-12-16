e14ad410551210455d72b1387deb6e49ea3715a9
Ben Pennell <ben.pennell@gmail.com>
2011-12-16 16:42:07 -0500

    Moving sword server into services

---
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/sword/server/managers/CollectionDepositManagerImpl.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.cdr.services.sword.server.managers;
+
+import org.swordapp.server.AuthCredentials;
+import org.swordapp.server.CollectionDepositManager;
+import org.swordapp.server.CollectionListManager;
+import org.swordapp.server.Deposit;
+import org.swordapp.server.DepositReceipt;
+import org.swordapp.server.SwordAuthException;
+import org.swordapp.server.SwordConfiguration;
+import org.swordapp.server.SwordError;
+import org.swordapp.server.SwordServerException;
+
+/**
+ * 
+ * @author bbpennel
+ *
+ */
+public class CollectionDepositManagerImpl extends AbstractFedoraManager implements CollectionDepositManager {
+
+	@Override
+	public DepositReceipt createNew(String collectionURI, Deposit deposit, AuthCredentials auth,
+			SwordConfiguration config) throws SwordError, SwordServerException, SwordAuthException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/sword/server/servlets/ContextAwareSwordServlet.java
@@ -0,0 +1,20 @@
+package edu.unc.lib.dl.cdr.services.sword.server.servlets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.swordapp.server.servlets.SwordServlet;
+
+@SuppressWarnings("serial")
+public class ContextAwareSwordServlet extends SwordServlet implements ApplicationContextAware {
+	protected static ApplicationContext context;
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(ContextAwareSwordServlet.class);
+
+	@Override
+	public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+		context = arg0;
+	}
+}
