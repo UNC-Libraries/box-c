d9894079b84a3e8df3459dac23a6dcd9aacddcf4
bbpennel <bbpennel@email.unc.edu>
2012-03-22 15:59:28 -0400

    Utility for extracting abdera atom pub entries containing datastreams into map of jdom metadata objects

---
+++ persistence/src/main/java/edu/unc/lib/dl/util/AtomPubMetadataParserUtil.java
@@ -0,0 +1,108 @@
+package edu.unc.lib.dl.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
+import org.apache.abdera.model.Element;
+import org.apache.abdera.model.Entry;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
+public class AtomPubMetadataParserUtil {
+
+	public static final String ATOM_DC_DATASTREAM = "ATOM_DC";
+	private static final QName datastreamQName = new QName("http://cdr.lib.unc.edu/", "datastream");
+	private static final QName modsQName = new QName("http://www.loc.gov/mods/v3", "mods");
+	private static final String dcNamespace = "http://purl.org/dc/terms/";
+	
+	/**
+	 * Returns a map containing the metadata content as jdom elements associated with their datastream id.  The content
+	 * is extracted from an Atom Pub abdera entry.  The entry can contain root level qualified dublin core tags or
+	 * a MODS entry, as well as any number of cdr:datastream tags containing specific metadata streams to extract.
+	 * 
+	 * If a datastream tag contains more than one root element, only the first element will be retained
+	 * @param entry abdera Atom Pub entry containing metadata for extraction.
+	 * @return
+	 * @throws IOException
+	 * @throws JDOMException
+	 */
+	public static Map<String,org.jdom.Element> extractDatastreams(Entry entry) throws IOException, JDOMException{
+		if (entry == null || entry.getElements().size() == 0){
+			return null;
+		}
+		Map<String,org.jdom.Element> datastreamMap = new HashMap<String,org.jdom.Element>();
+		//Outstream containing the compiled default dublin core tags
+		ByteArrayOutputStream dcOutStream = null;
+
+		try {
+			for (Element element: entry.getElements()){
+				if (datastreamQName.equals(element.getQName())){
+					//Create new datastream entry
+					String id = element.getAttributeValue("id");
+					if (id != null){
+						org.jdom.Element jdomElement = abderaToJDOM(element);
+						org.jdom.Element dsContentElement = null;
+						//Store the first child of the datastream tag as the content for this DS
+						if (jdomElement.getChildren().size() > 0){
+							dsContentElement = ((org.jdom.Element)jdomElement.getChildren().get(0));
+							datastreamMap.put(id, (org.jdom.Element)dsContentElement.detach());
+						}
+					}
+				} else if (modsQName.equals(element.getQName())){
+					//Create the default mods datastream
+					org.jdom.Element modsElement = abderaToJDOM(element);
+					datastreamMap.put(ContentModelHelper.Datastream.MD_DESCRIPTIVE.getName(), modsElement);
+				} else if (dcNamespace.equals(element.getQName().getNamespaceURI())){
+					//Populate dublin core properties from the default entry metadata
+					if (dcOutStream == null){
+						dcOutStream = new ByteArrayOutputStream();
+						dcOutStream.write("<oai_dc:dc xmlns:oai_dc=\"http://www.openarchives.org/OAI/2.0/oai_dc/\">".getBytes("UTF-8"));
+					}
+					element.writeTo(dcOutStream);
+				}
+			}
+			
+			//Create the atom dublin core default datastream if it's populated
+			if (dcOutStream != null){
+				dcOutStream.write("</oai_dc:dc>".getBytes("UTF-8"));
+				SAXBuilder saxBuilder = new SAXBuilder();
+				ByteArrayInputStream inStream = new ByteArrayInputStream(dcOutStream.toByteArray());
+				org.jdom.Document jdomDocument = saxBuilder.build(inStream);
+				datastreamMap.put(ATOM_DC_DATASTREAM, jdomDocument.detachRootElement());
+				inStream.close();
+			}
+		} finally {
+			if (dcOutStream != null)
+				dcOutStream.close();
+		}
+		return datastreamMap;
+	}
+
+	/**
+	 * Converts an abdera element to a jdom element by converting it back to raw xml.
+	 * @param element
+	 * @return
+	 * @throws JDOMException
+	 * @throws IOException
+	 */
+	public static org.jdom.Element abderaToJDOM(Element element) throws JDOMException, IOException{
+		SAXBuilder saxBuilder = new SAXBuilder();
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		ByteArrayInputStream inStream = null;
+		try {
+			element.writeTo(outStream);
+			inStream = new ByteArrayInputStream(outStream.toByteArray());
+			org.jdom.Document jdomDocument = saxBuilder.build(inStream);
+			return jdomDocument.detachRootElement();
+		} finally {
+			outStream.close();
+			if (inStream != null)
+				inStream.close();
+		}
+	}
+}
---
+++ persistence/src/test/java/edu/unc/lib/dl/util/AtomPubMetadataParserTest.java
@@ -0,0 +1,48 @@
+package edu.unc.lib.dl.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Map;
+
+import org.apache.abdera.Abdera;
+import org.apache.abdera.model.Document;
+import org.apache.abdera.model.Entry;
+import org.apache.abdera.parser.Parser;
+import org.jdom.output.XMLOutputter;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AtomPubMetadataParserTest extends Assert {
+
+	@Test
+	public void testDatastreamExtraction() throws Exception{
+		InputStream entryPart = new FileInputStream(new File("src/test/resources/metadataUpdateMultipleDS.xml"));
+		Abdera abdera = new Abdera();
+		Parser parser = abdera.getParser();
+		Document<Entry> entryDoc = parser.parse(entryPart);
+		Entry entry = entryDoc.getRoot();
+		Map<String,org.jdom.Element> datastreamMap = AtomPubMetadataParserUtil.extractDatastreams(entry);
+		
+		org.jdom.Element dcDS = datastreamMap.get(AtomPubMetadataParserUtil.ATOM_DC_DATASTREAM);
+		org.jdom.Element modsDS = datastreamMap.get(ContentModelHelper.Datastream.MD_DESCRIPTIVE.getName());
+		org.jdom.Element relsExtDS = datastreamMap.get(ContentModelHelper.Datastream.RELS_EXT.getName());
+		assertNotNull(modsDS);
+		assertNotNull(dcDS);
+		assertNotNull(relsExtDS);
+		
+		//Make sure everything has been unwrapped
+		assertTrue(modsDS.getName().equals("mods"));
+		assertTrue(dcDS.getName().equals("dc"));
+		assertTrue(relsExtDS.getName().equals("RDF"));
+		
+		XMLOutputter outputter = new XMLOutputter();
+		java.util.Iterator<java.util.Map.Entry<String,org.jdom.Element>> it = datastreamMap.entrySet().iterator();
+		
+		while (it.hasNext()){
+			java.util.Map.Entry<String,org.jdom.Element> element = it.next();
+			System.out.println(element.getKey() + ":\n");
+			outputter.output(element.getValue(), System.out);
+		}
+	}
+}
