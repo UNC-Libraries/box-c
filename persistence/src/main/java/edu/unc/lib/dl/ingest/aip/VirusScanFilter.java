a55acb48c06c30bcf8befeb96c9a7a4cd06d42b7
Gregory Jansen <count0@email.unc.edu>
2012-06-28 16:43:25 -0400

    added virus scan filter to aip ingest pipelines in admin and services added clam scan bean to relevant spring context files consolidated server.properties for test into one file in the etc dir added clamd properties to server.properties for test (the new one in /etc)

---
+++ persistence/src/main/java/edu/unc/lib/dl/ingest/aip/VirusScanFilter.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.ingest.aip;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jdom.Document;
+import org.jdom.Element;
+
+import com.philvarner.clamavj.ClamScan;
+import com.philvarner.clamavj.ScanResult;
+
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.util.PremisEventLogger;
+import edu.unc.lib.dl.xml.FOXMLJDOMUtil;
+
+/**
+ * This filter logs the earlier assignment of a unique identifier to each object being ingested. This identifier scheme
+ * is a UUID that is compatible with both the Fedora PID and a URN.
+ * 
+ * @author count0
+ * 
+ */
+public class VirusScanFilter implements AIPIngestFilter {
+	private static final Log log = LogFactory.getLog(VirusScanFilter.class);
+	
+	private ClamScan clamScan = null;
+
+	public ClamScan getClamScan() {
+		return clamScan;
+	}
+
+	public void setClamScan(ClamScan clamScan) {
+		this.clamScan = clamScan;
+	}
+
+	public VirusScanFilter() {
+	}
+
+	public ArchivalInformationPackage doFilter(ArchivalInformationPackage aip) throws AIPException {
+		log.debug("Starting");
+		for (PID pid : aip.getPIDs()) {
+			Document foxml = aip.getFOXMLDocument(pid);
+			for (Element cLocation : FOXMLJDOMUtil.getFileLocators(foxml)) {
+				String ref = cLocation.getAttributeValue("REF");
+				URI uri;
+				try {
+					uri = new URI(ref);
+				} catch (URISyntaxException e) {
+					throw new AIPException("Unable to construct URI for a METS file location.", e);
+				}
+				if (uri.getScheme() == null || uri.getScheme().contains("file")) {
+					File file = aip.getFileForUrl(ref);
+					try {
+						ScanResult result = this.clamScan.scan(new FileInputStream(file));
+						if(!ScanResult.Status.PASSED.equals(result.getStatus())) {
+							throw new AIPException("Virus check failed on "+ref+"\n"+result);
+						}
+					} catch (FileNotFoundException e) {
+						throw new AIPException("Cannot find local file referenced in METS manifest: "+ref, e);
+					}
+				}
+			}
+			aip.getEventLogger().logEvent(PremisEventLogger.Type.VIRUS_CHECK, "All packaged files passed pre-ingest scan for viruses", pid);
+		}
+		log.debug("Finished");
+		return aip;
+	}
+}
