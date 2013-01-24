c62c8c0ba28820ae114fe27fb09b682c4484f6ba
Gregory Jansen <count0@email.unc.edu>
2013-01-24 14:42:39 -0500

    updated fcrepo-version to 3.6.2 from 3.6.1 updated objecthandlers to better match the 3.6.2 codebase fixed query parameter for predicate (relationship API)

--- fcrepo-cdr-fesl/src/main/java/edu/unc/lib/dl/security/objecthandlers/AddRelationship.java
+++ fcrepo-cdr-fesl/src/main/java/edu/unc/lib/dl/security/objecthandlers/AddRelationship.java
@@ -16,9 +16,7 @@
 package edu.unc.lib.dl.security.objecthandlers;
 
 import java.io.IOException;
-
 import java.net.URI;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,19 +24,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.fcrepo.common.Constants;
+import org.fcrepo.server.security.xacml.pep.PEPException;
+import org.fcrepo.server.security.xacml.pep.ResourceAttributes;
+import org.fcrepo.server.security.xacml.pep.rest.filters.AbstractFilter;
+import org.fcrepo.server.security.xacml.util.LogUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sun.xacml.attr.AnyURIAttribute;
 import com.sun.xacml.attr.AttributeValue;
 import com.sun.xacml.attr.StringAttribute;
 import com.sun.xacml.ctx.RequestCtx;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.fcrepo.common.Constants;
-
-import org.fcrepo.server.security.xacml.pep.PEPException;
-import org.fcrepo.server.security.xacml.pep.rest.filters.AbstractFilter;
-import org.fcrepo.server.security.xacml.util.LogUtil;
 
 
 /**
@@ -72,37 +69,25 @@ public class AddRelationship
         if (logger.isDebugEnabled()) {
             logger.debug(this.getClass().getName() + "/handleRequest!");
         }
-
-        String path = request.getPathInfo();
-        String[] parts = path.split("/");
-
-        String pid = parts[1];
-
         
         RequestCtx req = null;
         Map<URI, AttributeValue> actions = new HashMap<URI, AttributeValue>();
-        Map<URI, AttributeValue> resAttr = new HashMap<URI, AttributeValue>();
+        Map<URI, AttributeValue> resAttr;
         try {
-            if (pid != null && !"".equals(pid)) {
-                resAttr.put(Constants.OBJECT.PID.getURI(),
-                            new StringAttribute(pid));
-            }
-            if (pid != null && !"".equals(pid)) {
-                resAttr.put(new URI(XACML_RESOURCE_ID),
-                            new AnyURIAttribute(new URI(pid)));
-            }
+            String[] parts = getPathParts(request);
+            String pid = parts[1];
+            resAttr = ResourceAttributes.getResources(parts);
+            actions.put(Constants.ACTION.ID.getURI(),
+                        Constants.ACTION.ADD_RELATIONSHIP
+                                .getStringAttribute());
+            actions.put(Constants.ACTION.API.getURI(),
+                        Constants.ACTION.APIM.getStringAttribute());
+            
             // adding predicates to the evaluation context
-            String predicate = request.getParameter("relationship");
+            String predicate = request.getParameter("predicate");
             if (predicate != null && !"".equals(predicate)) {
             	resAttr.put(new URI(RESOURCE_RELATIONSHIP_PREDICATE), new AnyURIAttribute(new URI(predicate)));
             }
-
-            actions.put(Constants.ACTION.ID.getURI(),
-                        new StringAttribute(Constants.ACTION.ADD_RELATIONSHIP
-                                .getURI().toASCIIString()));
-            actions.put(Constants.ACTION.API.getURI(),
-                        new StringAttribute(Constants.ACTION.APIM.getURI()
-                                .toASCIIString()));
 
             req =
                     getContextHandler().buildRequest(getSubjects(request),
