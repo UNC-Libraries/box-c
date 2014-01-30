098fe800e2a40ffbe24ed653368507b9b9226987
bbpennel <bbpennel@email.unc.edu>
2014-01-30 12:18:17 -0500

    Destroy api endpoint switched to using digitalObjectManager to ensure contains statements get cleaned up

--- services/src/main/java/edu/unc/lib/dl/cdr/services/rest/modify/DestroyObjectController.java
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/rest/modify/DestroyObjectController.java
@@ -6,17 +6,17 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import edu.unc.lib.dl.fedora.AuthorizationException;
+import edu.unc.lib.dl.acl.util.GroupsThreadStore;
 import edu.unc.lib.dl.fedora.FedoraException;
-import edu.unc.lib.dl.fedora.ManagementClient;
 import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.ingest.IngestException;
+import edu.unc.lib.dl.services.DigitalObjectManager;
 
 /**
  * API controller which permanently deletes/destroys objects in the repository.
@@ -29,8 +29,7 @@ public class DestroyObjectController {
 	private static final Logger log = LoggerFactory.getLogger(DestroyObjectController.class);
 
 	@Autowired(required = true)
-	@Qualifier("forwardedManagementClient")
-	private ManagementClient managementClient;
+	private DigitalObjectManager digitalObjectManager;
 	
 	@RequestMapping(value = "edit/destroy/{id}", method = RequestMethod.POST)
 	public @ResponseBody
@@ -43,15 +42,20 @@ public class DestroyObjectController {
 		
 		try {
 			log.debug("Destroying object {}", id);
-			managementClient.purgeObject(pid, "Object destroyed by CDR API", false);
+			digitalObjectManager.delete(pid, GroupsThreadStore.getUsername(), "Object destroyed by CDR API");
 			result.put("timestamp", System.currentTimeMillis());
-		} catch (AuthorizationException e) {
-			result.put("error", "Insufficient privileges to perform operation on object " + id);
 		} catch (FedoraException e) {
+			log.error("Failed to destroy object {}", pid, e);
+			result.put("error", e.toString());
+		} catch (IngestException e) {
 			log.error("Failed to destroy object {}", pid, e);
 			result.put("error", e.toString());
 		}
 		
 		return result;
+	}
+
+	public void setDigitalObjectManager(DigitalObjectManager digitalObjectManager) {
+		this.digitalObjectManager = digitalObjectManager;
 	}
 }
