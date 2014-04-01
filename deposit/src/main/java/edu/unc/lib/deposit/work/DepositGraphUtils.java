595f9859d6d2a5eb1faa6f783dc9ed70e59c13a2
Gregory Jansen <count0@email.unc.edu>
2014-04-01 17:40:22 -0400

    fixed rebase issues with graph utils

--- deposit/src/main/java/edu/unc/lib/deposit/work/DepositGraphUtils.java
+++ deposit/src/main/java/edu/unc/lib/deposit/work/DepositGraphUtils.java
@@ -1,8 +1,10 @@
 package edu.unc.lib.deposit.work;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+import com.hp.hpl.jena.rdf.model.Bag;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.NodeIterator;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -20,15 +22,15 @@ public class DepositGraphUtils {
 	public DepositGraphUtils() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public static Property dprop(Model m, DepositRelationship r) {
 		return m.getProperty(r.getURI().toString());
 	}
-	
+
 	public static Property cdrprop(Model m, CDRProperty r) {
 		return m.getProperty(r.getURI().toString());
 	}
-	
+
 	public static Property cdrprop(Model m, Relationship r) {
 		return m.getProperty(r.getURI().toString());
 	}
@@ -36,8 +38,9 @@ public class DepositGraphUtils {
 	public static Property fprop(Model m, FedoraProperty r) {
 		return m.getProperty(r.getURI().toString());
 	}
-	
-	public static Resource cmodel(Model m, edu.unc.lib.dl.util.ContentModelHelper.Model model) {
+
+	public static Resource cmodel(Model m,
+			edu.unc.lib.dl.util.ContentModelHelper.Model model) {
 		return m.getResource(model.getURI().toString());
 	}
 
@@ -45,19 +48,20 @@ public class DepositGraphUtils {
 		NodeIterator iterator = null;
 		if (c.hasProperty(RDF.type, RDF.Bag)) {
 			iterator = c.getModel().getBag(c).iterator();
-		} else if(c.hasProperty(RDF.type, RDF.Seq)){
+		} else if (c.hasProperty(RDF.type, RDF.Seq)) {
 			iterator = c.getModel().getSeq(c).iterator();
 		}
 		List<Resource> containers = new ArrayList<Resource>();
-		while(iterator.hasNext()) {
-			Resource n = (Resource)iterator.next();
+		while (iterator.hasNext()) {
+			Resource n = (Resource) iterator.next();
 			result.add(n);
-			if (n.hasProperty(RDF.type, RDF.Bag) || n.hasProperty(RDF.type, RDF.Seq)) {
+			if (n.hasProperty(RDF.type, RDF.Bag)
+					|| n.hasProperty(RDF.type, RDF.Seq)) {
 				containers.add(n);
 			}
 		}
 		iterator.close();
-		for(Resource r : containers) {
+		for (Resource r : containers) {
 			addChildren(r, result);
 		}
 	}
@@ -67,5 +71,28 @@ public class DepositGraphUtils {
 		Resource top = m.getResource(depositPID.getURI());
 		addChildren(top, result);
 		return result;
+	}
+
+	/**
+	 * 
+	 * Walk the children of the given bag in depth first order, storing children
+	 * in the given pids collection
+	 * 
+	 * @param bag bag to retrieve children from
+	 * @param pids collection to store bag children into, in depth first ordering
+	 * @param recursive if false, then only the first tier of children will be
+	 *        retrieved
+	 */
+	public static void walkChildrenDepthFirst(Bag bag, Collection<String> pids,
+			boolean recursive) {
+		NodeIterator childIt = bag.iterator();
+		while (childIt.hasNext()) {
+			Resource childResource = (Resource) childIt.next();
+			pids.add(childResource.getURI());
+			if (recursive) {
+				Bag childBag = childResource.getModel().getBag(childResource);
+				walkChildrenDepthFirst(childBag, pids, recursive);
+			}
+		}
 	}
 }
