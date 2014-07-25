8389b34c5867c639c445dcd090bb8cde77d5eae1
Gregory Jansen <count0@email.unc.edu>
2014-07-25 10:41:36 -0400

    batch ingest task now configures properly in spring errors are reported properly in depositor/admin emails confirmed in user testing on dev

--- persistence/src/main/java/edu/unc/lib/dl/services/MailNotifier.java
+++ persistence/src/main/java/edu/unc/lib/dl/services/MailNotifier.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
