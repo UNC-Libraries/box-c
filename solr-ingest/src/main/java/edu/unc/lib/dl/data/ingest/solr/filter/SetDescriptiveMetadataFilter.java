3503a83ab0179a76a0e83c7b517bb38720a2f993
Gregory Jansen <count0@email.unc.edu>
2014-09-02 10:37:28 -0400

    add recognition of "Author" marcrelator role term

--- solr-ingest/src/main/java/edu/unc/lib/dl/data/ingest/solr/filter/SetDescriptiveMetadataFilter.java
+++ solr-ingest/src/main/java/edu/unc/lib/dl/data/ingest/solr/filter/SetDescriptiveMetadataFilter.java
@@ -169,6 +169,10 @@ public class SetDescriptiveMetadataFilter extends AbstractIndexDocumentFilter {
 								isCreator = true;
 								break;
 							}
+							if ("author".equalsIgnoreCase(((Element)roleTerm).getValue())){
+								isCreator = true;
+								break;
+							}
 							if (isCreator)
 								break;
 						}
