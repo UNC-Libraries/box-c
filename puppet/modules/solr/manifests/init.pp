590b7bfcbeba1577f788ce2cf33cb4401ee8c485
Mike Daines <daines@email.unc.edu>
2014-10-02 16:44:53 -0400

    Add oai4solr configuration
    - Add oai4solr git submodule
    - Add task to Capfile to deploy the oai4solr jar
    - Add configuration for oai4solr to solr Puppet module

--- puppet/modules/solr/manifests/init.pp
+++ puppet/modules/solr/manifests/init.pp
@@ -1,5 +1,8 @@
 class solr(
   $remote_addr_valve_allow,
+  $oai_web_protocol,
+  $oai_web_host,
+  $oai_etd_set_query
 ) {
 
   package { "cdr-solr":
@@ -19,8 +22,42 @@ class solr(
     require => Package["cdr-solr"],
   }
   
+  file { "/opt/repository/solr/lib":
+    ensure => "link",
+    target => "/opt/deploy/solrlib",
+    force => true,
+  }
+  
   file { "/opt/repository/tomcat/conf/Catalina/localhost/solr.xml":
     content => template("solr/tomcat/conf/Catalina/localhost/solr.xml.erb"),
+    owner => "tomcat",
+    group => "tomcat",
+    require => Package["cdr-solr"],
+  }
+  
+  file { "/opt/repository/solr/access/conf/solrconfig.xml":
+    content => template("solr/home/access/conf/solrconfig.xml.erb"),
+    owner => "tomcat",
+    group => "tomcat",
+    require => Package["cdr-solr"],
+  }
+
+  file { "/opt/repository/solr/access/oai/Identify.xml":
+    content => template("solr/home/access/oai/Identify.xml.erb"),
+    owner => "tomcat",
+    group => "tomcat",
+    require => Package["cdr-solr"],
+  }
+
+  file { "/opt/repository/solr/access/oai/oai_dc.xsl":
+    content => template("solr/home/access/oai/oai_dc.xsl.erb"),
+    owner => "tomcat",
+    group => "tomcat",
+    require => Package["cdr-solr"],
+  }
+
+  file { "/opt/repository/solr/access/oai/solr.xsl":
+    content => template("solr/home/access/oai/solr.xsl.erb"),
     owner => "tomcat",
     group => "tomcat",
     require => Package["cdr-solr"],
