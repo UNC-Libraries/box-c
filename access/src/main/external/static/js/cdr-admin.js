738c1063b9024a6b390f78743bdafceaf34fc57d
bbpennel <bbpennel@email.unc.edu>
2015-07-17 13:50:19 -0400

    Displaying detailed warning message if attempting to assign multiple default web objects to the same parent.

--- access/src/main/external/static/js/cdr-admin.js
+++ access/src/main/external/static/js/cdr-admin.js
@@ -1273,6 +1273,9 @@ define('ConfirmationDialog', [ 'jquery', 'jquery-ui', 'PID', 'RemoteStateChangeM
 			this.confirmDialog = $("<div class='confirm_dialogue'></div>");
 			
 			this.setPromptText(this.options.promptText);
+			if (this.options.warningText) {
+				this.confirmDialog.append("<div class='warning'>" + this.options.warningText + "</div>");
+			}
 			
 			if (this.options.confirmMatchText) {
 				this.confirmMatchInput = $("<input type='text' />");
@@ -1336,6 +1339,7 @@ define('ConfirmationDialog', [ 'jquery', 'jquery-ui', 'PID', 'RemoteStateChangeM
 					id : 'cancel',
 					class : 'confirm_dialog_confirm',
 					text : self.options.confirmText,
+					disabled : self.options.disableConfirm,
 					click : function() {
 						if (self.options.confirmFunction) {
 							if (!self.inputMatches())
@@ -5214,17 +5218,58 @@ define('ResubmitPackageForm', [ 'jquery', 'jquery-ui', 'underscore', 'RemoteStat
 	SetAsDefaultWebObjectBatchAction.prototype.getTargets = function(targets) {
 		if (this.context.targets) {
 			return this.context.targets;
-		} 
+		}
 		return AbstractBatchAction.prototype.getTargets.call(this);
 	};
 	
 	SetAsDefaultWebObjectBatchAction.prototype.execute = function() {
+		var self = this;
 		this.targets = this.getTargets();
 		
 		if (!('confirm' in this.context) || this.context.confirm) {
+			var parentSet = {};
+			var multipleParents = false;
+			
 			this.context.confirm = {
 				confirmAnchor : this.context.anchor
 			};
+			
+			// Validating whether multiple items being selected have the same parent
+			for (var i in this.targets) {
+				var parentId = this.targets[i].metadata.rollup;
+				if (parentId in parentSet) {
+					multipleParents = true;
+					parentSet[parentId].push(this.targets[i]);
+				} else {
+					parentSet[parentId] = [this.targets[i]];
+				}
+			}
+			
+			// Adding warning if there are repeat parents and disabling the action
+			if (multipleParents) {
+				var warningText =  "<h2>Error</h2><p>Selected objects share common parents.  An aggregate object may only be assigned one primary access object at a time.</p><p>The following is a list of selected objects grouped by their shared parent:</p> <ul>";
+				$.each(parentSet, function(key, resultSet) {
+					if (resultSet.length > 1) {
+						var parentTitle = self.resultList.getResultObject(key).metadata.title;
+						warningText += "<li>" + parentTitle + "<ul>";
+						for (var i in resultSet) {
+							warningText += "<li>" + resultSet[i].metadata.title + "</li>";
+						}
+						warningText += "</ul></li>";
+					}
+				});
+				warningText += "</ul>";
+
+				this.context.confirm.warningText = warningText;
+				this.context.confirm.disableConfirm = true;
+			}
+			
+			this.context.confirm.dialogOptions = {
+				title : "Set Primary Access Object",
+				dialogClass : "confirm_dialog",
+				width: "400px"
+			};
+			
 			if (this.targets.length == 1)
 				this.context.confirm.promptText = "Set the selected object as the primary access object for its parent?";
 			else
