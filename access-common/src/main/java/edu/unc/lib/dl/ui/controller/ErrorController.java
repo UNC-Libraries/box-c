92afab2e722952baf8c2af6c73ab75cb6696d10c
bbpennel <bbpennel@email.unc.edu>
2015-08-19 12:41:06 -0400

    Eating BrokenPipeExceptions from CDR views and errors related to jsp streams already having closed.  Reducing logging when the attempt to get FOXML for the full record page times out.  Deleted extra ErrorController.

--- access-common/src/main/java/edu/unc/lib/dl/ui/controller/ErrorController.java
+++ access-common/src/main/java/edu/unc/lib/dl/ui/controller/ErrorController.java
@@ -15,6 +15,8 @@
  */
 package edu.unc.lib.dl.ui.controller;
 
+import java.net.SocketException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -29,30 +31,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author bbpennel
  */
 @Controller
-public class ErrorController {	
+public class ErrorController {
 	private static final Logger LOG = LoggerFactory.getLogger(ErrorController.class);
-	
+
 	@RequestMapping("/403")
-    public String handle403(Model model, HttpServletRequest request, HttpServletResponse response) {
+	public String handle403(Model model, HttpServletRequest request, HttpServletResponse response) {
 		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 		model.addAttribute("pageSubtitle", "Error");
-        return "error/403";
-    }
-	
+		return "error/403";
+	}
+
 	@RequestMapping("/404")
-    public String handle404(Model model, HttpServletResponse response) {
+	public String handle404(Model model, HttpServletResponse response) {
 		LOG.debug("Page not found, forwarding to 404 page");
 		response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 		model.addAttribute("pageSubtitle", "Page not found");
-        return "error/404";
-    }
+		return "error/404";
+	}
 
 	@RequestMapping("/exception")
-    public String handleException(Model model, HttpServletRequest request, HttpServletResponse response) {
+	public String handleException(Model model, HttpServletRequest request, HttpServletResponse response) {
 		response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
-		LOG.error("An uncaught exception occurred at " + request.getAttribute("javax.servlet.forward.request_uri"), throwable);
+		
+		if (throwable.getCause() instanceof SocketException || throwable instanceof IllegalStateException) {
+			LOG.debug("Connection aborted for {}", request.getAttribute("javax.servlet.forward.request_uri"), throwable);
+		} else {
+			LOG.error("An uncaught exception occurred at " + request.getAttribute("javax.servlet.forward.request_uri"),
+					throwable);
+		}
+		
 		model.addAttribute("pageSubtitle", "Error");
-        return "error/exception";
-    }
+		return "error/exception";
+	}
 }
