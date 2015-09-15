8b7b94c62480beeccfa63fdf52bdaf8a3581ae3d
bbpennel <bbpennel@email.unc.edu>
2015-09-15 14:34:02 -0400

    Preventing full text extract enhancement from rerunning on objects that extracted no text previously, but do not have a full text datastream

--- services-worker/src/main/java/edu/unc/lib/dl/cdr/services/text/FullTextEnhancementService.java
+++ services-worker/src/main/java/edu/unc/lib/dl/cdr/services/text/FullTextEnhancementService.java
@@ -15,6 +15,7 @@
  */
 package edu.unc.lib.dl.cdr.services.text;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 import org.jdom2.Element;
@@ -23,6 +24,9 @@ import edu.unc.lib.dl.cdr.services.AbstractDatastreamEnhancementService;
 import edu.unc.lib.dl.cdr.services.Enhancement;
 import edu.unc.lib.dl.cdr.services.exception.EnhancementException;
 import edu.unc.lib.dl.cdr.services.model.EnhancementMessage;
+import edu.unc.lib.dl.fedora.FedoraException;
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.util.ContentModelHelper.CDRProperty;
 import edu.unc.lib.dl.util.ContentModelHelper.Datastream;
 
 /**
@@ -38,6 +42,37 @@ public class FullTextEnhancementService extends AbstractDatastreamEnhancementSer
 		mimetypePattern = Pattern.compile("^(text/|application/pdf|application/msword|application/vnd\\.|application/rtf|application/powerpoint|application/postscript).*");
 		derivativeDatastream = Datastream.MD_FULL_TEXT.getName();
 		
+	}
+	
+	@Override
+	protected boolean isDatastreamApplicable(PID pid) throws FedoraException {
+		edu.unc.lib.dl.fedora.types.Datastream dataDoc
+				= managementClient.getDatastream(pid, Datastream.DATA_FILE.getName());
+		
+		// Don't process if there is no original data
+		if (dataDoc == null) {
+			return false;
+		}
+		
+		// Filter out objects with non-applicable mimetypes
+		if (mimetypePattern != null && !mimetypePattern.matcher(dataDoc.getMIMEType()).matches()){
+			return false;
+		}
+		
+		edu.unc.lib.dl.fedora.types.Datastream derivDoc
+				= managementClient.getDatastream(pid, derivativeDatastream);
+		
+		// No derivative present
+		if (derivDoc == null) {
+			// Check for placeholder indicating that no full text datastream is needed since the extract was empty
+			List<String> result =
+					tripleStoreQueryService.fetchBySubjectAndPredicate(pid, CDRProperty.fullText.toString());
+			return result == null || result.size() == 0 || Boolean.parseBoolean(result.get(0));
+		}
+		
+		// Derivative is older than the original data, need to reperform the enhancement
+		// Dates are in iso8601/UTC format, so lexographic string comparison is sufficient
+		return dataDoc.getCreateDate().compareTo(derivDoc.getCreateDate()) > 0;
 	}
 	
 	@Override
