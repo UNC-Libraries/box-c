1ae49d72f594019d5bba5224ac9867bc7892b96b
Mike Daines <daines@email.unc.edu>
2015-10-28 09:25:15 -0400

    Add checks for invalid or unsupported BagIt bags

--- deposit/src/main/java/edu/unc/lib/deposit/normalize/BagIt2N3BagJob.java
+++ deposit/src/main/java/edu/unc/lib/deposit/normalize/BagIt2N3BagJob.java
@@ -17,14 +17,17 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.NodeIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
 
+import edu.unc.lib.dl.util.ContentModelHelper;
 import edu.unc.lib.deposit.work.AbstractDepositJob;
 import edu.unc.lib.dl.fedora.PID;
 import edu.unc.lib.dl.util.ContentModelHelper.DepositRelationship;
 import edu.unc.lib.dl.util.ContentModelHelper.FedoraProperty;
 import edu.unc.lib.dl.util.RedisWorkerConstants.DepositField;
 import gov.loc.repository.bagit.Bag;
+import gov.loc.repository.bagit.Bag.Format;
 import gov.loc.repository.bagit.BagFactory;
 import gov.loc.repository.bagit.BagFile;
+import gov.loc.repository.bagit.BagHelper;
 import gov.loc.repository.bagit.Manifest;
 
 public class BagIt2N3BagJob extends AbstractDepositJob {
@@ -47,16 +50,30 @@ public class BagIt2N3BagJob extends AbstractDepositJob {
 		Map<String, String> status = getDepositStatus();
 		String sourcePath = status.get(DepositField.sourcePath.name());
 		
+		if (BagHelper.getVersion(new File(sourcePath)) == null) {
+			failJob("Can't find BagIt bag", "A BagIt bag could not be found at the source path.");
+		}
+		
 		BagFactory bagFactory = new BagFactory();
 		
 		File sourceFile = new File(sourcePath);
 		Bag bag = bagFactory.createBag(sourceFile);
 		
+		if (bag.getFormat() != Format.FILESYSTEM) {
+			failJob("Unsupported BagIt bag format", "Only filesystem bags are supported.");
+		}
+		
 		Collection<BagFile> payload = bag.getPayload();
 		
 		for (BagFile bagFile : payload) {
 			
-			List<String> path = Arrays.asList(bagFile.getFilepath().split("/"));
+			String bagFilepath = bagFile.getFilepath();
+			
+			if (bagFilepath.indexOf("/") == -1) {
+				failJob("Unsupported path separators in BagIt bag payload", "The payload must use \"/\" as the path separator.");
+			}
+			
+			List<String> path = Arrays.asList(bagFilepath.split("/"));
 			List<String> folderPath = path.subList(1, path.size() - 1);
 			com.hp.hpl.jena.rdf.model.Bag folder = getFolder(top, folderPath);
 			
@@ -101,7 +118,7 @@ public class BagIt2N3BagJob extends AbstractDepositJob {
 				folder = model.createBag(pid.getURI());
 				
 				model.add(folder, dprop(model, DepositRelationship.label), name);
-				model.add(folder, fprop(model, FedoraProperty.hasModel), edu.unc.lib.dl.util.ContentModelHelper.Model.CONTAINER.toString());
+				model.add(folder, fprop(model, FedoraProperty.hasModel), ContentModelHelper.Model.CONTAINER.toString());
 				
 				top.add(folder);
 			}
@@ -118,7 +135,7 @@ public class BagIt2N3BagJob extends AbstractDepositJob {
 		PID pid = new PID("uuid:" + uuid.toString());
 		Resource file = model.createResource(pid.getURI());
 
-		model.add(file, fprop(model, FedoraProperty.hasModel), edu.unc.lib.dl.util.ContentModelHelper.Model.SIMPLE.toString());
+		model.add(file, fprop(model, FedoraProperty.hasModel), ContentModelHelper.Model.SIMPLE.toString());
 		
 		List<String> path = Arrays.asList(bagFile.getFilepath().split("/"));
 		model.add(file, dprop(model, DepositRelationship.label), path.get(path.size() - 1));
@@ -127,6 +144,8 @@ public class BagIt2N3BagJob extends AbstractDepositJob {
 		
 		if (checksums.containsKey(Manifest.Algorithm.MD5)) {
 			model.add(file, dprop(model, DepositRelationship.md5sum), checksums.get(Manifest.Algorithm.MD5));
+		} else {
+			failJob("MD5 checksum not provided", "An MD5 checksum was not provided for the file: " + bagFile.getFilepath());
 		}
 		
 		return file;
