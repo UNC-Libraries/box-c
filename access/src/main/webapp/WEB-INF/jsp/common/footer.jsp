UNC-Libraries/box-c

commit 27584c1642b21486c53e2335b2d14170aaa41bc1

Remove extra spaces from footer.

access/src/main/webapp/WEB-INF/jsp/common/footer.jsp
@@ -27,12 +27,12 @@
   <div class="threecol fluid-cap-left">
 		<div class="contentarea">
 			<p>
-				<a href="${pageContext.request.contextPath}/">Home</a>&nbsp;|&nbsp;
-				<a href="collections">Browse Collections</a>&nbsp;|&nbsp;
-				<a href="http://blogs.lib.unc.edu/cdr/">About</a>&nbsp;|&nbsp;
-				<a href="http://blogs.lib.unc.edu/cdr/index.php/contact-us/">Contact Us</a>&nbsp;|&nbsp;
-				<a href="http://www.lib.unc.edu">Library Home</a>&nbsp;|&nbsp;
-				<a href="http://library.unc.edu/privacy-policy/">Privacy Policy</a>
+				<a href="${pageContext.request.contextPath}/">Home</a> |
+				<a href="collections">Browse Collections</a |
+				<a href="http://blogs.lib.unc.edu/cdr/">About</a> |
+				<a href="http://blogs.lib.unc.edu/cdr/index.php/contact-us/">Contact Us</a> |
+				<a href="http://www.lib.unc.edu">Library Home</a> |
+				<a href="http://www.lib.unc.edu/aoffice/policies/privacy_policy.html">Privacy Policy</a>
 			</p>
 		</div>
             </div>

commit 9fce989f452a70efee402d0d6cdaf60f9c5212bd

Fix broken HTML introduced by 27584c1642b21486c53e2335b2d14170aaa41bc1

access/src/main/webapp/WEB-INF/jsp/common/footer.jsp
@@ -28,7 +28,7 @@
 		<div class="contentarea">
 			<p>
 				<a href="${pageContext.request.contextPath}/">Home</a> |
-				<a href="collections">Browse Collections</a |
+				<a href="collections">Browse Collections</a> |
 				<a href="http://blogs.lib.unc.edu/cdr/">About</a> |
 				<a href="http://blogs.lib.unc.edu/cdr/index.php/contact-us/">Contact Us</a> |
 				<a href="http://www.lib.unc.edu">Library Home</a> |