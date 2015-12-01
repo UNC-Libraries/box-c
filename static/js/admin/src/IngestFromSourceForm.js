cc37866a3246853a6571ff3e2cd7bba5ff9ccaf7
bbpennel <bbpennel@email.unc.edu>
2015-12-01 14:33:53 -0500

    Add from file server UI cleanup, added checkboxes, select all, retaining selection after going back, result scrolling independently

--- static/js/admin/src/IngestFromSourceForm.js
+++ static/js/admin/src/IngestFromSourceForm.js
@@ -21,15 +21,16 @@ define('IngestFromSourceForm', [ 'jquery', 'AbstractFileUploadForm', 'ModalLoadi
 			width: 560,
 			height: 500,
 			modal: true,
-			title: "Ingest from file server"
+			title: "Add from file server",
+			resizable: false
 		});
 		
 		this.pid = pid;
 		
-		this.loadCandidateList(pid);
+		this.loadCandidateList();
 	};
 	
-	IngestFromSourceForm.prototype.loadCandidateList = function() {
+	IngestFromSourceForm.prototype.loadCandidateList = function(selectedCandidates) {
 		var self = this;
 		
 		var loadingOverlay = new ModalLoadingOverlay(this.dialog, {
@@ -45,12 +46,25 @@ define('IngestFromSourceForm', [ 'jquery', 'AbstractFileUploadForm', 'ModalLoadi
 		}).done(function(data){
 			loadingOverlay.remove();
 			
-			self.renderCandidateList(data.sources, data.candidates);
+			self.renderCandidateList(data.sources, data.candidates, selectedCandidates);
 		});
 	};
 	
-	IngestFromSourceForm.prototype.renderCandidateList = function(sources, candidates) {
+	IngestFromSourceForm.prototype.renderCandidateList = function(sources, candidates, selected) {
 		var self = this;
+		
+		if (candidates.length == 0) {
+			this.dialog.html("<p>No candidates were found for ingest to this container</p>");
+			return;
+		}
+		
+		// Sort the candidates by source and path
+		var candidates = candidates.sort(function (a, b) {
+			if ((a.sourceId + a.patternMatched) < (b.sourceId + b.patternMatched)) {
+				return -1;
+			}
+			return 1;
+		});
 		
 		// Generate index of strings to search per candidate and reformat candidate fields for display
 		var candidateIndex = [];
@@ -74,23 +88,60 @@ define('IngestFromSourceForm', [ 'jquery', 'AbstractFileUploadForm', 'ModalLoadi
 		var candidatesForm = candidatesTemplate({sources : sourceMap, candidates : candidates});
 		
 		this.dialog.html(candidatesForm);
+		var form = this.dialog.find("form");
+		
+		// If a list of selected items was provided from the user going back on the metadata page, then select them.
+		if (selected) {
+			for (var i = 0; i < candidates.length; i++) {
+				var candidate = candidates[i];
+				
+				for (var j = 0; j < selected.length; j++) {
+					if (selected[j].sourceId == candidate.sourceId && selected[j].patternMatched == candidate.patternMatched) {
+						var entry = $(".file_browse_entry", form).eq(i);
+						entry.addClass("selected").find("input").prop("checked", true);
+						break;
+					}
+				}
+			}
+		}
 		
 		this.updateCandidateSubmitButton();
 		
 		// Bind selection events for result entries
-		this.dialog.on("click", "#ingest_source_candidates .file_browse_entry", function(e){
+		form.on("click", ".file_browse_entry", function(e){
 			var $this = $(this);
-			if (e.metaKey || e.ctrlKey) {
-				$this.toggleClass("selected");
+			e.stopPropagation();
+			if ($this.hasClass("selected")) {
+				$this.removeClass("selected");
+				$("input", $this).prop("checked", false);
 			} else {
-				$this.addClass("selected").siblings().removeClass("selected");
+				$this.addClass("selected");
+				$("input", $this).prop("checked", true);
 			}
 			
 			self.updateCandidateSubmitButton();
 		});
 		
+		// Bind select all within a source button functionality
+		form.on("click", ".select_all_col input", function(e) {
+			var $this = $(this);
+			var sourceId = $this.data("sourceId");
+			var select = $this.data("action") == "select";
+			$this.data("action", select? "deselect" : "select");
+			$this.attr("value", select? "Deselect All" : "Select All");
+			$this.parents(".file_browse_heading").first().nextUntil(".file_browse_heading", ".file_browse_entry").each(function() {
+				if (select) {
+					$(this).addClass("selected");
+				} else {
+					$(this).removeClass("selected");
+				}
+				$("input", $(this)).prop("checked", select);
+			});
+			self.updateCandidateSubmitButton();
+		});
+		
 		// Filter box functionality
-		this.dialog.find("#candidate_filter").keyup(function() {
+		form.find("#candidate_filter").keyup(function() {
 			var $input = $(this);
 			var value = $input.val();
 			
@@ -118,7 +169,7 @@ define('IngestFromSourceForm', [ 'jquery', 'AbstractFileUploadForm', 'ModalLoadi
 			});
 		});
 		
-		this.dialog.find("form").submit(function(e) {
+		form.submit(function(e) {
 			e.preventDefault();
 			
 			// Determine which files were selected and match the current filter, to avoid hidden files getting chosen
@@ -150,7 +201,7 @@ define('IngestFromSourceForm', [ 'jquery', 'AbstractFileUploadForm', 'ModalLoadi
 		
 		this.dialog.find("#ingest_source_choose").click(function(e) {
 			e.preventDefault();
-			self.loadCandidateList();
+			self.loadCandidateList(selectedCandidates);
 		});
 		
 		this.$form = this.dialog.find("form");
@@ -158,10 +209,18 @@ define('IngestFromSourceForm', [ 'jquery', 'AbstractFileUploadForm', 'ModalLoadi
 			e.preventDefault();
 			
 			var fileInfo = [];
+			var missingFields = false;
 			// Generate a map of properties to pass to controller to get this deposit started
 			self.dialog.find(".file_browse_entry").each(function(index) {
 				var $this = $(this);
 				var candidate = selectedCandidates[index];
+				
+				var label = $this.find("input[name='file_label']").val();
+				if (!label.trim()) {
+					missingFields = true;
+					return false;
+				}
+				
 				var info = {
 					sourceId : candidate.sourceId,
 					packagePath : candidate.patternMatched,
@@ -172,6 +231,12 @@ define('IngestFromSourceForm', [ 'jquery', 'AbstractFileUploadForm', 'ModalLoadi
 				};
 				fileInfo.push(info);
 			});
+			
+			if (missingFields) {
+				self.$form.find(".error_stack").text("A label must be provided for each deposit");
+				self.$form.find(".errors").removeClass("hidden");
+				return false;
+			}
 			
 			var loadingOverlay = new ModalLoadingOverlay(self.dialog, {
 				autoOpen : false,
