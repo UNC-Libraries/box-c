5f6389803d450ce35859aabfab0cd5a03ae4c6eb
lfarrell <lfarrell@email.unc.edu>
2016-04-15 16:26:54 -0400

    Add performance visualizations

---
+++ static/js/admin/performance_visualizations/load.js
@@ -0,0 +1,10 @@
+d3_queue.queue().defer(d3.csv,'/static/js/admin/performance_visualizations/data/ingest-times-daily.csv')
+	.defer(d3.csv,'/static/js/admin/performance_visualizations/data/ingest-times-daily-deposit.csv')
+	.await(function(error, operation_totals, deposit_totals) {
+	var scatter_tip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    var drawGraphs = new CdrGraphs(operation_totals, deposit_totals, scatter_tip);
+    drawGraphs.draw();
+}); 
