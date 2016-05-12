ba881e666bd5b00bbca540366db4dc8f1a09f105
bbpennel <bbpennel@email.unc.edu>
2016-05-12 17:16:06 -0400

    Added initial skeleton of fcrepo4 client api classes

---
+++ fcrepo-clients/src/main/java/edu/unc/lib/dl/fcrepo4/ContentObject.java
@@ -0,0 +1,9 @@
+package edu.unc.lib.dl.fcrepo4;
+
+public class ContentObject {
+	private String fedoraUri;
+	private PCDMObject pcdm;
+	
+	
+	
+}
---
+++ fcrepo-clients/src/main/java/edu/unc/lib/dl/fcrepo4/DepositRecord.java
@@ -0,0 +1,27 @@
+package edu.unc.lib.dl.fcrepo4;
+
+import java.io.InputStream;
+import java.util.Collection;
+
+public class DepositRecord {
+	private void createDepositRecord() {
+		
+	}
+	
+	private void addManifest() {
+		
+	}
+	
+	private InputStream getManifest() {
+		return null;
+	}
+	
+	private Collection<String> getManifestPaths() {
+		return null;
+	}
+	
+	private Collection<?> listDepositedObjects() {
+		return null;
+	}
+
+}
