2036fa21a4477bde6047a04d7fd4f257761477eb
bbpennel <bbpennel@email.unc.edu>
2016-08-03 16:13:17 -0400

    Removed aggregate objects from structure browse

--- solr-search/src/main/java/edu/unc/lib/dl/search/solr/service/SearchStateFactory.java
+++ solr-search/src/main/java/edu/unc/lib/dl/search/solr/service/SearchStateFactory.java
@@ -161,7 +161,6 @@ public class SearchStateFactory {
 		List<String> containerTypes = new ArrayList<String>();
 		containerTypes.add(searchSettings.resourceTypeCollection);
 		containerTypes.add(searchSettings.resourceTypeFolder);
-		containerTypes.add(searchSettings.resourceTypeAggregate);
 		searchState.setResourceTypes(containerTypes);
 		
 		searchState.setSortType("title");
