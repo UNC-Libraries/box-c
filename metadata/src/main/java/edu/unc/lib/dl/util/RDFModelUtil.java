9f06f6fcf3e903aa5ee8288ae0993c5a1bcc4404
bbpennel <bbpennel@email.unc.edu>
2016-09-15 17:19:14 -0400

    Implemented utility method for creating a sparql update insert query out of a model.  Added unit test.

--- metadata/src/main/java/edu/unc/lib/dl/util/RDFModelUtil.java
+++ metadata/src/main/java/edu/unc/lib/dl/util/RDFModelUtil.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.unc.lib.dl.util;
 
 import java.io.ByteArrayInputStream;
@@ -10,17 +25,29 @@ import java.io.InputStream;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFFormat;
 
+import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
+/**
+ * Utility containing common methods for manipulating and transforming RDF
+ * models
+ * 
+ * @author bbpennel
+ *
+ */
 public class RDFModelUtil {
-	
+
 	public static void serializeModel(Model model, File file) throws IOException {
 		try (FileOutputStream fos = new FileOutputStream(file)) {
 			RDFDataMgr.write(fos, model, RDFFormat.TURTLE_PRETTY);
 		}
 	}
-	
+
 	/**
 	 * Serializes and streams the provided model as serialized turtle
 	 * 
@@ -46,7 +73,7 @@ public class RDFModelUtil {
 			return new ByteArrayInputStream(bos.toByteArray());
 		}
 	}
-	
+
 	/**
 	 * Returns a model built from the given turtle input stream
 	 * 
@@ -57,5 +84,46 @@ public class RDFModelUtil {
 		Model model = ModelFactory.createDefaultModel();
 		model.read(inStream, null, "TURTLE");
 		return model;
+	}
+
+	/**
+	 * Convert the given model into a SPARQL update query which inserts all
+	 * of the properties in the model.
+	 * 
+	 * @param model
+	 * @return sparql update query which adds all properties from the given model
+	 */
+	public static String createSparqlInsert(Model model) {
+		StringBuilder query = new StringBuilder();
+		query.append("INSERT {\n");
+
+		ResIterator it = model.listSubjects();
+		while (it.hasNext()) {
+			Resource resc = it.nextResource();
+			String currentUri = resc.getURI();
+
+			StmtIterator pIt = resc.listProperties();
+			while (pIt.hasNext()) {
+				Statement property = pIt.nextStatement();
+				query.append(" <").append(currentUri).append('>').append(" <").append(property.getPredicate().getURI())
+						.append("> ");
+				if (property.getObject().isResource()) {
+					query.append('<').append(property.getObject().asResource().getURI()).append('>');
+				} else {
+					Node node = property.getObject().asNode();
+					query.append('"').append(node.getLiteralLexicalForm()).append('"');
+					String typeUri = node.getLiteralDatatypeURI();
+					if (typeUri != null) {
+						query.append("^^<").append(node.getLiteralDatatypeURI()).append('>');
+					}
+				}
+				query.append(" .\n");
+			}
+
+		}
+
+		query.append("}\nWHERE {}");
+
+		return query.toString();
 	}
 }
