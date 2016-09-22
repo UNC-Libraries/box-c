7305b9caa9732be627789390f224f769078866b1
bbpennel <bbpennel@email.unc.edu>
2016-09-22 13:52:35 -0400

    Added missing premis properties

--- metadata/src/main/java/edu/unc/lib/dl/rdf/Premis.java
+++ metadata/src/main/java/edu/unc/lib/dl/rdf/Premis.java
@@ -81,6 +81,10 @@ public class Premis {
     
     public static final Property hasEventType = createProperty("http://www.loc.gov/premis/rdf/v1#hasEventType");
     
+    public static final Property hasMessageDigest = createProperty("http://www.loc.gov/premis/rdf/v1#hasMessageDigest");
+    
+    public static final Property hasSize = createProperty("http://www.loc.gov/premis/rdf/v1#hasSize");
+    
     public static final Resource File = createResource( "http://www.loc.gov/premis/rdf/v1#File" );
     
     public static final Resource Fixity = createResource( "http://www.loc.gov/premis/rdf/v1#Fixity" );
