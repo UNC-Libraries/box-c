10ade40b99273082f2d71ff1bf8b80dc5edb5280
Ben Pennell <ben.pennell@gmail.com>
2016-10-11 10:24:34 -0400

    Deleted unused and outdated classes.  Moved premis logger tests into the correct package (#517)

--- fcrepo-clients/src/test/java/edu/unc/lib/dl/event/FilePremisLoggerTest.java
+++ fcrepo-clients/src/test/java/edu/unc/lib/dl/event/FilePremisLoggerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.unc.lib.dl.util;
+package edu.unc.lib.dl.event;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
