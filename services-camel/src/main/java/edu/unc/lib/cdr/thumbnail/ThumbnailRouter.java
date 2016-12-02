cfb951609e4eb3a8610de11b8a72067d011ac138
lfarrell <lfarrell@email.unc.edu>
2016-12-02 13:11:15 -0500

    Create routing that runs in fcrepo4-vagrant and relies on cdr metadata module

--- services-camel/src/main/java/edu/unc/lib/cdr/thumbnail/ThumbnailRouter.java
+++ services-camel/src/main/java/edu/unc/lib/cdr/thumbnail/ThumbnailRouter.java
@@ -5,19 +5,26 @@ import java.util.StringJoiner;
 
 import org.apache.camel.builder.PredicateBuilder;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.Namespaces;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.Message;
 import org.apache.camel.Predicate;
 import org.apache.camel.Processor;
+import org.fcrepo.camel.RdfNamespaces;
 
 import static com.hp.hpl.jena.rdf.model.ModelFactory.createDefaultModel;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.RDF;
 import com.google.common.base.Splitter;
 
-/*import static edu.unc.lib.dl.rdf.Ebucore.hasMimeType;
-import static edu.unc.lib.dl.rdf.Premis.hasMessageDigest; */
+import edu.unc.lib.dl.rdf.Fcrepo4Repository;
+import static edu.unc.lib.dl.rdf.Ebucore.hasMimeType;
+import static edu.unc.lib.dl.rdf.Premis.hasMessageDigest;
+
 
 public class ThumbnailRouter extends RouteBuilder {
 	private static final String EVENT_TYPE = "http://fedora.info/definitions/v4/event";
@@ -27,57 +34,65 @@ public class ThumbnailRouter extends RouteBuilder {
 	 */
 	public void configure() throws Exception {
 		final String baseBinaryPath = "/var/lib/tomcat7/fcrepo4-data/fcrepo.binary.directory/";
+		final Namespaces ns = new Namespaces("rdf", RdfNamespaces.RDF);
 		Predicate isCreated = header("org.fcrepo.jms.eventType").isEqualTo(EVENT_TYPE + "#ResourceCreation");
 	//	Predicate isUpdated = header("org.fcrepo.jms.eventType").isEqualTo(EVENT_TYPE + "#ResourceModification");
-		Predicate isBinary = xpath("/rdf:RDF/rdf:Description/rdf:type[@rdf:resource='http://fedora.info/definitions/v4/repository#Binary']");
+	//	Predicate isBinary = xpath("/rdf:RDF/rdf:Description/rdf:type[@rdf:resource='http://fedora.info/definitions/v4/repository#Binary']", ns);
 		
 		from("activemq:topic:fedora")
 			.routeId("CdrServiceEnhancements")
 			.log(LoggingLevel.INFO, "Thumbnail Creation")
 			.filter(simple("${headers[org.fcrepo.jms.eventType]} not contains 'NODE_REMOVED'"))
-		.to("fcrepo:localhost:8080/fcrepo/rest?accept=text/turtle")
+			
+		.to("fcrepo:localhost:8080/fcrepo/rest?preferInclude=ServerManged&accept=application/rdf+xml")
+		.filter(header("org.fcrepo.jms.eventType").isEqualTo(EVENT_TYPE + "#ResourceCreation"))
+		.filter()
+			.xpath("/rdf:RDF/rdf:Description/rdf:type[@rdf:resource='http://fedora.info/definitions/v4/repository#Binary']", ns)
+			.to("fcrepo:localhost:8080/fcrepo/rest?preferInclude=ServerManged&accept=text/turtle")
 			.process(new Processor() {
 				@Override
 				public void process(final Exchange exchange) throws Exception {
 					final Message in = exchange.getIn();
 					final Model model = createDefaultModel();
-					
+
 					Model values = model.read(in.getBody(InputStream.class),  null, "Turtle");
-					
-					String fcrepoMimeType = values.getProperty("hasMimeType").toString();
-					String fcrepoChecksum = values.getProperty("hasMessageDigest").toString();
-					String fcrepoChecksumStart = fcrepoChecksum.substring(0, 7);
-					
+					Resource resource = values.listResourcesWithProperty(RDF.type, Fcrepo4Repository.Binary).next();
+
+					String mimeType = resource.getProperty(hasMimeType).getObject().toString();
+					String fcrepoChecksum = resource.getProperty(hasMessageDigest).getObject().toString();
+					String[] fcrepoChecksumSplit = fcrepoChecksum.split(":");
+					String fcrepoChecksumStart = fcrepoChecksumSplit[2].substring(0, 6);
+						
 					String binaryPath = "";
 					for (String substring : Splitter.fixedLength(2).split(fcrepoChecksumStart)) {
 						binaryPath += substring + "/";
 					}
-					
+						
 					String fullPath = new StringJoiner("")
 						.add(baseBinaryPath)
 						.add(binaryPath)
-						.add(fcrepoChecksum)
+						.add(fcrepoChecksumSplit[2])
 						.toString();
-					
-					in.setHeader("MimeType", fcrepoMimeType);
-					in.setHeader("CheckSum", fcrepoChecksum);
+						
+					in.setHeader("Checksum", fcrepoChecksumSplit[2]);
+					in.setHeader("MimeType", mimeType);
 					in.setHeader("BinaryPath", fullPath);
 				}
 			})
-			.log("HEADING: ${headers}")
-			.choice()
-				.when(PredicateBuilder.and(isCreated, isBinary, header("mimeType").startsWith("image")))
-					.to("fcrepo:localhost:8080/fcrepo/rest?metadata=false")
-					.setHeader("CamelFileName", simple("${headers[org.fcrepo.jms.identifier]}"))
+			.log("Minnie $header[MimeType]")
+		//	.choice()
+		//		.when(PredicateBuilder.and(isCreated, isBinary, header("mimeType").startsWith("image")))
+		//			.to("fcrepo:localhost:8080/fcrepo/rest?metadata=false")
+		//			.setHeader("CamelFileName", simple("${headers[org.fcrepo.jms.identifier]}"))
 					.multicast()
 					.to("direct:small.thumbnail", "direct:large.thumbnail");
 
 		from("direct:small.thumbnail")
 		.log(LoggingLevel.INFO, "Creating/Updating Small Thumbnail")
-		.recipientList(simple("exec:/bin/sh?args=/usr/local/bin/convertScaleStage.sh ${headers[binaryPath]} PNG 64 64&amp;workingDir=/tmp&amp;outFile=/tmp/${headers[CheckSum]}"));
+		.recipientList(simple("exec:/bin/sh?args=/usr/local/bin/convertScaleStage.sh ${headers[binaryPath]} PNG 64 64&workingDir=/tmp&outFile=/tmp/${headers[CheckSum]}"));
 		
 		from("direct:large.thumbnail")
 		.log(LoggingLevel.INFO, "Creating/Updating Large Thumbnail")
-		.recipientList(simple("exec:/bin/sh?args=/usr/local/bin/convertScaleStage.sh ${headers[binaryPath]} PNG 128 128&amp;workingDir=/tmp&amp;outFile=/tmp/${headers[CheckSum]}"));
+		.recipientList(simple("exec:/bin/sh?args=/usr/local/bin/convertScaleStage.sh ${headers[binaryPath]} PNG 128 128&workingDir=/tmp&outFile=/tmp/${headers[CheckSum]}"));
 	}
 }
