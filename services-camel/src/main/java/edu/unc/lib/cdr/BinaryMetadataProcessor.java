2e6ddbb2ee6b4db56a21343ef06f6d64c3659ad2
bbpennel <bbpennel@email.unc.edu>
2016-12-22 16:15:15 -0500

    Removed dependency on google guava in BinaryMetadataProcessor, added unit test

--- services-camel/src/main/java/edu/unc/lib/cdr/BinaryMetadataProcessor.java
+++ services-camel/src/main/java/edu/unc/lib/cdr/BinaryMetadataProcessor.java
@@ -26,7 +26,6 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 
-import com.google.common.base.Splitter;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
@@ -35,13 +34,18 @@ import com.hp.hpl.jena.vocabulary.RDF;
 import edu.unc.lib.dl.rdf.Fcrepo4Repository;
 
 /**
+ * Stores information related to identifying binary objects from the repository
  * 
  * @author lfarrell
  *
  */
 public class BinaryMetadataProcessor implements Processor {
+
+	private final int BINARY_PATH_DEPTH = 3;
+	private final int BINARY_PATH_LENGTH = 2;
+
 	private final String baseBinaryPath;
-	
+
 	protected BinaryMetadataProcessor(String baseBinaryPath) {
 		this.baseBinaryPath = baseBinaryPath;
 	}
@@ -51,33 +55,45 @@ public class BinaryMetadataProcessor implements Processor {
 		final Message in = exchange.getIn();
 		final Model model = createDefaultModel();
 
-		Model values = model.read(in.getBody(InputStream.class),  null, "Turtle");
+		Model values = model.read(in.getBody(InputStream.class), null, "Turtle");
 		ResIterator resources = values.listResourcesWithProperty(RDF.type, Fcrepo4Repository.Binary);
-		
+
 		if (resources.hasNext()) {
 			Resource resource = resources.next();
 			String mimeType = resource.getProperty(hasMimeType).getObject().toString();
 			String fcrepoChecksum = resource.getProperty(hasMessageDigest).getObject().toString();
 			String[] fcrepoChecksumSplit = fcrepoChecksum.split(":");
-			String fcrepoChecksumStart = fcrepoChecksumSplit[2].substring(0, 6);
-				
-			String binaryPath = "";
-			for (String substring : Splitter.fixedLength(2).split(fcrepoChecksumStart)) {
-				binaryPath += substring + "/";
-			}
-				
+
+			String binaryPath = idToPath(fcrepoChecksumSplit[2], BINARY_PATH_DEPTH, BINARY_PATH_LENGTH);
+
 			String fullPath = new StringJoiner("")
 				.add(baseBinaryPath)
 				.add(binaryPath)
 				.add(fcrepoChecksumSplit[2])
 				.toString();
 
-		//	String fcrepoBaseUrl = in.getHeader("org.fcrepo.jms.baseURL").toString().split(",")[0].trim();
-
-		//	in.setHeader("FcrepoBaseUrl", fcrepoBaseUrl);
 			in.setHeader("Checksum", fcrepoChecksumSplit[2]);
 			in.setHeader("MimeType", mimeType);
 			in.setHeader("BinaryPath", fullPath);
 		}
+	}
+
+	/**
+	 * Prepend id with defined levels of hashed containers based on the values.
+	 * For example, 9bd8b60e-93a2-4b66-8f0a-b62338483b39 would become
+	 *    9b/d8/b6/9bd8b60e-93a2-4b66-8f0a-b62338483b39
+	 * 
+	 * @param id
+	 * @return
+	 */
+	private String idToPath(String id, int pathDepth, int length) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < pathDepth; i++) {
+			sb.append(id.substring(i * length, i * length + length))
+					.append('/');
+		}
+
+		return sb.toString();
 	}
 }
