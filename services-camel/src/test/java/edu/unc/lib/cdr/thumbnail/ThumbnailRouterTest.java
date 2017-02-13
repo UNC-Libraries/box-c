22be69e29424704ed0a7865f0c1930e0f8154e28
lfarrell <lfarrell@email.unc.edu>
2017-02-13 16:26:55 -0500

    Busted camel tests

--- services-camel/src/test/java/edu/unc/lib/cdr/thumbnail/ThumbnailRouterTest.java
+++ services-camel/src/test/java/edu/unc/lib/cdr/thumbnail/ThumbnailRouterTest.java
@@ -33,30 +33,50 @@ import java.util.UUID;
 import org.apache.camel.BeanInject;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.model.ModelCamelContext;
-import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
-import org.apache.camel.test.spring.UseAdviceWith;
+import org.apache.camel.component.properties.PropertiesComponent;
+import org.apache.camel.spring.javaconfig.SingleRouteCamelConfiguration;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.camel.test.spring.CamelSpringDelegatingTestContextLoader;
+import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.apache.camel.test.spring.CamelTestContextBootstrapper;
+import org.apache.camel.test.spring.MockEndpoints;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
 
 import edu.unc.lib.dl.fcrepo4.PIDs;
 import edu.unc.lib.dl.fcrepo4.Repository;
 import edu.unc.lib.dl.fedora.PID;
 
-@UseAdviceWith
-public class ThumbnailRouterTest extends CamelBlueprintTestSupport {
-
+@RunWith(CamelSpringJUnit4ClassRunner.class)
+@BootstrapWith(CamelTestContextBootstrapper.class)
+@ContextConfiguration
+public class ThumbnailRouterTest   {
 	private static final String EVENT_NS = "http://fedora.info/definitions/v4/event#";
 	private static final long timestamp = 1428360320168L;
 	private static final String userID = "bypassAdmin";
 	private static final String userAgent = "curl/7.37.1";
 	private static final String fileID = "/file1";
+	
+	@Autowired
+    protected CamelContext context;
 	
 	@PropertyInject(value = "fcrepo.baseUri")
 	private static String baseUri;
@@ -64,24 +84,34 @@ public class ThumbnailRouterTest extends CamelBlueprintTestSupport {
 	@EndpointInject(uri = "mock:fcrepo")
 	protected MockEndpoint resultEndpoint;
 	
+	@Produce(uri = "direct:fcrepo")
+    protected ProducerTemplate template;
+	
 	@BeanInject(value = "repository")
 	private Repository repo;
-
-	@Override
-	protected String getBlueprintDescriptor() {
-		return "/OSGI-INF/blueprint/blueprint-test.xml";
+	
+	public void setUp() throws Exception {
+		super.setUp();
+		
+		baseUri = context.resolvePropertyPlaceholders("${fcrepo.baseUri}");
 	}
-
+	
 	@Override
-	protected CamelContext createCamelContext() throws Exception {
-		final CamelContext ctx = getOsgiService(CamelContext.class, "(camel.context.name=CdrServiceEnhancements)",
-				10000);
-		ctx.addComponent("activemq", ctx.getComponent("seda"));
-		context = (ModelCamelContext) ctx;
+	protected AbstractApplicationContext createApplicationContext() {
+		//return new ClassPathXmlApplicationContext(new String[] {});
+		return null;
+	}
+	
+	
+	/*protected CamelContext createCamelContext() throws Exception {
+		CamelContext context = super.createCamelContext();
+		context.addComponent("activemq", context.getComponent("seda"));
+		PropertiesComponent pc = context.getComponent("properties", PropertiesComponent.class);
+		pc.setLocation("classpath:config.properties");
 
 		return context;
-	}
-
+	} */
+	
 	@Before
 	public void init() {
 		PIDs.setRepository(repo);
@@ -91,21 +121,19 @@ public class ThumbnailRouterTest extends CamelBlueprintTestSupport {
 	@Test
 	public void testRoute() throws Exception {
 		final String eventTypes = EVENT_NS + "ResourceCreation";
-		
-		//MockEndpoint mock = getMockEndpoint("direct:fcrepo");
-		//resultEndpoint.expectedMessageCount(1);
-		getMockEndpoint("mock:fcrepo:{{fcrepo.baseUri}}").expectedMessageCount(1);
 
-		context.getRouteDefinition("SCdrThumbnailEnhancement").adviceWith(context, new AdviceWithRouteBuilder() {
+		//resultEndpoint.expectedMessageCount(1);
+
+		context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
 			@Override
 			public void configure() throws Exception {
 				mockEndpointsAndSkip("fcrepo*");
 				weaveById("simpleBinaryMetadataProcessor");
 			}
 		});
+		
 		context.start();
-
-		// Construct a model for the test object
+		
 		PID pid = PIDs.get(UUID.randomUUID().toString());
 		Model model = ModelFactory.createDefaultModel();
 		Resource resc = model.createResource(pid.getRepositoryPath());
@@ -113,16 +141,15 @@ public class ThumbnailRouterTest extends CamelBlueprintTestSupport {
 		resc.addProperty(hasMessageDigest, "123456789");
 		
 		// Serialize the object as a string so that the processor can receive it
-		String body;
+		String body = null;
 		try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
 			//RDFDataMgr.write(bos, model, RDFFormat.TURTLE_PRETTY);
 			body = new String(bos.toByteArray(), "UTF-8");
 		}
 		
-	
-		template.sendBodyAndHeaders("activemq:topic:fedora2",  body, createEvent(fileID, eventTypes));
-		
-		assertMockEndpointsSatisfied();
+		template.sendBodyAndHeaders("activemq:topic:fedora2", body, createEvent(fileID, eventTypes));
+
+		resultEndpoint.assertIsSatisfied();
 	}
 
 	private static Map<String, Object> createEvent(final String identifier, final String eventTypes) {
