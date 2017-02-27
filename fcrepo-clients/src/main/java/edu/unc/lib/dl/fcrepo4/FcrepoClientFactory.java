79d943653fd59f0b533a5281fd3b70a0123b86c6
bbpennel <bbpennel@email.unc.edu>
2017-02-27 13:39:08 -0500

    Added warnings and documentation to indicate that the FcrepoClientFactory should not be overwritten after construction

--- fcrepo-clients/src/main/java/edu/unc/lib/dl/fcrepo4/FcrepoClientFactory.java
+++ fcrepo-clients/src/main/java/edu/unc/lib/dl/fcrepo4/FcrepoClientFactory.java
@@ -16,6 +16,8 @@
 package edu.unc.lib.dl.fcrepo4;
 
 import org.fcrepo.client.FcrepoClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 
@@ -23,22 +25,47 @@ import org.fcrepo.client.FcrepoClient;
  *
  */
 public class FcrepoClientFactory {
-	
+	private static final Logger log = LoggerFactory.getLogger(FcrepoClientFactory.class);
+
 	private String baseUri;
-	
+
 	private static FcrepoClientFactory instance;
-	
+
 	private FcrepoClientFactory(String base) {
 		baseUri = base;
 	}
-	
+
+	/**
+	 * Returns a FcrepoClientFactory with the given baseUri if the factory has
+	 * not been previously constructed, otherwise the existing factory will be
+	 * returned.
+	 * 
+	 * @param baseUri
+	 *            base uri for the repository. Required.
+	 * @return a new FcrepoClientFactory with the given baseUri or the
+	 *         previously constructed factory.
+	 */
 	public static FcrepoClientFactory factory(String baseUri) {
+		if (baseUri == null) {
+			throw new IllegalArgumentException("A base URI is required to construct a factory");
+		}
+
 		if (instance == null) {
 			instance = new FcrepoClientFactory(baseUri);
+		} else {
+			log.warn("Requested to construct a factory when a previous instance has "
+					+ "already been initialized (current base uri: {0}, requested {1}), ignoring.",
+					instance.baseUri, baseUri);
 		}
 		return instance;
 	}
 
+	/**
+	 * Returns the default factory. The factory must have been previously
+	 * constructed via the factory(baseUri) method.
+	 * 
+	 * @return the existing default FcrepoClientFactory
+	 */
 	public static FcrepoClientFactory defaultFactory() {
 		return instance;
 	}
