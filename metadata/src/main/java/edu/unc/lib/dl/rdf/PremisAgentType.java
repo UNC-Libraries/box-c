ce8d488d7dcb70d1c3841ebce74e29104e0186bf
harringj <harring@email.unc.edu>
2017-04-19 16:56:55 -0400

    creates class for premise agent types

---
+++ metadata/src/main/java/edu/unc/lib/dl/rdf/PremisAgentType.java
@@ -0,0 +1,34 @@
+package edu.unc.lib.dl.rdf; 
+
+import static org.apache.jena.rdf.model.ResourceFactory.createResource;
+
+import org.apache.jena.rdf.model.Resource;
+ 
+/**
+ * Vocabulary definitions from rdf-schemas/premis.rdf 
+ * @author harring
+ */
+public class PremisAgentType {
+    
+    /** The namespace of the vocabulary as a string */
+    public static final String NS = "http://id.loc.gov/vocabulary/preservation/agentType";
+    
+    /** The namespace of the vocabulary as a string
+     *  @see #NS */
+    public static String getURI() {return NS;}
+    
+    /** The namespace of the vocabulary as a resource */
+    public static final Resource NAMESPACE = createResource( NS );
+    
+    /**
+     * A high-level characterization of the type of agent. See http://id.loc.gov/vocabulary/preservation/agentType/collection_PREMIS
+     */
+    
+    public static final Resource HARDWARE = createResource("http://id.loc.gov/vocabulary/preservation/agentType/har");
+    
+    public static final Resource ORGANIZATION = createResource("http://id.loc.gov/vocabulary/preservation/agentType/org");
+    
+    public static final Resource PERSON = createResource("http://id.loc.gov/vocabulary/preservation/agentType/per");
+    
+    public static final Resource SOFTWARE = createResource("http://id.loc.gov/vocabulary/preservation/agentType/sof");
+}
