21cb0a336c984d265d0c7f93d6c97b9092e1ece6
harringj <harring@email.unc.edu>
2017-05-03 15:40:45 -0400

    replaces fed3 properties with fed4 properties

--- solr-ingest/src/main/java/edu/unc/lib/dl/data/ingest/solr/filter/SetRecordDatesFilter.java
+++ solr-ingest/src/main/java/edu/unc/lib/dl/data/ingest/solr/filter/SetRecordDatesFilter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2008 The University of North Carolina at Chapel Hill
+ * Copyright 2017 The University of North Carolina at Chapel Hill
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import java.text.ParseException;
 
 import edu.unc.lib.dl.data.ingest.solr.exception.IndexingException;
 import edu.unc.lib.dl.data.ingest.solr.indexing.DocumentIndexingPackage;
-import edu.unc.lib.dl.util.ContentModelHelper.FedoraProperty;
+import edu.unc.lib.dl.rdf.Ebucore;
 
 /**
  * Indexing filter which extracts Fedora generated dates about the creation and modification state of the object
@@ -34,13 +34,13 @@ public class SetRecordDatesFilter extends AbstractIndexDocumentFilter {
 	@Override
 	public void filter(DocumentIndexingPackage dip) throws IndexingException {
 		try {
-			dip.getDocument().setDateAdded(dip.getFirstTriple(FedoraProperty.createdDate.toString()));
+			dip.getDocument().setDateAdded(dip.getFirstTriple(Ebucore.dateCreated.toString()));
 		} catch (ParseException e) {
 			throw new IndexingException("Failed to parse record dates from " + dip.getPid().getPid(), e);
 		}
 		
 		try {
-			dip.getDocument().setDateUpdated(dip.getFirstTriple(FedoraProperty.lastModifiedDate.toString()));
+			dip.getDocument().setDateUpdated(dip.getFirstTriple(Ebucore.dateModified.toString()));
 		} catch (ParseException e) {
 			throw new IndexingException("Failed to parse record dates from " + dip.getPid().getPid(), e);
 		}
