8dd4c6955da0b99d1839824e7eecb7a49034e189
lfarrell <lfarrell@email.unc.edu>
2017-07-11 11:12:18 -0400

    Allow for selecting a form via the keyboard

--- static/js/public/studentPapers.js
+++ static/js/public/studentPapers.js
@@ -11,9 +11,21 @@ require.config({
 });
 
 define('studentPapers', ['module', 'jquery'], function(module, $) {
-  $('#mp-dept').change(function () {
-    if ($(this).val() != '') {
-      location.href = $(this).val();
-    }
-  })
+	var selector = $("#mp-dept");
+
+	selector.on('click', function() {
+		$(this).on('change', function() {
+			if ($(this).val() !== '') {
+				location.href = $(this).val();
+			}
+		});
+	});
+
+	selector.on('keypress', function(e) {
+		var code = e.keyCode || e.which;
+
+		if ($(this).val() !== '' && code === 13) {
+			location.href = $(this).val();
+		}
+	});
 });
