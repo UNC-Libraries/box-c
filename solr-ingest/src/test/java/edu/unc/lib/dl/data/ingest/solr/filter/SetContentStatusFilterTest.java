d2083f41892d0ad2644da1dac581843b14238a60
harringj <harring@email.unc.edu>
2017-07-14 09:25:45 -0400

    completes unit test

--- solr-ingest/src/test/java/edu/unc/lib/dl/data/ingest/solr/filter/SetContentStatusFilterTest.java
+++ solr-ingest/src/test/java/edu/unc/lib/dl/data/ingest/solr/filter/SetContentStatusFilterTest.java
@@ -15,6 +15,7 @@
  */
 package edu.unc.lib.dl.data.ingest.solr.filter;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
@@ -32,28 +33,26 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 
 import edu.unc.lib.dl.data.ingest.solr.indexing.DocumentIndexingPackage;
-import edu.unc.lib.dl.fcrepo4.BinaryObject;
-import edu.unc.lib.dl.fcrepo4.ContentObject;
 import edu.unc.lib.dl.fcrepo4.FileObject;
 import edu.unc.lib.dl.fcrepo4.FolderObject;
 import edu.unc.lib.dl.fcrepo4.WorkObject;
 import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.rdf.Cdr;
 import edu.unc.lib.dl.search.solr.model.IndexDocumentBean;
 import edu.unc.lib.dl.search.solr.util.FacetConstants;
 
+/**
+ * @author harring
+ */
 public class SetContentStatusFilterTest {
     @Mock
     private DocumentIndexingPackage dip;
     @Mock
     private PID pid;
     @Mock
-    private ContentObject contentObj;
-    @Mock
     private FileObject fileObj;
     @Mock
     private WorkObject workObj;
-    @Mock
-    private BinaryObject binObj;
     @Mock
     private FolderObject folderObj;
     @Mock
@@ -71,9 +70,8 @@ public class SetContentStatusFilterTest {
 
         when(dip.getDocument()).thenReturn(idb);
         when(dip.getPid()).thenReturn(pid);
-        when(contentObj.getResource()).thenReturn(resc);
-
-        when(resc.hasProperty(any(Property.class))).thenReturn(true);
+        // by default an object in this test suite has only FacetConstants.CONTENT_NOT_DESCRIBED set
+        when(resc.hasProperty(any(Property.class))).thenReturn(false);
 
         filter = new SetContentStatusFilter();
     }
@@ -82,26 +80,83 @@ public class SetContentStatusFilterTest {
     public void testDescribedWork() throws Exception {
         when(dip.getContentObject()).thenReturn(workObj);
         when(workObj.getResource()).thenReturn(resc);
+        when(resc.hasProperty(Cdr.hasMods)).thenReturn(true);
 
         filter.filter(dip);
 
         verify(idb).setContentStatus(listCaptor.capture());
         assertTrue(listCaptor.getValue().contains(FacetConstants.CONTENT_DESCRIBED));
-
+        assertFalse(listCaptor.getValue().contains(FacetConstants.CONTENT_NOT_DESCRIBED));
     }
 
     @Test
     public void testNotDescribedWork() throws Exception {
         when(dip.getContentObject()).thenReturn(workObj);
         when(workObj.getResource()).thenReturn(resc);
-        when(resc.hasProperty(any(Property.class))).thenReturn(false);
 
         filter.filter(dip);
 
         verify(idb).setContentStatus(listCaptor.capture());
         assertTrue(listCaptor.getValue().contains(FacetConstants.CONTENT_NOT_DESCRIBED));
-
+        assertFalse(listCaptor.getValue().contains(FacetConstants.CONTENT_DESCRIBED));
     }
 
+    @Test
+    public void testWorkNoPrimaryObject() throws Exception {
+        when(dip.getContentObject()).thenReturn(workObj);
+        when(workObj.getResource()).thenReturn(resc);
+
+        filter.filter(dip);
+
+        verify(idb).setContentStatus(listCaptor.capture());
+        assertTrue(listCaptor.getValue().contains(FacetConstants.NO_PRIMARY_OBJECT));
+    }
+
+    @Test
+    public void testFolderNoPrimaryObject() throws Exception {
+        when(dip.getContentObject()).thenReturn(folderObj);
+        when(folderObj.getResource()).thenReturn(resc);
+
+        filter.filter(dip);
+
+        verify(idb).setContentStatus(listCaptor.capture());
+        assertFalse(listCaptor.getValue().contains(FacetConstants.NO_PRIMARY_OBJECT));
+    }
+
+    @Test
+    public void testWorkWithPrimaryObject() throws Exception {
+        when(dip.getContentObject()).thenReturn(workObj);
+        when(workObj.getResource()).thenReturn(resc);
+        when(workObj.getPrimaryObject()).thenReturn(fileObj);
+
+        filter.filter(dip);
+
+        verify(idb).setContentStatus(listCaptor.capture());
+        assertFalse(listCaptor.getValue().contains(FacetConstants.NO_PRIMARY_OBJECT));
+    }
+
+    @Test
+    public void testWorkWithInvalidTerm() throws Exception {
+        when(dip.getContentObject()).thenReturn(workObj);
+        when(workObj.getResource()).thenReturn(resc);
+        when(resc.hasProperty(Cdr.invalidTerm)).thenReturn(true);
+
+        filter.filter(dip);
+
+        verify(idb).setContentStatus(listCaptor.capture());
+        assertTrue(listCaptor.getValue().contains(FacetConstants.INVALID_VOCAB_TERM));
+    }
+
+    @Test
+    public void testUnpublishedFileObject() throws Exception {
+        when(dip.getContentObject()).thenReturn(fileObj);
+        when(fileObj.getResource()).thenReturn(resc);
+        when(resc.hasProperty(Cdr.unpublished)).thenReturn(true);
+
+        filter.filter(dip);
+
+        verify(idb).setContentStatus(listCaptor.capture());
+        assertTrue(listCaptor.getValue().contains(FacetConstants.UNPUBLISHED));
+    }
 
 }
