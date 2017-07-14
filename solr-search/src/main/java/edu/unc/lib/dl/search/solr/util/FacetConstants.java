6afe676b6d7ece38c3003d954517bdbcffa9231c
harringj <harring@email.unc.edu>
2017-07-14 15:14:20 -0400

    creates SetAceessStatusFilter and adds some new facet constants

--- solr-search/src/main/java/edu/unc/lib/dl/search/solr/util/FacetConstants.java
+++ solr-search/src/main/java/edu/unc/lib/dl/search/solr/util/FacetConstants.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2008 The University of North Carolina at Chapel Hill
+ * Copyright 2017 The University of North Carolina at Chapel Hill
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,18 +17,20 @@ package edu.unc.lib.dl.search.solr.util;
 
 /**
  *
- * @author bbpennel
+ * @author bbpennel, harring
  *
  */
 public abstract class FacetConstants {
 
-    public static final String CONTENT_NOT_DESCRIBED = "Not Described";
     public static final String CONTENT_DESCRIBED = "Described";
-
-    public static final String NO_PRIMARY_OBJECT = "No Primary Object";
-    public static final String IS_PRIMARY_OBJECT = "Is Primary Object";
-
+    public static final String CONTENT_NOT_DESCRIBED = "Not Described";
+    public static final String DELETED = "Deleted";
+    public static final String EMBARGOED = "Embargoed";
     public static final String INVALID_VOCAB_TERM = "Invalid Term";
-
+    public static final String IS_PRIMARY_OBJECT = "Is Primary Object";
+    public static final String NO_PRIMARY_OBJECT = "No Primary Object";
+    public static final String PARENT_STAFF_ONLY = "Parent Staff Only";
+    public static final String PUBLIC_ACCESS = "Public Access";
+    public static final String STAFF_ONLY = "Staff Only";
     public static final String UNPUBLISHED = "Unpublished";
 }
