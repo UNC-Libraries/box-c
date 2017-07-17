87080d43da52bbd0c83ed23d9267fbe6920d9242
Dean Farrell <farrelldlfarrell@gmail.com>
2017-07-17 09:41:23 -0400

    Allow for selecting a masters paper form via the keyboard (#608)
    * Allow for selecting a form via the keyboard

--- static/js/public/studentPapers.js
+++ static/js/public/studentPapers.js
@@ -11,9 +11,33 @@ require.config({
 });
 
 define('studentPapers', ['module', 'jquery'], function(module, $) {
-  $('#mp-dept').change(function () {
-    if ($(this).val() != '') {
-      location.href = $(this).val();
-    }
-  })
+	var selector = $("#mp-dept");
+	selector .val('');
+
+	selector.on('click', function() {
+		redirectLink(this, null); // Required for Safari to redirect the page
+
+		$(this).on('change', function() {
+			redirectLink(this, null); // Required for Chrome and FireFox to redirect the page
+		});
+	});
+
+	selector.on('keypress', function(e) {
+		var key_code = e.keyCode || e.which;
+		redirectLink(this, key_code);
+	});
+
+	function redirectLink(context, key_code) {
+		var url = $(context).val();
+
+		if (key_code !== null) {
+			if (url !== '' && key_code === 13) {
+				location.href = url;
+			}
+		} else {
+			if (url !== '') {
+				location.href = url;
+			}
+		}
+	}
 });
