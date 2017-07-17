46dffb4aec892f8499b34f3261637a4546282fd3
harringj <harring@email.unc.edu>
2017-07-17 13:17:35 -0400

    completes access status filter and amends facet constants

--- solr-ingest/src/main/java/edu/unc/lib/dl/data/ingest/solr/filter/SetAccessStatusFilter.java
+++ solr-ingest/src/main/java/edu/unc/lib/dl/data/ingest/solr/filter/SetAccessStatusFilter.java
@@ -15,8 +15,19 @@
  */
 package edu.unc.lib.dl.data.ingest.solr.filter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.unc.lib.dl.acl.fcrepo4.InheritedAclFactory;
+import edu.unc.lib.dl.acl.service.PatronAccess;
 import edu.unc.lib.dl.data.ingest.solr.exception.IndexingException;
 import edu.unc.lib.dl.data.ingest.solr.indexing.DocumentIndexingPackage;
+import edu.unc.lib.dl.fcrepo4.ContentObject;
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.search.solr.util.FacetConstants;
 /**
  * Sets access-related status tags
  *
@@ -24,11 +35,50 @@ import edu.unc.lib.dl.data.ingest.solr.indexing.DocumentIndexingPackage;
  *
  */
 public class SetAccessStatusFilter implements IndexDocumentFilter {
+    private static final Logger log = LoggerFactory.getLogger(SetAccessStatusFilter.class);
+    private InheritedAclFactory aclFactory;
 
-	@Override
-	public void filter(DocumentIndexingPackage dip) throws IndexingException {
-		// TODO Auto-generated method stub
+    @Override
+    public void filter(DocumentIndexingPackage dip) throws IndexingException {
+        log.debug("Performing set access status filter on {}", dip.getPid());
+        dip.getDocument().setStatus(determineAccessStatus(dip));
+    }
 
-	}
+    /**
+     * Sets acl factory
+     *
+     * @param aclFactory an inherited acl factory
+     */
+    public void setAclFactory(InheritedAclFactory aclFactory) {
+        this.aclFactory = aclFactory;
+    }
+
+    private List<String> determineAccessStatus(DocumentIndexingPackage dip)
+            throws IndexingException {
+
+        ContentObject obj = dip.getContentObject();
+        PID pid = obj.getPid();
+        List<String> status = new ArrayList<>();
+
+        if (aclFactory.isMarkedForDeletion(pid)) {
+            status.add(FacetConstants.MARKED_FOR_DELETION);
+        }
+
+        if (aclFactory.getEmbargoUntil(pid) != null) {
+            status.add(FacetConstants.EMBARGOED);
+        }
+
+        if (aclFactory.getPatronAccess(pid).equals(PatronAccess.everyone)) {
+            status.add(FacetConstants.PUBLIC_ACCESS);
+        } else if (aclFactory.getPatronAccess(pid).equals(PatronAccess.authenticated)) {
+            status.add(FacetConstants.STAFF_ONLY_ACCESS);
+        }
+
+        if (aclFactory.getPatronAccess(obj.getParent().getPid()).equals(PatronAccess.authenticated)) {
+            status.add(FacetConstants.PARENT_HAS_STAFF_ONLY_ACCESS);
+        }
+
+        return status;
+    }
 
 }
--- solr-search/src/main/java/edu/unc/lib/dl/search/solr/util/FacetConstants.java
+++ solr-search/src/main/java/edu/unc/lib/dl/search/solr/util/FacetConstants.java
@@ -24,13 +24,13 @@ public abstract class FacetConstants {
 
     public static final String CONTENT_DESCRIBED = "Described";
     public static final String CONTENT_NOT_DESCRIBED = "Not Described";
-    public static final String DELETED = "Deleted";
     public static final String EMBARGOED = "Embargoed";
     public static final String INVALID_VOCAB_TERM = "Invalid Term";
     public static final String IS_PRIMARY_OBJECT = "Is Primary Object";
+    public static final String MARKED_FOR_DELETION = "Marked For Deletion";
     public static final String NO_PRIMARY_OBJECT = "No Primary Object";
-    public static final String PARENT_STAFF_ONLY = "Parent Staff Only";
+    public static final String PARENT_HAS_STAFF_ONLY_ACCESS = "Parent Has Staff-Only Access";
     public static final String PUBLIC_ACCESS = "Public Access";
-    public static final String STAFF_ONLY = "Staff Only";
+    public static final String STAFF_ONLY_ACCESS = "Staff-Only Access";
     public static final String UNPUBLISHED = "Unpublished";
 }
