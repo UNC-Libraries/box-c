6e1556a8bcafab9ba94801b8e1d2f553153a020b
harringj <harring@email.unc.edu>
2017-08-03 15:06:37 -0400

    adds logging to document indexing pipeline

--- solr-ingest/src/main/java/edu/unc/lib/dl/data/ingest/solr/indexing/DocumentIndexingPipeline.java
+++ solr-ingest/src/main/java/edu/unc/lib/dl/data/ingest/solr/indexing/DocumentIndexingPipeline.java
@@ -18,6 +18,9 @@ package edu.unc.lib.dl.data.ingest.solr.indexing;
 import java.util.Collection;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.unc.lib.dl.data.ingest.solr.exception.IndexingException;
 import edu.unc.lib.dl.data.ingest.solr.filter.IndexDocumentFilter;
 
@@ -27,12 +30,14 @@ import edu.unc.lib.dl.data.ingest.solr.filter.IndexDocumentFilter;
  *
  */
 public class DocumentIndexingPipeline implements DocumentFilteringPipeline {
+    private static final Logger log = LoggerFactory.getLogger(DocumentIndexingPipeline.class);
     protected Collection<IndexDocumentFilter> filters;
 
     @Override
     public void process(DocumentIndexingPackage dip) throws IndexingException {
 
         for (IndexDocumentFilter filter : filters) {
+            log.info("filter {} executed on pid {}", filter.getClass().getName(), dip.getPid());
             filter.filter(dip);
         }
     }
