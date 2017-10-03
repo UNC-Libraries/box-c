3b03ccc052348f2d6d4de73c6104a0a6efbd61bc
bbpennel <bbpennel@email.unc.edu>
2017-10-03 15:48:25 -0400

    Cleanup of solr update processor and null handling

--- services-camel/src/main/java/edu/unc/lib/cdr/SolrUpdateProcessor.java
+++ services-camel/src/main/java/edu/unc/lib/cdr/SolrUpdateProcessor.java
@@ -17,7 +17,6 @@ package edu.unc.lib.cdr;
 
 import static edu.unc.lib.dl.xml.JDOMNamespaceUtil.ATOM_NS;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +29,7 @@ import org.jdom2.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.unc.lib.cdr.util.MessageUtil;
 import edu.unc.lib.dl.data.ingest.solr.ChildSetRequest;
 import edu.unc.lib.dl.data.ingest.solr.SolrUpdateRequest;
 import edu.unc.lib.dl.data.ingest.solr.action.IndexingAction;
@@ -43,26 +43,29 @@ import edu.unc.lib.dl.util.IndexingActionType;
  */
 public class SolrUpdateProcessor implements Processor {
     final Logger log = LoggerFactory.getLogger(SolrUpdateProcessor.class);
+
     private Map<IndexingActionType, IndexingAction> solrIndexingActionMap;
-    private SolrUpdateRequest updateRequest;
-    private List<String> children;
 
     @Override
     public void process(Exchange exchange) throws Exception {
+        log.debug("Processing solr update");
         final Message in = exchange.getIn();
-        Document msgBody = (Document) in.getBody();
+
+        Document msgBody = MessageUtil.getDocumentBody(in);
         Element body = msgBody.getRootElement();
 
         String pid = body.getChild("pid", ATOM_NS).getTextTrim();
         String action = body.getChild("solrActionType", ATOM_NS).getTextTrim();
 
-        String getChildren = body.getChild("children", ATOM_NS).getTextTrim();
+        String getChildren = body.getChildTextTrim("children", ATOM_NS);
 
+        List<String> children = null;
         if (getChildren != null) {
-            children = new ArrayList<>(Arrays.asList(getChildren.split(",")));
+            children = Arrays.asList(getChildren.split(","));
         }
 
         try {
+            SolrUpdateRequest updateRequest;
             if (children == null) {
                 updateRequest = new SolrUpdateRequest(pid, IndexingActionType.getAction(action));
             } else {
@@ -81,4 +84,10 @@ public class SolrUpdateProcessor implements Processor {
         }
     }
 
+    /**
+     * @param solrIndexingActionMap the solrIndexingActionMap to set
+     */
+    public void setSolrIndexingActionMap(Map<IndexingActionType, IndexingAction> solrIndexingActionMap) {
+        this.solrIndexingActionMap = solrIndexingActionMap;
+    }
 }
