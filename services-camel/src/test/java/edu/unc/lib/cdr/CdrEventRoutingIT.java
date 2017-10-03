aac6e2b6df50d6ff566579501a9fdfd0776af5b2
bbpennel <bbpennel@email.unc.edu>
2017-10-03 15:46:27 -0400

    Rename of IT for cdr event routing

--- services-camel/src/test/java/edu/unc/lib/cdr/CdrEventRoutingIT.java
+++ services-camel/src/test/java/edu/unc/lib/cdr/CdrEventRoutingIT.java
@@ -15,33 +15,56 @@
  */
 package edu.unc.lib.cdr;
 
+import static edu.unc.lib.dl.util.IndexingActionType.ADD_SET_TO_PARENT;
+import static edu.unc.lib.dl.util.IndexingActionType.UPDATE_STATUS;
 import static java.util.Collections.emptyList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.NotifyBuilder;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import edu.unc.lib.dl.data.ingest.solr.ChildSetRequest;
+import edu.unc.lib.dl.data.ingest.solr.SolrUpdateRequest;
+import edu.unc.lib.dl.data.ingest.solr.action.IndexingAction;
 import edu.unc.lib.dl.fcrepo4.PIDs;
 import edu.unc.lib.dl.fcrepo4.Repository;
 import edu.unc.lib.dl.fedora.PID;
 import edu.unc.lib.dl.services.OperationsMessageSender;
+import edu.unc.lib.dl.util.IndexingActionType;
 
+/**
+ *
+ * @author bbpennel
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"/service-context.xml", "/cdr-event-to-solr-it-context.xml"})
-public class CdrEventToSolrUpdateProcessorIT {
+public class CdrEventRoutingIT {
 
     private static final String USER_ID = "user";
     private static final String DEPOSIT_ID = "deposit";
-    private static final String BASE_URI = "http://example.com/rest";
+    private static final String BASE_URI = "http://example.com/rest/";
 
     @Autowired
     private OperationsMessageSender opsMsgSender;
@@ -50,88 +73,71 @@ public class CdrEventToSolrUpdateProcessorIT {
     private SolrUpdateProcessor solrUpdateProcessor;
 
     @Autowired
-    private JmsTemplate cdrEventsJmsTemplate;
-
-    @Autowired
     private Repository repository;
 
-    private List<PID> destinations;
+    @Autowired
+    private CamelContext cdrServiceSolrUpdate;
+
+    @Mock
+    private Map<IndexingActionType, IndexingAction> mockActionMap;
+    @Mock
+    private IndexingAction mockIndexingAction;
+    @Captor
+    private ArgumentCaptor<SolrUpdateRequest> updateRequestCaptor;
 
     @Before
     public void init() throws Exception {
-        //initMocks(this);
+        initMocks(this);
 
         when(repository.getBaseUri()).thenReturn(BASE_URI);
         PIDs.setRepository(repository);
 
-        destinations = pidList(1);
-    }
+        solrUpdateProcessor.setSolrIndexingActionMap(mockActionMap);
 
-    @Test
-    public void testNoMessageBody() throws Exception {
-
-    }
-
-    @Test
-    public void testUnknownAction() throws Exception {
-
-    }
-
-    @Test
-    public void testMoveAction() throws Exception {
-        // opsMsgSender.sendMoveOperation(userid, sources, destination, moved, reordered);
+        when(mockActionMap.get(any(IndexingActionType.class)))
+                .thenReturn(mockIndexingAction);
     }
 
     @Test
     public void testAddAction() throws Exception {
         List<PID> added = pidList(3);
+        List<PID> destinations = pidList(1);
 
         opsMsgSender.sendAddOperation(USER_ID, destinations, added, emptyList(), DEPOSIT_ID);
 
-        Thread.sleep(5000);
-        System.out.println("Finishing up");
-//        Boolean messageWaiting = true;
-//        while (messageWaiting) {
-//            messageWaiting =
-//                cdrEventsJmsTemplate.browse("direct-vm:cdrEvents.start",
-//                                   (session, browser) -> browser.getEnumeration().hasMoreElements());
-//        }
-        // verify(solrUpdateProcessor).process(any(Exchange.class));
-    }
+        NotifyBuilder notify = new NotifyBuilder(cdrServiceSolrUpdate)
+                .whenCompleted(1)
+                .create();
 
-    @Test
-    public void testReorderAction() throws Exception {
+        notify.matches(3l, TimeUnit.SECONDS);
 
-    }
+        verify(mockIndexingAction).performAction(updateRequestCaptor.capture());
 
-    @Test
-    public void testIndexAction() throws Exception {
+        ChildSetRequest updateRequest = (ChildSetRequest) updateRequestCaptor.getValue();
+        assertEquals(ADD_SET_TO_PARENT, updateRequest.getUpdateAction());
 
-    }
-
-    @Test
-    public void testIndexPrimaryObjectAction() throws Exception {
-
-    }
-
-    @Test
-    public void testReindexAction() throws Exception {
-
-    }
-
-    @Test
-    public void testReindexInplaceAction() throws Exception {
-
+        assertTrue(updateRequest.getChildren().containsAll(added));
     }
 
     @Test
     public void testPublishAction() throws Exception {
+        int numPids = 3;
+        List<PID> pids = pidList(numPids);
 
-    }
+        opsMsgSender.sendPublishOperation(USER_ID, pids, true);
 
-    @Test
-    public void testEditTypeAction() throws Exception {
+        NotifyBuilder notify = new NotifyBuilder(cdrServiceSolrUpdate)
+                .whenCompleted(numPids)
+                .create();
 
+        notify.matches(3l, TimeUnit.SECONDS);
+
+        verify(mockIndexingAction, times(numPids)).performAction(updateRequestCaptor.capture());
+
+        // Verify that all requests processed triggered status updates
+        List<SolrUpdateRequest> updateRequests = updateRequestCaptor.getAllValues();
+        updateRequests.stream()
+                .peek(r -> assertEquals(UPDATE_STATUS, r.getUpdateAction()));
     }
 
     private List<PID> pidList(int count) {
