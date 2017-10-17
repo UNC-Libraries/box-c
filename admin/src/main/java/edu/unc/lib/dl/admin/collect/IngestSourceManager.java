19c2d4235f0e1d1fec2de3e9b0427e2c7b8cd87a
harringj <harring@email.unc.edu>
2017-10-17 15:37:12 -0400

    deletes and comments out unneeded code to get around compile errors

--- admin/src/main/java/edu/unc/lib/dl/admin/collect/IngestSourceManager.java
+++ admin/src/main/java/edu/unc/lib/dl/admin/collect/IngestSourceManager.java
@@ -18,18 +18,13 @@ package edu.unc.lib.dl.admin.collect;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -43,9 +38,7 @@ import org.codehaus.jackson.map.type.CollectionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.unc.lib.dl.fedora.PID;
 import edu.unc.lib.dl.util.PackagingType;
-import edu.unc.lib.dl.util.TripleStoreQueryService;
 import gov.loc.repository.bagit.Bag;
 import gov.loc.repository.bagit.BagFactory;
 import gov.loc.repository.bagit.BagFile;
@@ -53,7 +46,7 @@ import gov.loc.repository.bagit.BagHelper;
 
 /**
  * Loads and manages ingest sources, which are preconfigured locations to find packages for deposit.
- * 
+ *
  * @author bbpennel
  * @date Oct 22, 2015
  */
@@ -62,8 +55,6 @@ public class IngestSourceManager {
     private static final Logger log = LoggerFactory.getLogger(IngestSourceManager.class);
 
     private List<IngestSourceConfiguration> configs;
-
-    private TripleStoreQueryService tripleService;
 
     private String configPath;
 
@@ -119,80 +110,80 @@ public class IngestSourceManager {
 
     /**
      * Retrieves a list of ingest sources which contain or match the destination object provided.
-     * 
+     *
      * @param destination
      * @return
      */
-    public List<IngestSourceConfiguration> listSources(PID destination) {
-        List<PID> ancestors = tripleService.lookupAllContainersAbove(destination);
-
-        // Determine which sources apply to the selected destination
-        List<IngestSourceConfiguration> applicableSources = new ArrayList<>();
-        for (IngestSourceConfiguration source : configs) {
-            for (String container : source.getContainers()) {
-                PID containerPID = new PID(container);
-                if (containerPID.equals(destination) || ancestors.contains(containerPID)) {
-                    applicableSources.add(source);
-                    continue;
-                }
-            }
-        }
-
-        return applicableSources;
-    }
+//    public List<IngestSourceConfiguration> listSources(PID destination) {
+//        List<PID> ancestors = tripleService.lookupAllContainersAbove(destination);
+//
+//        // Determine which sources apply to the selected destination
+//        List<IngestSourceConfiguration> applicableSources = new ArrayList<>();
+//        for (IngestSourceConfiguration source : configs) {
+//            for (String container : source.getContainers()) {
+//                PID containerPID = new PID(container);
+//                if (containerPID.equals(destination) || ancestors.contains(containerPID)) {
+//                    applicableSources.add(source);
+//                    continue;
+//                }
+//            }
+//        }
+//
+//        return applicableSources;
+//    }
 
     /**
      * Retrieves a list of candidate file information for ingestable packages from sources which are
      * applicable to the destination provided.
-     * 
+     *
      * @param destination
      * @return
      */
-    public List<Map<String, Object>> listCandidates(PID destination) {
-
-        List<IngestSourceConfiguration> applicableSources = listSources(destination);
-
-        final List<Map<String, Object>> candidates = new ArrayList<>();
-        for (final IngestSourceConfiguration source : applicableSources) {
-            final String base = source.getBase();
-
-            // Gathering candidates per pattern within a particular base directory
-            for (String pattern : source.getPatterns()) {
-                final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + base + pattern);
-                try {
-                    Files.walkFileTree(Paths.get(base), new SimpleFileVisitor<Path>() {
-                        @Override
-                        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-                                throws IOException {
-                            if (matcher.matches(dir)) {
-                                log.debug("Matched dir {} for source {}", dir, source.getId());
-                                addCandidate(candidates, dir, source, base);
-                                return FileVisitResult.SKIP_SUBTREE;
-                            }
-                            return FileVisitResult.CONTINUE;
-                        }
-
-                        @Override
-                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                            if (matcher.matches(file)) {
-                                log.debug("Matched file {} for source {}", file, source.getId());
-                                addCandidate(candidates, file, source, base);
-                            }
-                            return FileVisitResult.CONTINUE;
-                        }
-                    });
-                } catch (IOException e) {
-                    log.error("Failed to gather candidate files for source {}", source.getId(), e);
-                }
-            }
-        }
-
-        return candidates;
-    }
+//    public List<Map<String, Object>> listCandidates(PID destination) {
+//
+//        List<IngestSourceConfiguration> applicableSources = listSources(destination);
+//
+//        final List<Map<String, Object>> candidates = new ArrayList<>();
+//        for (final IngestSourceConfiguration source : applicableSources) {
+//            final String base = source.getBase();
+//
+//            // Gathering candidates per pattern within a particular base directory
+//            for (String pattern : source.getPatterns()) {
+//                final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:" + base + pattern);
+//                try {
+//                    Files.walkFileTree(Paths.get(base), new SimpleFileVisitor<Path>() {
+//                        @Override
+//                        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+//                                throws IOException {
+//                            if (matcher.matches(dir)) {
+//                                log.debug("Matched dir {} for source {}", dir, source.getId());
+//                                addCandidate(candidates, dir, source, base);
+//                                return FileVisitResult.SKIP_SUBTREE;
+//                            }
+//                            return FileVisitResult.CONTINUE;
+//                        }
+//
+//                        @Override
+//                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+//                            if (matcher.matches(file)) {
+//                                log.debug("Matched file {} for source {}", file, source.getId());
+//                                addCandidate(candidates, file, source, base);
+//                            }
+//                            return FileVisitResult.CONTINUE;
+//                        }
+//                    });
+//                } catch (IOException e) {
+//                    log.error("Failed to gather candidate files for source {}", source.getId(), e);
+//                }
+//            }
+//        }
+//
+//        return candidates;
+//    }
 
     /**
      * Adds information about applicable packages to the list of candidates
-     * 
+     *
      * @param candidates
      * @param filePath
      * @param source
@@ -254,7 +245,7 @@ public class IngestSourceManager {
 
     /**
      * Returns true if the given path is from valid for the given source and present.
-     * 
+     *
      * @param pathString
      * @param sourceId
      * @return
@@ -275,7 +266,7 @@ public class IngestSourceManager {
 
     /**
      * Returns true if the given path matches any of the patterns specified for the given source
-     * 
+     *
      * @param path
      * @param source
      * @return
@@ -302,10 +293,6 @@ public class IngestSourceManager {
 
     public void setConfigs(List<IngestSourceConfiguration> configs) {
         this.configs = configs;
-    }
-
-    public void setTripleService(TripleStoreQueryService tripleService) {
-        this.tripleService = tripleService;
     }
 
     public void setConfigPath(String configPath) {
--- admin/src/main/java/edu/unc/lib/dl/admin/controller/EditLabelController.java
+++ admin/src/main/java/edu/unc/lib/dl/admin/controller/EditLabelController.java
@@ -15,61 +15,42 @@
  */
 package edu.unc.lib.dl.admin.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.annotation.Resource;
-
-import org.jdom2.Element;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import edu.unc.lib.dl.fedora.FedoraException;
-import edu.unc.lib.dl.fedora.ManagementClient;
-import edu.unc.lib.dl.fedora.PID;
-import edu.unc.lib.dl.ingest.IngestException;
-import edu.unc.lib.dl.util.PremisEventLogger;
 
 /**
- * 
+ *
  * @author sreenug
  *
  */
 @Controller
 public class EditLabelController {
-    @Resource(name = "managementClient")
-    private ManagementClient client;
 
-    @RequestMapping(value = "editlabel/{pid}", method = RequestMethod.POST)
-    public @ResponseBody
-    Object saveLabel(@PathVariable("pid") String pid,
-            @RequestParam("label") String label) throws IngestException {
-
-        if (label != null && label.trim().length() > 0) {
-            try {
-                PremisEventLogger logger = new PremisEventLogger(pid);
-                PID pidObject = new PID(pid);
-
-                this.client.modifyObject(pidObject, label, null, null, null);
-
-                Element event = logger.logEvent(PremisEventLogger.Type.MIGRATION,
-                        "Object renamed to " + label, pidObject);
-                PremisEventLogger.addDetailedOutcome(event, "success", "Object renamed successfully", null);
-                this.client.writePremisEventsToFedoraObject(logger, pidObject);
-            } catch (FedoraException e) {
-                throw new IngestException("Could not update label for " + pid, e);
-            }
-        } else {
-            Map<String, String> response = new HashMap<>();
-            response.put("message", "error");
-            return response ;
-        }
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "success");
-        return response;
-    }
+//    @RequestMapping(value = "editlabel/{pid}", method = RequestMethod.POST)
+//    public @ResponseBody
+//    Object saveLabel(@PathVariable("pid") String pid,
+//            @RequestParam("label") String label) throws IngestException {
+//
+//        if (label != null && label.trim().length() > 0) {
+//            try {
+//                PremisEventLogger logger = new PremisEventLogger(pid);
+//                PID pidObject = PIDs.get(pid);
+//
+//                this.client.modifyObject(pidObject, label, null, null, null);
+//
+//                Element event = logger.logEvent(PremisEventLogger.Type.MIGRATION,
+//                        "Object renamed to " + label, pidObject);
+//                PremisEventLogger.addDetailedOutcome(event, "success", "Object renamed successfully", null);
+//                this.client.writePremisEventsToFedoraObject(logger, pidObject);
+//            } catch (FedoraException e) {
+//                throw new IngestException("Could not update label for " + pid, e);
+//            }
+//        } else {
+//            Map<String, String> response = new HashMap<>();
+//            response.put("message", "error");
+//            return response ;
+//        }
+//        Map<String, String> response = new HashMap<>();
+//        response.put("message", "success");
+//        return response;
+//    }
 }
