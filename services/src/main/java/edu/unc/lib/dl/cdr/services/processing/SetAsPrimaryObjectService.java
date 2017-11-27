f236a49e99f0dea8ace448c3f6a9c0a0bcb6bec9
harringj <harring@email.unc.edu>
2017-11-27 11:56:11 -0500

    restores files after failed cherry-pick from fcrepo4

---
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/processing/SetAsPrimaryObjectService.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.cdr.services.processing;
+
+import static edu.unc.lib.dl.acl.util.Permission.editResourceType;
+
+import java.util.Arrays;
+
+import edu.unc.lib.dl.acl.service.AccessControlService;
+import edu.unc.lib.dl.acl.util.AgentPrincipals;
+import edu.unc.lib.dl.fcrepo4.FileObject;
+import edu.unc.lib.dl.fcrepo4.RepositoryObject;
+import edu.unc.lib.dl.fcrepo4.RepositoryObjectLoader;
+import edu.unc.lib.dl.fcrepo4.WorkObject;
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.model.InvalidOperationForObjectType;
+import edu.unc.lib.dl.services.OperationsMessageSender;
+import edu.unc.lib.dl.util.IndexingActionType;
+
+/**
+ * Service that manages setting a primary object on a work
+ *
+ * @author harring
+ *
+ */
+public class SetAsPrimaryObjectService {
+
+    private AccessControlService aclService;
+    private RepositoryObjectLoader repoObjLoader;
+    private OperationsMessageSender operationsMessageSender;
+
+    /**
+     * Sets file object with the given pid as the primary object for its work using the agent principals provided.
+     *
+     * @param agent security principals of the agent making request.
+     * @param fileObjPid the file object to be set as primary object
+     */
+    public void setAsPrimaryObject(AgentPrincipals agent, PID fileObjPid) {
+
+        aclService.assertHasAccess("Insufficient privileges to set " + fileObjPid.getUUID() + " as primary object",
+                fileObjPid, agent.getPrincipals(), editResourceType);
+
+        RepositoryObject repoObj = repoObjLoader.getRepositoryObject(fileObjPid);
+
+        if (!(repoObj instanceof FileObject)) {
+            throw new InvalidOperationForObjectType("Cannot set " + fileObjPid.getUUID()
+                    + " as primary object, since objects of type " + repoObj.getClass().getName()
+                    + " are not eligible.");
+        }
+
+        RepositoryObject parent = repoObj.getParent();
+        if (!(parent instanceof WorkObject)) {
+            throw new InvalidOperationForObjectType("Object of type " + parent.getClass().getName()
+            + " cannot have a primary object.");
+        }
+        WorkObject work = (WorkObject) parent;
+        work.setPrimaryObject(fileObjPid);
+
+        // Send message that the action completed
+        operationsMessageSender.sendIndexingOperation(agent.getUsername(), Arrays.asList(fileObjPid),
+                IndexingActionType.SET_AS_PRIMARY_OBJECT);
+    }
+
+    /**
+     * @param aclService the aclService to set
+     */
+    public void setAclService(AccessControlService aclService) {
+        this.aclService = aclService;
+    }
+
+    /**
+     * @param repoObjLoader the object loader to set
+     */
+    public void setRepositoryObjectLoader(RepositoryObjectLoader repoObjLoader) {
+        this.repoObjLoader = repoObjLoader;
+    }
+
+    /**
+     * @param operatonsMessageSender the message sender to set
+     */
+    public void setOperationsMessageSender(OperationsMessageSender operationsMessageSender) {
+        this.operationsMessageSender = operationsMessageSender;
+    }
+
+}
