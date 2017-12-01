967cfa5194dddd40982feb5513c400e2ff4d9db7
bbpennel <bbpennel@email.unc.edu>
2017-12-01 15:56:46 -0500

    Implemented MODS validator and unit test.  Added metadata validation exception

---
+++ metadata/src/main/java/edu/unc/lib/dl/validation/MetadataValidationException.java
@@ -0,0 +1,58 @@
+package edu.unc.lib.dl.validation;
+
+/**
+ * Exception indicating that metadata was determined to be invalid
+ *
+ * @author bbpennel
+ *
+ */
+public class MetadataValidationException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    private String details;
+
+    public MetadataValidationException() {
+    }
+
+    public MetadataValidationException(String message) {
+        super(message);
+    }
+
+    public MetadataValidationException(String message, String details) {
+        super(message);
+        this.details = details;
+    }
+
+    public MetadataValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    public MetadataValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MetadataValidationException(String message, String details, Throwable cause) {
+        super(message, cause);
+        this.details = details;
+    }
+
+    /**
+     * @return the details
+     */
+    public String getDetails() {
+        return details;
+    }
+
+    /**
+     * Returns the message and details of this exception if available.
+     *
+     * @return
+     */
+    public String getDetailedMessage() {
+        if (details == null) {
+            return getMessage();
+        } else {
+            return getMessage() + "\n" + details;
+        }
+    }
+}
