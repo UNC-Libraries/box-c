683b92a7f2d40253d10bc9f39c57d6e49c18fc4f
harringj <harring@email.unc.edu>
2017-12-04 14:03:16 -0500

    updates service to use mods validator

--- services/src/main/java/edu/unc/lib/dl/cdr/services/processing/UpdateDescriptionService.java
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/processing/UpdateDescriptionService.java
@@ -15,7 +15,7 @@
  */
 package edu.unc.lib.dl.cdr.services.processing;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
@@ -26,6 +26,8 @@ import edu.unc.lib.dl.fcrepo4.ContentObject;
 import edu.unc.lib.dl.fcrepo4.RepositoryObjectLoader;
 import edu.unc.lib.dl.fedora.PID;
 import edu.unc.lib.dl.services.OperationsMessageSender;
+import edu.unc.lib.dl.validation.MODSValidator;
+import edu.unc.lib.dl.validation.MetadataValidationException;
 
 /**
  * Service that manages description, e.g., MODS, updates
@@ -38,24 +40,20 @@ public class UpdateDescriptionService {
     private AccessControlService aclService;
     private RepositoryObjectLoader repoObjLoader;
     private OperationsMessageSender operationsMessageSender;
+    private MODSValidator modsValidator;
 
     public void updateDescription(AgentPrincipals agent, PID pid, InputStream modsStream)
-            throws FileNotFoundException {
+            throws MetadataValidationException, IOException {
         aclService.assertHasAccess("User does not have permissions to update description",
                 pid, agent.getPrincipals(), Permission.editDescription);
 
         String username = agent.getUsername();
-        validateMODS(modsStream, pid, username);
+        modsStream = modsValidator.validate(modsStream);
 
         ContentObject obj = (ContentObject) repoObjLoader.getRepositoryObject(pid);
         obj.addDescription(modsStream);
 
         operationsMessageSender.sendUpdateDescriptionOperation(username, Arrays.asList(pid));
-
-    }
-
-    private void validateMODS(InputStream modsStream, PID pid, String username) {
-
     }
 
     /**
@@ -78,6 +76,14 @@ public class UpdateDescriptionService {
      */
     public void setOperationsMessageSender(OperationsMessageSender operationsMessageSender) {
         this.operationsMessageSender = operationsMessageSender;
+    }
+
+    /**
+     *
+     * @param modsValidator
+     */
+    public void setMODSValidator(MODSValidator modsValidator) {
+        this.modsValidator = modsValidator;
     }
 
 }
