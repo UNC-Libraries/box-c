97fee3de9d797e4cd95e0f9a901a3eb3104a6f18
harringj <harring@email.unc.edu>
2017-12-04 14:03:23 -0500

    updates unit test to get all cases passing

--- services/src/test/java/edu/unc/lib/dl/cdr/services/processing/UpdateDescriptionServiceTest.java
+++ services/src/test/java/edu/unc/lib/dl/cdr/services/processing/UpdateDescriptionServiceTest.java
@@ -15,13 +15,13 @@
  */
 package edu.unc.lib.dl.cdr.services.processing;
 
-import static edu.unc.lib.dl.acl.util.Permission.viewMetadata;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -42,10 +42,15 @@ import edu.unc.lib.dl.acl.exception.AccessRestrictionException;
 import edu.unc.lib.dl.acl.service.AccessControlService;
 import edu.unc.lib.dl.acl.util.AccessGroupSet;
 import edu.unc.lib.dl.acl.util.AgentPrincipals;
+import edu.unc.lib.dl.acl.util.Permission;
+import edu.unc.lib.dl.fcrepo4.ContentObject;
+import edu.unc.lib.dl.fcrepo4.FileObject;
 import edu.unc.lib.dl.fcrepo4.PIDs;
 import edu.unc.lib.dl.fcrepo4.RepositoryObjectLoader;
 import edu.unc.lib.dl.fedora.PID;
 import edu.unc.lib.dl.services.OperationsMessageSender;
+import edu.unc.lib.dl.validation.MODSValidator;
+import edu.unc.lib.dl.validation.MetadataValidationException;
 
 /**
  *
@@ -60,17 +65,21 @@ public class UpdateDescriptionServiceTest {
     private RepositoryObjectLoader repoObjLoader;
     @Mock
     private OperationsMessageSender messageSender;
+    @Mock
+    private MODSValidator modsValidator;
 
     @Mock
     private AgentPrincipals agent;
     @Mock
     private AccessGroupSet groups;
+    @Mock
+    private ContentObject obj;
 
     @Captor
     private ArgumentCaptor<Collection<PID>> pidsCaptor;
 
     private UpdateDescriptionService service;
-    private PID objectPid;
+    private PID objPid;
     private InputStream modsStream;
 
     @SuppressWarnings("unchecked")
@@ -79,33 +88,47 @@ public class UpdateDescriptionServiceTest {
         initMocks(this);
 
         when(agent.getPrincipals()).thenReturn(groups);
+        when(agent.getUsername()).thenReturn("username");
+        when(repoObjLoader.getRepositoryObject(any(PID.class))).thenReturn(obj);
+        when(obj.addDescription(eq(modsStream))).thenReturn(mock(FileObject.class));
         when(messageSender.sendUpdateDescriptionOperation(anyString(), any(Collection.class)))
                 .thenReturn("message_id");
 
-        objectPid = PIDs.get(UUID.randomUUID().toString());
-        modsStream = new FileInputStream(new File("txt.txt"));
+        objPid = PIDs.get(UUID.randomUUID().toString());
+        modsStream = new FileInputStream(new File("src/test/resources/txt.txt"));
 
         service = new UpdateDescriptionService();
         service.setAclService(aclService);
         service.setRepositoryObjectLoader(repoObjLoader);
         service.setOperationsMessageSender(messageSender);
+        service.setMODSValidator(modsValidator);
     }
 
     @SuppressWarnings("unchecked")
     @Test
     public void updateDescriptionTest() throws Exception {
-        service.updateDescription(agent, objectPid, modsStream);
+        when(modsValidator.validate(modsStream)).thenReturn(modsStream);
+        service.updateDescription(agent, objPid, modsStream);
 
         verify(messageSender).sendUpdateDescriptionOperation(anyString(), pidsCaptor.capture());
         Collection<PID> pids = pidsCaptor.getValue();
         assertEquals(pids.size(), 1);
-        assertTrue(pids.contains(objectPid));
+        assertTrue(pids.contains(objPid));
     }
 
     @Test(expected = AccessRestrictionException.class)
-    public void insufficientAccessTest() {
+    public void insufficientAccessTest() throws Exception {
         doThrow(new AccessRestrictionException()).when(aclService)
-        .assertHasAccess(anyString(), eq(objectPid), any(AccessGroupSet.class), eq(viewMetadata));
+        .assertHasAccess(anyString(), eq(objPid), any(AccessGroupSet.class), any(Permission.class));
+
+        service.updateDescription(agent, objPid, modsStream);
+    }
+
+    @Test(expected = MetadataValidationException.class)
+    public void invalidModsTest() throws Exception {
+        doThrow(new MetadataValidationException()).when(modsValidator).validate(eq(modsStream));
+
+        service.updateDescription(agent, objPid, modsStream);
     }
 
 }
