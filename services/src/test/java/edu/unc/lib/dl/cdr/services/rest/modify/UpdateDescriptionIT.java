9a5206d528cb966e98ebed6e30a86cc074604665
harringj <harring@email.unc.edu>
2017-12-05 16:02:49 -0500

    adds skeleton code for update description IT

---
+++ services/src/test/java/edu/unc/lib/dl/cdr/services/rest/modify/UpdateDescriptionIT.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.cdr.services.rest.modify;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import edu.unc.lib.dl.acl.service.AccessControlService;
+import edu.unc.lib.dl.acl.util.AccessGroupSet;
+import edu.unc.lib.dl.acl.util.GroupsThreadStore;
+import edu.unc.lib.dl.fcrepo4.ContentObject;
+import edu.unc.lib.dl.fcrepo4.PIDs;
+import edu.unc.lib.dl.fcrepo4.RepositoryObjectLoader;
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.test.TestHelper;
+
+/**
+ *
+ * @author harring
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"/spring-test/test-fedora-container.xml", "/spring-test/cdr-client-container.xml", "/update-description-it-servlet.xml"})
+@WebAppConfiguration
+public class UpdateDescriptionIT {
+
+    @Autowired
+    private WebApplicationContext context;
+    @Autowired
+    private AccessControlService aclService;
+    @Autowired
+    private RepositoryObjectLoader repoObjLoader;
+
+    private MockMvc mvc;
+    private PID objPid;
+
+    @Before
+    public void init() {
+
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+
+        TestHelper.setContentBase("http://localhost:48085/rest");
+
+        GroupsThreadStore.storeUsername("user");
+        GroupsThreadStore.storeGroups(new AccessGroupSet("adminGroup"));
+
+        objPid = makePid();
+
+    }
+
+    @After
+    public void tearDown() {
+        GroupsThreadStore.clearStore();
+    }
+
+    @Test
+    public void testUpdateDescription() throws UnsupportedOperationException, Exception {
+
+        MvcResult result = mvc.perform(put("/edit/description/" + objPid.getUUID()))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+    }
+
+    private PID makePid() {
+        return PIDs.get(UUID.randomUUID().toString());
+    }
+
+    private Map<String, Object> getMapFromResponse(MvcResult result) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(result.getResponse().getContentAsString(),
+                new TypeReference<Map<String, Object>>(){});
+    }
+
+    private void assertDescriptionUpdated(PID objPid) {
+        ContentObject obj = (ContentObject) repoObjLoader.getRepositoryObject(objPid);
+        assertNotNull(obj.getDescription().getMODS());
+    }
+
+}
