f320ba73aa3b02248e9bf6410e6fd7d6aa3a3eff
bbpennel <bbpennel@email.unc.edu>
2018-01-02 10:04:44 -0500

    Fix unit tests for structure and count queries

--- access-common/src/test/java/edu/unc/lib/dl/ui/service/AbstractSolrQueryLayerTest.java
+++ access-common/src/test/java/edu/unc/lib/dl/ui/service/AbstractSolrQueryLayerTest.java
@@ -20,10 +20,14 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.core.CoreContainer;
 import org.junit.After;
 import org.junit.Before;
@@ -32,6 +36,9 @@ import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
 
 import edu.unc.lib.dl.acl.fcrepo4.GlobalPermissionEvaluator;
+import edu.unc.lib.dl.fcrepo4.PIDs;
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.rdf.Cdr;
 import edu.unc.lib.dl.search.solr.service.SearchStateFactory;
 import edu.unc.lib.dl.search.solr.util.FacetFieldUtil;
 import edu.unc.lib.dl.search.solr.util.SearchSettings;
@@ -60,7 +67,11 @@ public class AbstractSolrQueryLayerTest {
     @Rule
     public final TemporaryFolder tmpFolder = new TemporaryFolder();
 
-    public final String COLLECTIONS_PID = "uuid:Collections";
+    protected PID rootPid;
+    protected PID coll1Pid;
+    protected PID coll2Pid;
+    protected PID folderPid;
+    protected PID workPid;
 
     @Before
     public void setUp() throws Exception {
@@ -104,5 +115,98 @@ public class AbstractSolrQueryLayerTest {
     public void tearDown() throws Exception {
         server.close();
         FileUtils.forceDelete(dataDir);
+    }
+
+    protected List<SolrInputDocument> populate() {
+        List<SolrInputDocument> docs = new ArrayList<>();
+
+        SolrInputDocument newDoc = new SolrInputDocument();
+        rootPid = makePid();
+        String rootId = rootPid.getId();
+        newDoc.addField("title", "Collections Root");
+        addAccessFields(newDoc, Cdr.ContentRoot.getLocalName(), rootId);
+        addAncestors(newDoc, true, rootId);
+        docs.add(newDoc);
+
+        newDoc = new SolrInputDocument();
+        coll1Pid = makePid();
+        String coll1Id = coll1Pid.getId();
+        newDoc.addField("title", "coll 1");
+        addAccessFields(newDoc, "Collection", coll1Id);
+        addAncestors(newDoc, true, rootId, coll1Id);
+        docs.add(newDoc);
+
+        newDoc = new SolrInputDocument();
+        coll2Pid = makePid();
+        String coll2Id = coll2Pid.getId();
+        newDoc.addField("title", "coll 2");
+        addAccessFields(newDoc, "Collection", coll2Id);
+        addAncestors(newDoc, true, rootId, coll2Id);
+        docs.add(newDoc);
+
+        newDoc = new SolrInputDocument();
+        folderPid = makePid();
+        String folderId = folderPid.getId();
+        newDoc.addField("title", "folder 1 coll 1");
+        addAccessFields(newDoc, "Folder", folderId);
+        addAncestors(newDoc, true, rootId, coll1Id, folderId);
+        docs.add(newDoc);
+
+        newDoc = new SolrInputDocument();
+        workPid = makePid();
+        String workId = workPid.getId();
+        newDoc.addField("title", "work 1 folder 1 coll 1");
+        addAccessFields(newDoc, "Work", workId);
+        addAncestors(newDoc, true, rootId, coll1Id, folderId, workId);
+        docs.add(newDoc);
+
+        newDoc = new SolrInputDocument();
+        PID file1Pid = makePid();
+        String file1Id = file1Pid.getId();
+        newDoc.addField("title", "file1 work 1 folder 1 coll 1");
+        addAccessFields(newDoc, "File", file1Id, workId);
+        addAncestors(newDoc, false, rootId, coll1Id, folderId, workId, file1Id);
+        docs.add(newDoc);
+
+        newDoc = new SolrInputDocument();
+        PID file2Pid = makePid();
+        String file2Id = file2Pid.getId();
+        newDoc.addField("title", "file2 work 1 folder 1 coll 1");
+        addAccessFields(newDoc, "File", file2Id, workId);
+        addAncestors(newDoc, false, rootId, coll1Id, folderId, workId, file2Id);
+        docs.add(newDoc);
+
+        return docs;
+    }
+
+    private void addAncestors(SolrInputDocument doc, boolean isContainer, String... ids) {
+        List<String> ancestorPath = new ArrayList<>();
+        String ancestorIds = "";
+        for (int i = 0; i < ids.length; i++) {
+            if (i < ids.length - 1) {
+                ancestorPath.add((i + 1) + "," + ids[i]);
+            }
+            if (i < ids.length - 1 || isContainer) {
+                ancestorIds += "/" + ids[i];
+            }
+        }
+        doc.addField("ancestorIds", ancestorIds);
+        doc.addField("ancestorPath", ancestorPath);
+    }
+
+    protected void addAccessFields(SolrInputDocument doc, String type, String id) {
+        addAccessFields(doc, type, id, id);
+    }
+
+    protected void addAccessFields(SolrInputDocument doc, String type, String id, String rollup) {
+        doc.addField("id", id);
+        doc.addField("rollup", rollup);
+        doc.addField("resourceType", type);
+        doc.addField("roleGroup", "public admin");
+        doc.addField("readGroup", "public");
+    }
+
+    protected PID makePid() {
+        return PIDs.get(UUID.randomUUID().toString());
     }
 }
--- access-common/src/test/java/edu/unc/lib/dl/ui/service/ChildrenCountTest.java
+++ access-common/src/test/java/edu/unc/lib/dl/ui/service/ChildrenCountTest.java
@@ -17,18 +17,13 @@ package edu.unc.lib.dl.ui.service;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-import org.apache.solr.common.SolrInputDocument;
 import org.junit.Before;
 import org.junit.Test;
 
 import edu.unc.lib.dl.acl.util.AccessGroupSet;
-import edu.unc.lib.dl.fcrepo4.PIDs;
 import edu.unc.lib.dl.fedora.PID;
-import edu.unc.lib.dl.rdf.Cdr;
 import edu.unc.lib.dl.search.solr.model.BriefObjectMetadata;
 import edu.unc.lib.dl.search.solr.model.SearchRequest;
 import edu.unc.lib.dl.search.solr.model.SearchResultResponse;
@@ -39,11 +34,6 @@ import edu.unc.lib.dl.search.solr.model.SearchState;
  * @since Jan 23, 2015
  */
 public class ChildrenCountTest extends AbstractSolrQueryLayerTest {
-
-    private PID rootPid;
-    private PID collPid;
-    private PID folderPid;
-    private PID workPid;
 
     @Before
     public void init() throws Exception {
@@ -58,7 +48,7 @@ public class ChildrenCountTest extends AbstractSolrQueryLayerTest {
         queryLayer.getChildrenCounts(resp.getResultList(), new AccessGroupSet("public"));
 
         BriefObjectMetadata collections = getResultByPID(rootPid.getId(), resp.getResultList());
-        assertEquals("Incorrect child count on collections object", 5,
+        assertEquals("Incorrect child count on collections object", 6,
                 getChildrenCount(collections, "child"));
 
         BriefObjectMetadata folder = getResultByPID(folderPid.getId(), resp.getResultList());
@@ -74,17 +64,17 @@ public class ChildrenCountTest extends AbstractSolrQueryLayerTest {
                 "-resourceType:File", null);
 
         BriefObjectMetadata collections = getResultByPID(rootPid.getId(), resp.getResultList());
-        assertEquals("Incorrect child count on collections object", 3,
+        assertEquals("Incorrect child count on collections object", 4,
                 getChildrenCount(collections, "containers"));
     }
 
     @Test
     public void testCountFromCollection() throws Exception {
-        SearchResultResponse resp = getResult(collPid);
+        SearchResultResponse resp = getResult(coll1Pid);
 
         queryLayer.getChildrenCounts(resp.getResultList(), new AccessGroupSet("public"));
 
-        BriefObjectMetadata collections = getResultByPID(collPid.getId(), resp.getResultList());
+        BriefObjectMetadata collections = getResultByPID(coll1Pid.getId(), resp.getResultList());
         assertEquals("Incorrect child count on collections object", 4,
                 getChildrenCount(collections, "child"));
 
@@ -113,91 +103,7 @@ public class ChildrenCountTest extends AbstractSolrQueryLayerTest {
         return queryLayer.getSearchResults(request);
     }
 
-    private List<SolrInputDocument> populate() {
-        List<SolrInputDocument> docs = new ArrayList<>();
 
-        SolrInputDocument newDoc = new SolrInputDocument();
-        rootPid = makePid();
-        String rootId = rootPid.getId();
-        newDoc.addField("title", "Collections");
-        addAccessFields(newDoc, Cdr.ContentRoot.getLocalName(), rootId);
-        addAncestors(newDoc, true, rootId);
-        docs.add(newDoc);
-
-        newDoc = new SolrInputDocument();
-        collPid = makePid();
-        String collId = collPid.getId();
-        String collName = "collection 1";
-        newDoc.addField("title", collName);
-        addAccessFields(newDoc, "Collection", collId);
-        addAncestors(newDoc, true, rootId, collId);
-        docs.add(newDoc);
-
-        newDoc = new SolrInputDocument();
-        folderPid = makePid();
-        String folderId = folderPid.getId();
-        newDoc.addField("title", "folder 1 coll 1");
-        addAccessFields(newDoc, "Folder", folderId);
-        addAncestors(newDoc, true, rootId, collId, folderId);
-        docs.add(newDoc);
-
-        newDoc = new SolrInputDocument();
-        workPid = makePid();
-        String workId = workPid.getId();
-        newDoc.addField("title", "work 1 folder 1 coll 1");
-        addAccessFields(newDoc, "Work", workId);
-        addAncestors(newDoc, true, rootId, collId, folderId, workId);
-        docs.add(newDoc);
-
-        newDoc = new SolrInputDocument();
-        PID file1Pid = makePid();
-        String file1Id = file1Pid.getId();
-        newDoc.addField("title", "file1 work 1 folder 1 coll 1");
-        addAccessFields(newDoc, "File", file1Id, workId);
-        addAncestors(newDoc, false, rootId, collId, folderId, workId, file1Id);
-        docs.add(newDoc);
-
-        newDoc = new SolrInputDocument();
-        PID file2Pid = makePid();
-        String file2Id = file2Pid.getId();
-        newDoc.addField("title", "file2 work 1 folder 1 coll 1");
-        addAccessFields(newDoc, "File", file2Id, workId);
-        addAncestors(newDoc, false, rootId, collId, folderId, workId, file2Id);
-        docs.add(newDoc);
-
-        return docs;
-    }
-
-    private void addAncestors(SolrInputDocument doc, boolean isContainer, String... ids) {
-        List<String> ancestorPath = new ArrayList<>();
-        String ancestorIds = "";
-        for (int i = 0; i < ids.length; i++) {
-            if (i < ids.length - 1) {
-                ancestorPath.add((i + 1) + "," + ids[i]);
-            }
-            if (i < ids.length - 1 || isContainer) {
-                ancestorIds += "/" + ids[i];
-            }
-        }
-        doc.addField("ancestorIds", ancestorIds);
-        doc.addField("ancestorPath", ancestorPath);
-    }
-
-    private void addAccessFields(SolrInputDocument doc, String type, String id) {
-        addAccessFields(doc, type, id, id);
-    }
-
-    private void addAccessFields(SolrInputDocument doc, String type, String id, String rollup) {
-        doc.addField("id", id);
-        doc.addField("rollup", rollup);
-        doc.addField("resourceType", type);
-        doc.addField("roleGroup", "public admin");
-        doc.addField("readGroup", "public");
-    }
-
-    private PID makePid() {
-        return PIDs.get(UUID.randomUUID().toString());
-    }
 
     private int getChildrenCount(BriefObjectMetadata md, String countType) {
         return md.getCountMap().get(countType).intValue();
