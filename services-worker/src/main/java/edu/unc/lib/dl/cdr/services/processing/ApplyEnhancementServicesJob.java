f9e83bdd3c90558ea500c23f4a31229f7b74ed34
lfarrell <lfarrell@email.unc.edu>
2018-01-16 11:01:37 -0500

    Add check that fedora is available before running enhancement job

--- services-worker/src/main/java/edu/unc/lib/dl/cdr/services/processing/ApplyEnhancementServicesJob.java
+++ services-worker/src/main/java/edu/unc/lib/dl/cdr/services/processing/ApplyEnhancementServicesJob.java
@@ -7,10 +7,12 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ws.client.WebServiceTransportException;
 
 import edu.unc.lib.dl.cdr.services.ObjectEnhancementService;
 import edu.unc.lib.dl.cdr.services.exception.EnhancementException;
 import edu.unc.lib.dl.cdr.services.model.EnhancementMessage;
+import edu.unc.lib.dl.fedora.ManagementClient;
 import edu.unc.lib.dl.reporting.ActivityMetricsClient;
 import edu.unc.lib.dl.util.JMSMessageUtil.ServicesActions;
 
@@ -22,41 +24,45 @@ public class ApplyEnhancementServicesJob implements Runnable {
 	private long recoverableDelay = 0;
 	private final EnhancementMessage message;
 	private ActivityMetricsClient metricsClient;
-	
-	
+	private ManagementClient managementClient;
+
 	public ApplyEnhancementServicesJob(String pidString, boolean force) {
 		this.message = new EnhancementMessage(pidString, servicesMessageNamespace,
 				ServicesActions.APPLY_SERVICE_STACK.getName());
 		this.message.setCompletedServices(new ArrayList<String>());
 		this.message.setForce(force);
 	}
-	
+
 	public ApplyEnhancementServicesJob(String messagePid, String messageNamespace, String messageAction, String messageServiceName, List<String> filteredServices) {
 		this.message = new EnhancementMessage(messagePid, messageNamespace, messageAction, messageServiceName);
 		this.message.setFilteredServices(filteredServices);
 		this.message.setCompletedServices(new ArrayList<String>());
 	}
-	
+
 	public void setServices(List<ObjectEnhancementService> services) {
 		this.services = services;
 	}
-	
+
 	public void setRecoverableDelay(long recoverableDelay) {
 		this.recoverableDelay = recoverableDelay;
 	}
-	
+
 	public void setMetricsClient(ActivityMetricsClient operationMetricsClient) {
 		this.metricsClient = operationMetricsClient;
 	}
 
 	@Override
 	public void run() {
+		long backoffDelay = 10000;
+		int backoffAttempts = 1;
+		int maxBackoffAttempts = 5;
+
 		for (ObjectEnhancementService service : services) {
 			if (message.getFilteredServices() != null
 					&& !message.getFilteredServices().contains(service.getClass().getName())) {
 				continue;
 			}
-			
+
 			try {
 				if (!service.isApplicable(message)) {
 					continue;
@@ -64,20 +70,45 @@ public class ApplyEnhancementServicesJob implements Runnable {
 			} catch (EnhancementException e) {
 				LOG.error("Error determining applicability for service " + service.getClass().getName() + " and object " + message.getTargetID(), e);
 			}
-			
-			try {
-				applyService(service);
-				metricsClient.incrFinishedEnhancement(service.getClass().getName());
-			} catch (EnhancementException e) {
-				LOG.error("Error applying service " + service.getClass().getName() + " to object " + message.getTargetID(), e);
-				metricsClient.incrFailedEnhancement(service.getClass().getName());
-			} catch (Throwable t) {
-				metricsClient.incrFailedEnhancement(service.getClass().getName());
-				throw t;
+
+			while (backoffAttempts <= maxBackoffAttempts) {
+				try {
+					if (managementClient.isRepositoryAvailable()) {
+						processEnhancement(service);
+						break;
+					} else {
+						throw new WebServiceTransportException("Unable to connect to Fedora");
+					}
+				} catch (WebServiceTransportException e) {
+					try {
+						Thread.sleep(backoffDelay * backoffAttempts);
+					} catch (InterruptedException e1) {
+						LOG.warn("Back off time was interrupted for job " + service.getClass().getName());
+					}
+
+					if (backoffAttempts == maxBackoffAttempts) {
+						LOG.warn("Unable to connect to fedora. Unable to run job for " + service.getClass().getName());
+					}
+
+					backoffAttempts++;
+				}
 			}
 		}
 	}
-	
+
+	private void processEnhancement(ObjectEnhancementService service) {
+		try {
+			applyService(service);
+			metricsClient.incrFinishedEnhancement(service.getClass().getName());
+		} catch (EnhancementException e) {
+			LOG.error("Error applying service " + service.getClass().getName() + " to object " + message.getTargetID(), e);
+			metricsClient.incrFailedEnhancement(service.getClass().getName());
+		} catch (Throwable t) {
+			metricsClient.incrFailedEnhancement(service.getClass().getName());
+			throw t;
+		}
+	}
+
 	private void applyService(ObjectEnhancementService service) throws EnhancementException {
 		while (true) {
 			LOG.info("Applying service {} to object {}", service.getClass().getName(), message.getTargetID());
@@ -85,7 +116,7 @@ public class ApplyEnhancementServicesJob implements Runnable {
 			try {
 				service.getEnhancement(message).call();
 				message.getCompletedServices().add(service.getClass().getName());
-				
+
 				break;
 			} catch (EnhancementException e) {
 				if (e.getSeverity() == EnhancementException.Severity.RECOVERABLE) {
@@ -94,7 +125,7 @@ public class ApplyEnhancementServicesJob implements Runnable {
 					throw e;
 				}
 			}
-			
+
 			try {
 				Thread.sleep(recoverableDelay);
 			} catch (InterruptedException e) {
@@ -103,5 +134,5 @@ public class ApplyEnhancementServicesJob implements Runnable {
 			}
 		}
 	}
-	
+
 }
