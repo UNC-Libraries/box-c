b006ac79d16625bcd084012aac095898a5198243
harringj <harring@email.unc.edu>
2018-01-18 12:02:33 -0500

    finishes unit test for XMLExportService

--- services/src/test/java/edu/unc/lib/dl/cdr/services/processing/XMLExportServiceTest.java
+++ services/src/test/java/edu/unc/lib/dl/cdr/services/processing/XMLExportServiceTest.java
@@ -16,6 +16,10 @@
 package edu.unc.lib.dl.cdr.services.processing;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -32,6 +36,10 @@ import edu.unc.lib.dl.acl.service.AccessControlService;
 import edu.unc.lib.dl.acl.util.AccessGroupSet;
 import edu.unc.lib.dl.cdr.services.rest.modify.ExportXMLController.XMLExportRequest;
 import edu.unc.lib.dl.fcrepo4.PIDs;
+import edu.unc.lib.dl.search.solr.model.BriefObjectMetadata;
+import edu.unc.lib.dl.search.solr.model.SearchRequest;
+import edu.unc.lib.dl.search.solr.model.SearchResultResponse;
+import edu.unc.lib.dl.search.solr.model.SearchState;
 import edu.unc.lib.dl.search.solr.service.SearchStateFactory;
 import edu.unc.lib.dl.ui.service.SolrQueryLayerService;
 
@@ -47,13 +55,20 @@ public class XMLExportServiceTest {
     @Mock
     private SearchStateFactory searchStateFactory;
     @Mock
+    private SearchState searchState;
+    @Mock
     private SolrQueryLayerService queryLayer;
+    @Mock
+    BriefObjectMetadata bom;
+    @Mock
+    SearchResultResponse resultResponse;
     @Mock
     private XMLExportRequest request;
     @Mock
     private AccessGroupSet group;
     @Mock
     private XMLExportJob job;
+
 
     private String username;
     private List<String> pids;
@@ -74,6 +89,26 @@ public class XMLExportServiceTest {
     public void exportXMLTest() {
         when(request.exportChildren()).thenReturn(false);
         when(request.getPids()).thenReturn(pids);
+
+        Map<String,String> response = service.exportXml(username, group, request);
+
+        assertEquals("Metadata export for " + request.getPids().size()
+                + " objects has begun, you will receive the data via email soon", response.get("message"));
+    }
+
+    @Test
+    public void exportXMLWithChildrenTest() {
+        @SuppressWarnings("unchecked")
+        List<BriefObjectMetadata> objects = mock(List.class);
+
+        when(request.exportChildren()).thenReturn(false);
+        when(searchStateFactory.createSearchState()).thenReturn(searchState);
+        when(queryLayer.addSelectedContainer(anyString(), eq(searchState), any(Boolean.class))).thenReturn(bom);
+        when(queryLayer.getSearchResults(any(SearchRequest.class))).thenReturn(resultResponse);
+        when(resultResponse.getResultList()).thenReturn(objects);
+        when(request.getPids()).thenReturn(pids);
+
+        pids.add(PIDs.get(UUID.randomUUID().toString()).toString());
 
         Map<String,String> response = service.exportXml(username, group, request);
 
