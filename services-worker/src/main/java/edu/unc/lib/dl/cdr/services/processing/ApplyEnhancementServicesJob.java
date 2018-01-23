c0e51e5f48d2b46c9f3c0e08ddd920c7befd37a4
bbpennel <bbpennel@email.unc.edu>
2018-01-23 11:24:43 -0500

    Moved handling of enhance service retrying when fedora is not available down into applyService method

--- services-worker/src/main/java/edu/unc/lib/dl/cdr/services/processing/ApplyEnhancementServicesJob.java
+++ services-worker/src/main/java/edu/unc/lib/dl/cdr/services/processing/ApplyEnhancementServicesJob.java
@@ -4,6 +4,7 @@ import static edu.unc.lib.dl.util.JMSMessageUtil.servicesMessageNamespace;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,8 +60,6 @@ public class ApplyEnhancementServicesJob implements Runnable {
 
 	@Override
 	public void run() {
-		int backoffAttempts = 1;
-
 		for (ObjectEnhancementService service : services) {
 			if (message.getFilteredServices() != null
 					&& !message.getFilteredServices().contains(service.getClass().getName())) {
@@ -75,45 +74,31 @@ public class ApplyEnhancementServicesJob implements Runnable {
 				LOG.error("Error determining applicability for service " + service.getClass().getName() + " and object " + message.getTargetID(), e);
 			}
 
-			while (true) {
-				try {
-					if (fedoraManagementClient.isRepositoryAvailable()) {
-						applyService(service);
-						metricsClient.incrFinishedEnhancement(service.getClass().getName());
-						break;
-					} else {
-						throw new ServiceException(new WebServiceIOException("Unable to connect to Fedora"));
-					}
-				} catch (ServiceException e) {
-					if (e.getCause() instanceof WebServiceIOException) {
-						LOG.warn("Unable to connect to fedora. Unable to run job for " + service.getClass().getName()
-							+ ". Retry attempt " + backoffAttempts);
-						try {
-							Thread.sleep(BACKOFF_DELAY * backoffAttempts);
-						} catch (InterruptedException e1) {
-							LOG.warn("Back off time was interrupted for job " + service.getClass().getName());
-							return;
-						}
-						backoffAttempts++;
-					} else {
-						throw e;
-					}
-				} catch (EnhancementException e) {
-					LOG.error("Error applying service " + service.getClass().getName() + " to object " + message.getTargetID(), e);
-					metricsClient.incrFailedEnhancement(service.getClass().getName());
-				} catch (Throwable t) {
-					metricsClient.incrFailedEnhancement(service.getClass().getName());
-					throw t;
-				}
+			try {
+				applyService(service);
+				metricsClient.incrFinishedEnhancement(service.getClass().getName());
+			} catch (EnhancementException e) {
+				LOG.error("Error applying service " + service.getClass().getName() + " to object " + message.getTargetID(), e);
+				metricsClient.incrFailedEnhancement(service.getClass().getName());
+			} catch (Throwable t) {
+				metricsClient.incrFailedEnhancement(service.getClass().getName());
+				throw t;
 			}
 		}
 	}
 
 	private void applyService(ObjectEnhancementService service) throws EnhancementException {
+		int backoffAttempts = 1;
+		
 		while (true) {
 			LOG.info("Applying service {} to object {}", service.getClass().getName(), message.getTargetID());
 
+			long retryDelay;
 			try {
+				if (!fedoraManagementClient.isRepositoryAvailable()) {
+					throw new ServiceException(new WebServiceIOException("Unable to connect to Fedora"));
+				}
+				
 				service.getEnhancement(message).call();
 				message.getCompletedServices().add(service.getClass().getName());
 
@@ -121,17 +106,29 @@ public class ApplyEnhancementServicesJob implements Runnable {
 			} catch (EnhancementException e) {
 				if (e.getSeverity() == EnhancementException.Severity.RECOVERABLE) {
 					LOG.error("Retrying service for recoverable exception: " + service.getClass().getName(), e);
+					retryDelay = recoverableDelay;
+				} else {
+					throw e;
+				}
+			} catch (ServiceException e) {
+				if (e.getCause() instanceof WebServiceIOException) {
+					LOG.warn("Unable to connect to fedora. Unable to run job for " + service.getClass().getName()
+						+ ". Retry attempt " + backoffAttempts);
+					retryDelay = BACKOFF_DELAY * backoffAttempts;
+					backoffAttempts++;
 				} else {
 					throw e;
 				}
 			}
 
 			try {
-				Thread.sleep(recoverableDelay);
-			} catch (InterruptedException e) {
+				Thread.sleep(retryDelay);
+			} catch (InterruptedException e1) {
+				LOG.warn("Thread interrupted while waiting to reattempt job {}", service.getClass().getName());
 				Thread.currentThread().interrupt();
 				break;
 			}
+			
 		}
 	}
 
