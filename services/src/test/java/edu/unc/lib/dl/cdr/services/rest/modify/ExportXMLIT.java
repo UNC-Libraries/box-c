464baee0c26e416ba9ce849e55b294e3bd1b2d70
harringj <harring@email.unc.edu>
2018-01-26 16:49:54 -0500

    gets second test case passing in IT

--- services/src/test/java/edu/unc/lib/dl/cdr/services/rest/modify/ExportXMLIT.java
+++ services/src/test/java/edu/unc/lib/dl/cdr/services/rest/modify/ExportXMLIT.java
@@ -21,11 +21,14 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +42,10 @@ import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.web.servlet.MvcResult;
@@ -47,6 +53,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import edu.unc.lib.dl.acl.util.AccessGroupSet;
 import edu.unc.lib.dl.acl.util.GroupsThreadStore;
 import edu.unc.lib.dl.cdr.services.rest.modify.ExportXMLController.XMLExportRequest;
+import edu.unc.lib.dl.fcrepo4.ContentObject;
+import edu.unc.lib.dl.fcrepo4.RepositoryObjectFactory;
 import edu.unc.lib.dl.fedora.PID;
 import edu.unc.lib.persist.services.EmailHandler;
 
@@ -62,14 +70,28 @@ import edu.unc.lib.persist.services.EmailHandler;
 })
 public class ExportXMLIT extends AbstractAPIIT {
 
-    @Mock
+    @Autowired
+    private RepositoryObjectFactory repositoryObjectFactory;
+
+    @Autowired
     private EmailHandler emailHandler;
     @Mock
-    private MimeMessage mimeMessage = mock(MimeMessage.class);
+    private MimeMessage mimeMessage;
+
+    @Captor
+    private ArgumentCaptor<String> toCaptor;
+    @Captor
+    private ArgumentCaptor<String> subjectCaptor;
+    @Captor
+    private ArgumentCaptor<String> bodyCaptor;
+    @Captor
+    private ArgumentCaptor<String> filenameCaptor;
+    @Captor
+    private ArgumentCaptor<File> attachmentCaptor;
 
     @Before
     public void init_() throws Exception {
-
+        initMocks(this);
         when(aclService.hasAccess(any(PID.class), any(AccessGroupSet.class), eq(bulkUpdateDescription))).thenReturn(true);
     }
 
@@ -86,6 +108,10 @@ public class ExportXMLIT extends AbstractAPIIT {
                 .andReturn();
 
         // Verify response from api
+        verify(emailHandler).sendEmail(toCaptor.capture(), subjectCaptor.capture(), bodyCaptor.capture(),
+                filenameCaptor.capture(), attachmentCaptor.capture());
+        assertEquals("The XML metadata for 2 object(s) requested for export by test_user is attached.\n",
+                bodyCaptor.getValue());
         Map<String, Object> respMap = getMapFromResponse(result);
         assertEquals("export xml", respMap.get("action"));
     }
@@ -110,8 +136,13 @@ public class ExportXMLIT extends AbstractAPIIT {
     }
 
     private String makeJSON(boolean exportChildren) throws JsonGenerationException, JsonMappingException, IOException {
-        String pid1 = makePid().getRepositoryPath();
-        String pid2 = makePid().getRepositoryPath();
+        ContentObject folder = repositoryObjectFactory.createFolderObject(null);
+        ContentObject work = repositoryObjectFactory.createWorkObject(null);
+        folder.addDescription(new FileInputStream(new File("src/test/resources/mods", "valid-mods.xml")));
+        work.addDescription(new FileInputStream(new File("src/test/resources/mods", "valid-mods2.xml")));
+
+        String pid1 = folder.getPid().getRepositoryPath();
+        String pid2 = work.getPid().getRepositoryPath();
         List<String> pids = new ArrayList<>();
         pids.add(pid1);
         pids.add(pid2);
