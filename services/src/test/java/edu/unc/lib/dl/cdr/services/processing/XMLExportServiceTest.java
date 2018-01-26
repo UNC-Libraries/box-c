59afb9cc81781846f716fdde1333d1a57364ca0f
bbpennel <bbpennel@email.unc.edu>
2018-01-26 13:11:36 -0500

    Added additional unit tests for export service

--- services/src/test/java/edu/unc/lib/dl/cdr/services/processing/XMLExportServiceTest.java
+++ services/src/test/java/edu/unc/lib/dl/cdr/services/processing/XMLExportServiceTest.java
@@ -16,19 +16,22 @@
 package edu.unc.lib.dl.cdr.services.processing;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -47,10 +50,12 @@ import edu.unc.lib.dl.acl.service.AccessControlService;
 import edu.unc.lib.dl.acl.util.AccessGroupSet;
 import edu.unc.lib.dl.acl.util.Permission;
 import edu.unc.lib.dl.cdr.services.rest.modify.ExportXMLController.XMLExportRequest;
+import edu.unc.lib.dl.fcrepo4.BinaryObject;
 import edu.unc.lib.dl.fcrepo4.ContentObject;
 import edu.unc.lib.dl.fcrepo4.PIDs;
 import edu.unc.lib.dl.fcrepo4.RepositoryObjectLoader;
 import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.fedora.ServiceException;
 import edu.unc.lib.dl.search.solr.model.BriefObjectMetadata;
 import edu.unc.lib.dl.search.solr.model.SearchRequest;
 import edu.unc.lib.dl.search.solr.model.SearchResultResponse;
@@ -58,11 +63,13 @@ import edu.unc.lib.dl.search.solr.model.SearchState;
 import edu.unc.lib.dl.search.solr.service.SearchStateFactory;
 import edu.unc.lib.dl.ui.service.SolrQueryLayerService;
 import edu.unc.lib.dl.util.ZipFileUtil;
+import edu.unc.lib.dl.xml.JDOMNamespaceUtil;
 import edu.unc.lib.persist.services.EmailHandler;
 
 /**
  *
  * @author harring
+ * @author bbpennel
  *
  */
 public class XMLExportServiceTest {
@@ -83,7 +90,6 @@ public class XMLExportServiceTest {
     private SolrQueryLayerService queryLayer;
     @Mock
     SearchResultResponse resultResponse;
-    @Mock
     private XMLExportRequest request;
     @Mock
     private AccessGroupSet group;
@@ -103,7 +109,6 @@ public class XMLExportServiceTest {
 
         username = "user";
         pids = new ArrayList<>();
-        when(request.getPids()).thenReturn(pids);
 
         service = new XMLExportService();
         service.setAclService(aclService);
@@ -114,14 +119,21 @@ public class XMLExportServiceTest {
 
         when(aclService.hasAccess(any(PID.class), any(AccessGroupSet.class), eq(Permission.bulkUpdateDescription)))
                 .thenReturn(true);
-        when(request.getEmail()).thenReturn(TO_EMAIL);
 
         when(searchStateFactory.createSearchState()).thenReturn(searchState);
+
+        request = new XMLExportRequest(pids, false, TO_EMAIL);
     }
 
     private PID registerObject() {
-        PID pid = PIDs.get(UUID.randomUUID().toString());
+        PID pid = mockObject();
         pids.add(pid.getRepositoryPath());
+
+        return pid;
+    }
+
+    private PID mockObject() {
+        PID pid = PIDs.get(UUID.randomUUID().toString());
 
         ContentObject obj = mock(ContentObject.class);
         when(obj.getPid()).thenReturn(pid);
@@ -133,15 +145,10 @@ public class XMLExportServiceTest {
 
     @Test
     public void exportXMLTest() throws Exception {
-        when(request.getExportChildren()).thenReturn(false);
-
         PID pid1 = registerObject();
         PID pid2 = registerObject();
 
-        Map<String,String> response = service.exportXml(username, group, request);
-
-        assertEquals("Metadata export for " + request.getPids().size()
-                + " objects has begun, you will receive the data via email soon", response.get("message"));
+        assertSuccess(request);
 
         Document doc = getExportedDocument();
 
@@ -154,10 +161,10 @@ public class XMLExportServiceTest {
 
     @Test
     public void exportXMLWithChildrenTest() throws Exception {
-        when(request.getExportChildren()).thenReturn(true);
+        request.setExportChildren(true);
 
-        PID pid1 = registerObject();
-        PID pid2 = registerObject();
+        PID pid1 = mockObject();
+        PID pid2 = mockObject();
         PID parentPid = registerObject();
 
         List<BriefObjectMetadata> childrenMd = new ArrayList<>();
@@ -166,14 +173,7 @@ public class XMLExportServiceTest {
         when(resultResponse.getResultList()).thenReturn(childrenMd);
         when(queryLayer.getSearchResults(any(SearchRequest.class))).thenReturn(resultResponse);
 
-        BriefObjectMetadata parentMd = mockMd(parentPid);
-        when(queryLayer.addSelectedContainer(anyString(), eq(searchState), any(Boolean.class)))
-                .thenReturn(parentMd);
-
-        Map<String,String> response = service.exportXml(username, group, request);
-
-        assertEquals("Metadata export for " + request.getPids().size()
-                + " objects has begun, you will receive the data via email soon", response.get("message"));
+        assertSuccess(request);
 
         Document doc = getExportedDocument();
 
@@ -187,25 +187,139 @@ public class XMLExportServiceTest {
 
     @Test
     public void exportWithModsTest() throws Exception {
+        PID pid1 = registerObject();
+        ContentObject contentObj = (ContentObject) repoObjLoader.getRepositoryObject(pid1);
 
+        BinaryObject modsObj = mock(BinaryObject.class);
+        InputStream modsIs = new FileInputStream(new File(
+                "src/test/resources/mods/valid-mods.xml"));
+        when(modsObj.getBinaryStream()).thenReturn(modsIs);
+        when(contentObj.getMODS()).thenReturn(modsObj);
+
+        assertSuccess(request);
+
+        Document doc = getExportedDocument();
+        Element rootEl = doc.getRootElement();
+        assertEquals("bulkMetadata", rootEl.getName());
+
+        Element objEl = rootEl.getChild("object");
+
+        assertEquals(pid1.getRepositoryPath(), objEl.getAttributeValue("pid"));
+
+        Element updateEl = objEl.getChild("update");
+        assertEquals("MODS", updateEl.getAttributeValue("type"));
+        assertNotNull(updateEl.getAttributeValue("lastModified"));
+
+        Element modsEl = updateEl.getChild("mods", JDOMNamespaceUtil.MODS_V3_NS);
+        assertNotNull(modsEl);
+        assertNotNull(modsEl.getChild("titleInfo", JDOMNamespaceUtil.MODS_V3_NS));
+    }
+
+    @Test
+    public void exportNoPermissionTest() throws Exception {
+        PID pid1 = registerObject();
+        PID pid2 = registerObject();
+
+        when(aclService.hasAccess(eq(pid1), any(AccessGroupSet.class), eq(Permission.bulkUpdateDescription)))
+                .thenReturn(true);
+        when(aclService.hasAccess(eq(pid2), any(AccessGroupSet.class), eq(Permission.bulkUpdateDescription)))
+                .thenReturn(false);
+        assertSuccess(request);
+
+        Document doc = getExportedDocument();
+
+        Element rootEl = doc.getRootElement();
+        assertHasObjectWithoutMods(rootEl, pid1);
+        assertNull("Object must not be present due to permissions", getObjectElByPid(rootEl, pid2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void exportNoObjectsTest() throws Exception {
+        // Register no objects for export
+
+        service.exportXml(username, group, request);
+    }
+
+    @Test
+    public void exportNoChildrenTest() throws Exception {
+        request.setExportChildren(true);
+
+        PID parentPid = registerObject();
+
+        when(resultResponse.getResultList()).thenReturn(Collections.emptyList());
+        when(queryLayer.getSearchResults(any(SearchRequest.class))).thenReturn(resultResponse);
+
+        assertSuccess(request);
+
+        Document doc = getExportedDocument();
+
+        Element rootEl = doc.getRootElement();
+        assertEquals("bulkMetadata", rootEl.getName());
+
+        assertHasObjectWithoutMods(rootEl, parentPid);
+    }
+
+    @Test(expected = ServiceException.class)
+    public void exportFailToGetChildrenTest() throws Exception {
+        request.setExportChildren(true);
+
+        // Register a parent and child object
+        registerObject();
+        mockObject();
+
+        when(queryLayer.getSearchResults(any(SearchRequest.class))).thenReturn(null);
+
+        service.exportXml(username, group, request);
+    }
+
+    @Test
+    public void asynchronousExportTest() throws Exception {
+        service.setAsynchronous(true);
+
+        PID pid1 = registerObject();
+
+        assertSuccess(request);
+
+        verify(emailHandler, timeout(10000)).sendEmail(eq(TO_EMAIL), anyString(), anyString(),
+                eq("xml_export.zip"), fileCaptor.capture());
+        Document doc = getExportedDocument(fileCaptor.getValue());
+
+        Element rootEl = doc.getRootElement();
+        assertEquals("bulkMetadata", rootEl.getName());
+
+        assertHasObjectWithoutMods(rootEl, pid1);
     }
 
     private void assertHasObjectWithoutMods(Element rootEl, PID pid) {
+        Element objEl = getObjectElByPid(rootEl, pid);
+        assertNotNull("Did not contain expected child object " + pid, objEl);
+        assertNull(objEl.getChild("update"));
+    }
+
+    private Element getObjectElByPid(Element rootEl, PID pid) {
         List<Element> objEls = rootEl.getChildren("object");
         for (Element objEl : objEls) {
             String pidAttr = objEl.getAttributeValue("pid");
             if (pid.getRepositoryPath().equals(pidAttr)) {
-                assertNull(objEl.getChild("update"));
-                return;
+                return objEl;
             }
         }
-        fail("Did not contain expected child object " + pid);
+        return null;
+    }
+
+    private void assertSuccess(XMLExportRequest request) throws Exception {
+        Map<String,String> response = service.exportXml(username, group, request);
+        assertEquals("Metadata export for " + request.getPids().size()
+                + " objects has begun, you will receive the data via email soon", response.get("message"));
     }
 
     private Document getExportedDocument() throws Exception {
         verify(emailHandler).sendEmail(eq(TO_EMAIL), anyString(), anyString(),
                 eq("xml_export.zip"), fileCaptor.capture());
-        File reportZip = fileCaptor.getValue();
+        return getExportedDocument(fileCaptor.getValue());
+    }
+
+    private Document getExportedDocument(File reportZip) throws Exception {
         File unzipDir = ZipFileUtil.unzipToTemp(reportZip);
         File reportFile = new File(unzipDir, "export.xml");
 
