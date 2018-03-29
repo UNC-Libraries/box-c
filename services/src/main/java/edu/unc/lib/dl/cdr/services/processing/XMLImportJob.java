0e40a0bb4486408762959b59a3665b49fb0c0648
harringj <harring@email.unc.edu>
2018-03-29 11:00:00 -0400

    adds service to webapp's context and fixes minor spacing issue with the job's license header

--- services/src/main/java/edu/unc/lib/dl/cdr/services/processing/XMLImportJob.java
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/processing/XMLImportJob.java
@@ -12,7 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package edu.unc.lib.dl.cdr.services.processing;
+ */
+package edu.unc.lib.dl.cdr.services.processing;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
