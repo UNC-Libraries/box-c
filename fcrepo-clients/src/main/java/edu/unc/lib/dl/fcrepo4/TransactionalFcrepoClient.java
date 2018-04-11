47b09f2fa1b2a2a67dd90d76df2eff0c795a530c
bbpennel <bbpennel@email.unc.edu>
2018-04-11 16:26:13 -0400

    Using constant for rest path component length

--- fcrepo-clients/src/main/java/edu/unc/lib/dl/fcrepo4/TransactionalFcrepoClient.java
+++ fcrepo-clients/src/main/java/edu/unc/lib/dl/fcrepo4/TransactionalFcrepoClient.java
@@ -48,6 +48,7 @@ import edu.unc.lib.dl.util.URIUtil;
 public class TransactionalFcrepoClient extends FcrepoClient {
 
     private static final String REST = "rest/";
+    private static final int REST_LENGTH = 4;
     private static final List<String> RDF_MIMETYPES = Arrays.asList(new String[] {
             "application/sparql-update", "text/turtle", "text/rdf+n3", "application/n3",
             "text/n3", "application/rdf+xml", "application/n-triples", "application/ld+json"});
@@ -132,7 +133,7 @@ public class TransactionalFcrepoClient extends FcrepoClient {
             if (index == -1) {
                 throw new IllegalArgumentException("Requested URI not within allowed domain");
             }
-            requestUri = URI.create(URIUtil.join(baseUri, uriString.substring(index + 4)));
+            requestUri = URI.create(URIUtil.join(baseUri, uriString.substring(index + REST_LENGTH)));
         }
         return requestUri;
     }
