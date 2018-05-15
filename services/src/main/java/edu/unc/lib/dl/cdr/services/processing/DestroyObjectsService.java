dd2cef72a0d5610a2ad51580f269b3a95bc73cec
harringj <harring@email.unc.edu>
2018-05-15 16:53:18 -0400

    initial commit for destroy-objects action
    - includes new classes for controller, service, and job

---
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/processing/DestroyObjectsService.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.cdr.services.processing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.unc.lib.dl.acl.service.AccessControlService;
+import edu.unc.lib.dl.acl.util.AgentPrincipals;
+import edu.unc.lib.dl.acl.util.Permission;
+import edu.unc.lib.dl.fcrepo4.PIDs;
+import edu.unc.lib.dl.fedora.PID;
+
+/**
+ *
+ * @author harring
+ *
+ */
+public class DestroyObjectsService {
+
+    private static final Logger log = LoggerFactory.getLogger(DestroyObjectsService.class);
+
+    private AccessControlService aclService;
+
+    /**
+     *
+     * @param agent security principals of the agent making request
+     * @param ids list of objects to destroy
+     */
+    public void destroyObjects(AgentPrincipals agent, List<String> ids) {
+        List<PID> objsToDestroy = new ArrayList<>();
+
+        for (String id : ids) {
+            PID pid = PIDs.get(id);
+            aclService.assertHasAccess("User does not have permission to destroy this object", pid,
+                    agent.getPrincipals(), Permission.destroy);
+            objsToDestroy.add(pid);
+        }
+        if (!objsToDestroy.isEmpty()) {
+            DestroyObjectsJob job = new DestroyObjectsJob(objsToDestroy);
+            log.info("Starting destroy job for {} objects", objsToDestroy.size());
+            job.run();
+        }
+    }
+
+}
---
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/rest/modify/DestroyObjectsController.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.cdr.services.rest.modify;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.unc.lib.dl.acl.util.AgentPrincipals;
+import edu.unc.lib.dl.cdr.services.processing.DestroyObjectsService;
+
+/**
+ * API controller for destroying objects
+ *
+ * @author harring
+ *
+ */
+@Controller
+public class DestroyObjectsController {
+    private static final Logger log = LoggerFactory.getLogger(DestroyObjectsController.class);
+
+    @Autowired
+    private DestroyObjectsService service;
+
+    @RequestMapping(value = "edit/destroy/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Object> destroyObjects(@PathVariable("id") List<String> ids) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("object ids", ids.toString());
+        result.put("action", "destroy");
+
+        service.destroyObjects(AgentPrincipals.createFromThread(), ids);
+
+        result.put("timestamp", System.currentTimeMillis());
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+
+
+}
