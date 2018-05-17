65433283dac6d2e4a23c93bbb60722ff8c399660
harringj <harring@email.unc.edu>
2018-05-17 10:50:07 -0400

    moves service and job for destroy action to its own package in persistence module

--- persistence/src/main/java/edu/unc/lib/dl/persist/services/destroy/DestroyObjectsService.java
+++ persistence/src/main/java/edu/unc/lib/dl/persist/services/destroy/DestroyObjectsService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.unc.lib.dl.cdr.services.processing;
+package edu.unc.lib.dl.persist.services.destroy;
 
 import java.util.ArrayList;
 import java.util.List;
