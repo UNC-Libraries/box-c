59675448ece399acf13a5422c24be6ebd62485c7
harringj <harring@email.unc.edu>
2018-05-24 10:37:53 -0400

    finishes building out DestroyObjectsJob

--- persistence/src/main/java/edu/unc/lib/dl/persist/services/destroy/DestroyObjectsJob.java
+++ persistence/src/main/java/edu/unc/lib/dl/persist/services/destroy/DestroyObjectsJob.java
@@ -18,14 +18,31 @@ package edu.unc.lib.dl.persist.services.destroy;
 import java.util.List;
 
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Selector;
+import org.apache.jena.rdf.model.SimpleSelector;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.vocabulary.RDF;
 
 import edu.unc.lib.dl.acl.util.AgentPrincipals;
+import edu.unc.lib.dl.fcrepo4.BinaryObject;
+import edu.unc.lib.dl.fcrepo4.ContentContainerObject;
+import edu.unc.lib.dl.fcrepo4.ContentObject;
+import edu.unc.lib.dl.fcrepo4.FedoraTransaction;
+import edu.unc.lib.dl.fcrepo4.FileObject;
+import edu.unc.lib.dl.fcrepo4.RepositoryObject;
 import edu.unc.lib.dl.fcrepo4.RepositoryObjectFactory;
-import edu.unc.lib.dl.fcrepo4.Tombstone;
+import edu.unc.lib.dl.fcrepo4.RepositoryObjectLoader;
+import edu.unc.lib.dl.fcrepo4.TransactionManager;
+import edu.unc.lib.dl.fedora.FedoraException;
 import edu.unc.lib.dl.fedora.PID;
 import edu.unc.lib.dl.metrics.TimerFactory;
+import edu.unc.lib.dl.rdf.Cdr;
+import edu.unc.lib.dl.rdf.DcElements;
+import edu.unc.lib.dl.rdf.Ebucore;
 import edu.unc.lib.dl.rdf.Premis;
+import edu.unc.lib.dl.search.solr.service.ObjectPathFactory;
 import io.dropwizard.metrics5.Timer;
 
 /**
@@ -39,6 +56,10 @@ public class DestroyObjectsJob implements Runnable {
     private List<PID> objsToDestroy;
     private AgentPrincipals agent;
     private RepositoryObjectFactory repoObjFactory;
+    private RepositoryObjectLoader repoObjLoader;
+    private TransactionManager txManager;
+    private DestroyProxyService proxyService;
+    private ObjectPathFactory pathFactory;
 
     public DestroyObjectsJob(AgentPrincipals agent, List<PID> objsToDestroy) {
         this.objsToDestroy = objsToDestroy;
@@ -46,26 +67,88 @@ public class DestroyObjectsJob implements Runnable {
 
     @Override
     public void run() {
+        FedoraTransaction tx = txManager.startTransaction();
         try (Timer.Context context = timer.time()) {
-            // create tombstone for each destroyed obj
-            Tombstone tStone = null;
+            // convert each destroyed obj to a tombstone
             for (PID pid : objsToDestroy) {
-                Model model = ModelFactory.createDefaultModel();
-                tStone = repoObjFactory.createTombstone(pid, model);
-
-                //TODO: remove containment relation from obj's parent
-
-                //add premis event to tombstone
-                tStone.getPremisLog().buildEvent(Premis.Deletion)
-                .addImplementorAgent(agent.getUsernameUri())
-                .addEventDetail("Item deleted from repository and replaced by this tombstone")
-                .write();
-
-                tStone.getRecord().put("PREMIS log", tStone.getPremisLog().getEvents().toString());
-            }
+                // remove containment relation from obj's parent
+                proxyService.destroyProxy(pid);
+                RepositoryObject repoObj = repoObjLoader.getRepositoryObject(pid);
+                // purge tree with repoObj as root from repository
+                destroyTree(repoObj);
+           }
+        } catch (Exception e) {
+             tx.cancel(e);
+        } finally {
+             tx.close();
         }
     }
 
+    private void destroyTree(RepositoryObject rootOfTree) throws FedoraException {
+        if (rootOfTree instanceof ContentContainerObject) {
+            ContentContainerObject container = (ContentContainerObject) rootOfTree;
+            List<ContentObject> members = container.getMembers();
+            for (ContentObject member : members) {
+                destroyTree(member);
+            }
+        } else if (rootOfTree instanceof FileObject) {
+            FileObject file = (FileObject) rootOfTree;
+            List<BinaryObject> binaries = file.getBinaryObjects();
+            for (BinaryObject binary : binaries) {
+                destroyTree(binary);
+            }
+        }
+        // destroy root of tree
+        Model stoneModel = rootOfTree.getModel();
+        stoneModel = convertModelToTombstone(rootOfTree);
+        repoObjFactory.createObject(rootOfTree.getUri(), stoneModel);
 
+        //add premis event to tombstone
+        rootOfTree.getPremisLog().buildEvent(Premis.Deletion)
+        .addImplementorAgent(agent.getUsernameUri())
+        .addEventDetail("Item deleted from repository and replaced by tombstone")
+        .write();
 
+        // persist model
+        repoObjFactory.createObject(rootOfTree.getUri(), stoneModel);
+    }
+
+    private Model convertModelToTombstone(RepositoryObject destroyedObj) {
+        Model model = destroyedObj.getModel();
+        Resource resc = destroyedObj.getResource();
+
+        if (destroyedObj instanceof BinaryObject) {
+            // whitelisted properties for tombstone model of binaries
+            Selector selectEbucoreFilename = new SimpleSelector(resc, Ebucore.filename, (Object) null);
+            Selector selectDcFilename = new SimpleSelector(resc, DcElements.title, (Object) null);
+            Selector selectPremisOriginalName = new SimpleSelector(resc, Premis.hasOriginalName, (Object) null);
+            Selector selectMimetype = new SimpleSelector(resc, Ebucore.hasMimeType, (Object) null);
+            Selector selectChecksum= new SimpleSelector(resc, Premis.hasMessageDigest, (Object) null);
+            Selector selectFilesize = new SimpleSelector(resc, Premis.hasSize, (Long) null);
+
+            StmtIterator iter = model.listStatements();
+            while (iter.hasNext()) {
+                Statement s = iter.nextStatement();
+                if (!(selectEbucoreFilename.test(s) || selectDcFilename.test(s) || selectPremisOriginalName.test(s)
+                        || selectMimetype.test(s) || selectChecksum.test(s) || selectFilesize.test(s))) {
+                    model.remove(s);
+                }
+            }
+        } else {
+            // retain only the object-type of a non-binary object
+            Selector selectRdfType = new SimpleSelector(resc, RDF.type, (Object) null);
+            StmtIterator iter = model.listStatements();
+            while (iter.hasNext()) {
+                Statement s = iter.nextStatement();
+                if (!selectRdfType.test(s)) {
+                    model.remove(s);
+                }
+            }
+        }
+        // determine path and store in tombstone model
+        String path = pathFactory.getPath(destroyedObj.getPid()).toNamePath();
+        model.add(resc, Cdr.historicalPath, path);
+        model.add(resc, RDF.type, Cdr.Tombstone);
+        return model;
+    }
 }
