8ad6f0675ca728262ea3c445ef164f75603b9280
lfarrell <lfarrell@email.unc.edu>
2018-11-21 14:02:30 -0500

    Style fixes

--- access/src/main/java/edu/unc/lib/dl/ui/controller/AboutRepositoryController.java
+++ access/src/main/java/edu/unc/lib/dl/ui/controller/AboutRepositoryController.java
@@ -19,6 +19,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * Handles requests to the about the repository page
+ * @author lfarrell
+ */
 @Controller
 @RequestMapping("/aboutRepository")
 public class AboutRepositoryController {
