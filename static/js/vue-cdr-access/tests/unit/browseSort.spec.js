c0145dc44b54faa6fe8db8fa5aa6a09e6f6920e7
lfarrell <lfarrell@email.unc.edu>
2019-06-14 14:58:09 -0400

    Add test for sorting and fix setting query parameters bug

--- static/js/vue-cdr-access/tests/unit/browseSort.spec.js
+++ static/js/vue-cdr-access/tests/unit/browseSort.spec.js
@@ -1,17 +1,25 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
-import VueRouter from 'vue-router'
-import browseSort from '@/components/browseSort.vue'
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import VueRouter from 'vue-router';
+import browseSort from '@/components/browseSort.vue';
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
-const router = new VueRouter();
-let wrapper;
+const router = new VueRouter({
+    routes: [
+        {
+            path: '/record/uuid1234',
+            name: 'browseDisplay'
+        }
+    ]
+});
 
-describe('browseSearch.vue', () => {
-    beforeEach(() => {
-        wrapper = shallowMount(browseSort, {
+describe('browseSort.vue', () => {
+    it("updates the url when the dropdown changes", () => {
+        let wrapper = shallowMount(browseSort, {
             localVue,
             router
         });
+        wrapper.findAll('option').at(2).setSelected();
+        expect(wrapper.vm.$router.currentRoute.query.sort).toEqual('title,reverse');
     });
 });
