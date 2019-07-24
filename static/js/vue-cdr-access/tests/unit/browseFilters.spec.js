8876843408114ba0b647433a0756dba3d1d1461c
lfarrell <lfarrell@email.unc.edu>
2019-07-24 11:40:37 -0400

    Update filter tests

--- static/js/vue-cdr-access/tests/unit/browseFilters.spec.js
+++ static/js/vue-cdr-access/tests/unit/browseFilters.spec.js
@@ -22,12 +22,13 @@ describe('browseFilters.vue', () => {
             localVue,
             router,
             propsData: {
-                container_type: 'Collection'
+                browseType: 'gallery-display',
+                containerType: 'Collection'
             }
         });
 
         wrapper.setData({
-            images_only: false,
+            filtered: false,
             update_params: {}
         });
 
@@ -38,31 +39,89 @@ describe('browseFilters.vue', () => {
         record.trigger('change');
     });
 
-    it("selects only images when the checkbox is checked", () => {
-        expect(wrapper.vm.images_only).toBe(true);
+    it("does not display a checkbox for non-collections", () => {
+        wrapper.setProps({containerType: 'AdminUnit' });
+        expect(wrapper.find('.filter-format').contains('.checkbox')).toBe(false);
+    });
+
+    it("updates checkbox text when browse type changes", () => {
+        let record = wrapper.find('.filter-format');
+        expect(record.text()).toBe('Show images only?');
+
+        wrapper.setProps({browseType: 'structure-display' });
+        expect(record.text()).toBe('Show folders only?');
+    });
+
+    it("resets the filter parameters to be unchecked when browse type changes", () => {
+        expect(wrapper.vm.browse_type_updated).toBe(false);
         expect(wrapper.vm.$router.currentRoute.query).toEqual({
             format: "image",
             rows: 20,
             sort: "title,normal",
-            start: 0
+            start: 0,
+            types: "Work"
+        });
+
+        wrapper.setProps({browseType: 'structure-display' });
+        expect(wrapper.vm.$router.currentRoute.query).toEqual({
+            rows: 20,
+            sort: "title,normal",
+            start: 0,
+            types: "Work"
+        });
+        // Make sure field is reset to false
+        expect(wrapper.vm.browse_type_updated).toBe(false);
+    });
+
+    it("selects only images when the checkbox is checked in gallery browse mode", () => {
+        expect(wrapper.vm.filtered).toBe(true);
+        expect(wrapper.vm.$router.currentRoute.query).toEqual({
+            format: "image",
+            rows: 20,
+            sort: "title,normal",
+            start: 0,
+            types: "Work"
         });
     });
 
-    it("selects all records when the checkbox is unchecked", () => {
+    it("selects all gallery browse records when the checkbox is unchecked", () => {
         // Uncheck the checkbox
         record.trigger('click');
         record.trigger('change');
 
-        expect(wrapper.vm.images_only).toBe(false);
+        expect(wrapper.vm.filtered).toBe(false);
         expect(wrapper.vm.$router.currentRoute.query).toEqual({
             rows: 20,
             sort: "title,normal",
-            start: 0
+            start: 0,
+            types: "Work"
         });
     });
 
-    it("does not display a checkbox for non-collections", () => {
-        wrapper.setProps({container_type: 'AdminUnit' });
-        expect(wrapper.find('.imgs-only').contains('.checkbox')).toBe(false);
+    it("selects only folders when the checkbox is checked in structures browse mode", () => {
+        wrapper.setProps({browseType: 'structure-display' });
+        // Check the checkbox
+        record.trigger('click');
+        record.trigger('change');
+
+        expect(wrapper.vm.filtered).toBe(true);
+        expect(wrapper.vm.$router.currentRoute.query).toEqual({
+            rows: 20,
+            sort: "title,normal",
+            start: 0,
+            types: "Work,Folder"
+        });
+    });
+
+    it("selects all structure browse records when the checkbox is unchecked in structures browse mode", () => {
+        wrapper.setProps({browseType: 'structure-display' });
+
+        expect(wrapper.vm.filtered).toBe(false);
+        expect(wrapper.vm.$router.currentRoute.query).toEqual({
+            rows: 20,
+            sort: "title,normal",
+            start: 0,
+            types: "Work"
+        });
     });
 });
