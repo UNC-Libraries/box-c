a6cdc8e8e4a62f80efdf7f4fbb8c3cecc42a022e
bbpennel <bbpennel@email.unc.edu>
2019-08-27 09:51:47 -0400

    Move ingest source manager to persistence module

--- persistence/src/main/java/edu/unc/lib/dl/persist/services/ingest/IngestSourceManager.java
+++ persistence/src/main/java/edu/unc/lib/dl/persist/services/ingest/IngestSourceManager.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.unc.lib.dl.admin.collect;
+package edu.unc.lib.dl.persist.services.ingest;
 
 import java.io.File;
 import java.io.IOException;
