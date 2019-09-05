3caff54d6efdfd9995dcde0e1cb05bbcdcaa88f8
lfarrell <lfarrell@email.unc.edu>
2019-09-05 12:00:20 -0400

    Update tests so they don't always pass regardless of data. Using moxios without passing in 'done' results in this issue.

--- static/js/vue-cdr-access/tests/unit/modalMetadata.spec.js
+++ static/js/vue-cdr-access/tests/unit/modalMetadata.spec.js
@@ -9,16 +9,12 @@ localVue.use(VueRouter);
 
 const updated_uuid = 'c03a4bd7-25f4-4a6c-a68d-fedc4251b680';
 const title = 'Test Collection';
-const response = `<table><tbody><tr><th>Creator</th>
-    <td><p>Real Dean</p></td></tr></tbody>
-    </table>`;
+const response = `<table><tbody><tr><th>Creator</th> <td><p>Real Dean</p></td></tr></tbody></table>`;
 let wrapper;
 let btn;
 
 describe('modalMetadata.vue', () => {
     beforeEach(() => {
-        moxios.install();
-
         wrapper = shallowMount(modalMetadata, {
             localVue,
             router,
@@ -29,46 +25,47 @@ describe('modalMetadata.vue', () => {
         });
 
         btn = wrapper.find('#show-modal');
+    });
+
+    it("fetches the record metadata when the metadata button is clicked", () => {
+        moxios.install();
         moxios.stubRequest(`record/${updated_uuid}/metadataView`, {
             status: 200,
             responseText: {
                 data: response
             }
         });
-    });
 
-    it("fetches the record metadata when the metadata button is clicked", () => {
         btn.trigger('click');
 
         moxios.wait(() => {
             expect(wrapper.vm.metadata).toEqual(response);
             done();
         });
+
+        moxios.uninstall();
     });
 
     it("is hidden by default", () => {
-        expect(wrapper.find('.meta-modal').contains('.modal-body')).toBe(false);
+        expect(wrapper.find('.meta-modal').contains('.modal-container')).toBe(false);
     });
 
     it("displays a record title when triggered", () => {
-        btn.trigger('click');
-
-        moxios.wait(() => {
-            const record = wrapper.find('h3');
-            expect(record.text()).toBe(title);
+        wrapper.setData({
+            showModal: true
         });
+
+        const record = wrapper.find('h3');
+        expect(record.text()).toBe(title);
     });
 
     it("displays metadata when triggered", () => {
-        btn.trigger('click');
-
-        moxios.wait(() => {
-            const record = wrapper.find('table');
-            expect(record.html()).toBe(wrapper.vm.metadata);
+        wrapper.setData({
+            metadata: response,
+            showModal: true
         });
-    });
 
-    afterEach(() => {
-        moxios.uninstall();
-    })
+        const record = wrapper.find('table');
+        expect(record.html()).toBe(wrapper.vm.metadata);
+    });
 });
