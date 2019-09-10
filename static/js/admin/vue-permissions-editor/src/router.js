962896d9264a26f5e94ae297cec1af045c41b9a0
lfarrell <lfarrell@email.unc.edu>
2019-09-10 12:01:28 -0400

    Make uuid in url optional.

--- static/js/admin/vue-permissions-editor/src/router.js
+++ static/js/admin/vue-permissions-editor/src/router.js
@@ -9,7 +9,7 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/admin/list/:uuid',
+      path: '/admin/list/:uuid?',
       name: 'modalEditor',
       component:  modalEditor
     }
