36d7ea3d3040a778f51bca914c4e08a976c581bf
smithjp <smithj15@email.unc.edu>
2019-12-06 16:00:02 -0500

    title edit (#865)
    * Add EditTitleService file for adding and updating titles in mods records

---
+++ persistence/src/main/java/edu/unc/lib/dl/persist/services/edit/EditTitleService.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.persist.services.edit;
+
+import edu.unc.lib.dl.acl.service.AccessControlService;
+import edu.unc.lib.dl.acl.util.AgentPrincipals;
+import edu.unc.lib.dl.acl.util.Permission;
+import edu.unc.lib.dl.fcrepo4.BinaryObject;
+import edu.unc.lib.dl.fcrepo4.ContentObject;
+import edu.unc.lib.dl.fcrepo4.RepositoryObjectLoader;
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.fedora.ServiceException;
+import edu.unc.lib.dl.metrics.TimerFactory;
+import edu.unc.lib.dl.services.OperationsMessageSender;
+import edu.unc.lib.dl.validation.MODSValidator;
+import io.dropwizard.metrics5.Timer;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.input.sax.XMLReaderSAX2Factory;
+import org.jdom2.output.XMLOutputter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import static edu.unc.lib.dl.xml.JDOMNamespaceUtil.MODS_V3_NS;
+
+/**
+ * Service that manages editing of the mods:title property on an object
+ *
+ * @author smithjp
+ *
+ */
+public class EditTitleService {
+
+    private AccessControlService aclService;
+    private MODSValidator modsValidator;
+    private OperationsMessageSender operationsMessageSender;
+    private RepositoryObjectLoader repoObjLoader;
+
+    private static final Timer timer = TimerFactory.createTimerForClass(EditTitleService.class);
+
+    public EditTitleService() {
+    }
+
+    /**
+     * Changes an object's mods:title
+     *
+     * @param agent security principals of the agent making request
+     * @param pid the pid of the object whose label is to be changed
+     * @param title the new label (dc:title) of the given object
+     */
+    public void editTitle(AgentPrincipals agent, PID pid, String title) {
+        try (Timer.Context context = timer.time()) {
+
+            aclService.assertHasAccess(
+                    "User does not have permissions to edit titles",
+                    pid, agent.getPrincipals(), Permission.editDescription);
+
+            ContentObject obj = (ContentObject) repoObjLoader.getRepositoryObject(pid);
+            BinaryObject mods = obj.getDescription();
+
+            Document newMods;
+            InputStream modsStream;
+            if (mods != null) {
+                modsStream = mods.getBinaryStream();
+            } else {
+                modsStream = null;
+            }
+
+            if (modsStream != null) {
+                SAXBuilder sb = new SAXBuilder(new XMLReaderSAX2Factory(false));
+                Document document;
+                try {
+                    document = sb.build(modsStream);
+                } catch (IOException | JDOMException e) {
+                    throw new ServiceException("Unable to build mods document for " + pid, e);
+                }
+                Element rootEl = document.getRootElement();
+
+                if (hasExistingTitle(rootEl)) {
+                    newMods = updateTitle(document, title);
+                } else {
+                    newMods = addTitleToMODS(document, title);
+                }
+            } else {
+                Document document = new Document();
+                document.addContent(new Element("mods", MODS_V3_NS));
+                newMods = addTitleToMODS(document, title);
+            }
+
+            InputStream newModsStream;
+            try {
+                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                new XMLOutputter().output(newMods, outStream);
+                newModsStream = new ByteArrayInputStream(outStream.toByteArray());
+            } catch (IOException e) {
+                throw new ServiceException("Unable to build new mods stream for " + pid, e);
+            }
+
+            modsValidator.validate(newModsStream);
+            obj.setDescription(newModsStream);
+        }
+
+        operationsMessageSender.sendUpdateDescriptionOperation(agent.getUsername(), Arrays.asList(pid));
+    }
+
+    /**
+     * @param aclService the aclService to set
+     */
+    public void setAclService(AccessControlService aclService) {
+        this.aclService = aclService;
+    }
+
+    public RepositoryObjectLoader getRepoObjLoader() {
+        return repoObjLoader;
+    }
+
+    /**
+     *
+     * @param repoObjLoader
+     */
+    public void setRepoObjLoader(RepositoryObjectLoader repoObjLoader) {
+        this.repoObjLoader = repoObjLoader;
+    }
+
+    /**
+     *
+     * @param modsValidator
+     */
+    public void setModsValidator(MODSValidator modsValidator) {
+        this.modsValidator = modsValidator;
+    }
+
+    /**
+     *
+     * @param operationsMessageSender
+     */
+    public void setOperationsMessageSender(OperationsMessageSender operationsMessageSender) {
+        this.operationsMessageSender = operationsMessageSender;
+    }
+
+    /**
+     *
+     * @param mods the mods record to be edited
+     * @return true if mods has title
+     */
+    private boolean hasExistingTitle(Element mods) {
+        boolean hasOldTitle;
+        try {
+            Element title = mods.getChild("titleInfo", MODS_V3_NS).getChild("title", MODS_V3_NS);
+            hasOldTitle = true;
+        } catch (NullPointerException e) {
+            hasOldTitle = false;
+        }
+
+        return hasOldTitle;
+    }
+
+    /**
+     *
+     * @param doc the mods document to be edited
+     * @param title the new title to be added to the mods document
+     * @return updated mods document
+     */
+    private Document addTitleToMODS(Document doc, String title) {
+        doc.getRootElement()
+                .addContent(new Element("titleInfo", MODS_V3_NS)
+                        .addContent(new Element("title", MODS_V3_NS)
+                                .setText(title)));
+
+        return doc;
+    }
+
+    /**
+     *
+     * @param doc
+     * @param title
+     * @return updated mods document
+     */
+    private Document updateTitle(Document doc, String title) {
+        Element oldTitle = doc.getRootElement().getChild("titleInfo", MODS_V3_NS).getChild("title",
+                MODS_V3_NS);
+        oldTitle.setText(title);
+
+        return doc;
+    }
+}
---
+++ persistence/src/test/java/edu/unc/lib/dl/persist/services/edit/EditTitleServiceTest.java
@@ -0,0 +1,218 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.persist.services.edit;
+
+import edu.unc.lib.dl.acl.exception.AccessRestrictionException;
+import edu.unc.lib.dl.acl.service.AccessControlService;
+import edu.unc.lib.dl.acl.util.AccessGroupSet;
+import edu.unc.lib.dl.acl.util.AgentPrincipals;
+import edu.unc.lib.dl.acl.util.Permission;
+import edu.unc.lib.dl.fcrepo4.*;
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.services.OperationsMessageSender;
+import edu.unc.lib.dl.validation.MODSValidator;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.input.sax.XMLReaderSAX2Factory;
+import org.jdom2.output.XMLOutputter;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.UUID;
+
+import static edu.unc.lib.dl.xml.JDOMNamespaceUtil.MODS_V3_NS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class EditTitleServiceTest {
+
+    @Mock
+    private AccessControlService aclService;
+    @Mock
+    private AgentPrincipals agent;
+    @Mock
+    private AccessGroupSet groups;
+    @Mock
+    private RepositoryObjectLoader repoObjLoader;
+    @Mock
+    private ContentObject contentObj;
+    @Mock
+    private BinaryObject binaryObj;
+    @Mock
+    private OperationsMessageSender operationsMessageSender;
+    @Mock
+    private MODSValidator modsValidator;
+
+    @Captor
+    private ArgumentCaptor<Collection<PID>> pidsCaptor;
+    @Captor
+    private ArgumentCaptor<InputStream> inputStreamCaptor;
+
+    private EditTitleService service;
+    private PID pid;
+    private Document document;
+
+    @Before
+    public void init() throws Exception {
+        initMocks(this);
+
+        pid = PIDs.get(UUID.randomUUID().toString());
+        service = new EditTitleService();
+
+        service.setAclService(aclService);
+        service.setModsValidator(modsValidator);
+        service.setRepoObjLoader(repoObjLoader);
+        service.setOperationsMessageSender(operationsMessageSender);
+
+        when(repoObjLoader.getRepositoryObject(eq(pid))).thenReturn(contentObj);
+        when(contentObj.getDescription()).thenReturn(binaryObj);
+        when(aclService.hasAccess(any(PID.class), any(AccessGroupSet.class), eq(Permission.editDescription)))
+                .thenReturn(true);
+        when(repoObjLoader.getRepositoryObject(any(PID.class))).thenReturn(contentObj);
+        when(agent.getPrincipals()).thenReturn(groups);
+        when(agent.getUsername()).thenReturn("agentname");
+        when(operationsMessageSender.sendUpdateDescriptionOperation(anyString(), any(Collection.class))).thenReturn("message_id");
+
+        document = new Document();
+    }
+
+    @Test
+    public void editTitleTest() throws Exception {
+        String title = "new title";
+        document.addContent(new Element("mods", MODS_V3_NS)
+                .addContent(new Element("titleInfo", MODS_V3_NS)
+                        .addContent(new Element("title", MODS_V3_NS).setText("original title"))));
+        when(binaryObj.getBinaryStream()).thenReturn(convertDocumentToStream(document));
+
+        service.editTitle(agent, pid, title);
+
+        verify(operationsMessageSender).sendUpdateDescriptionOperation(anyString(), pidsCaptor.capture());
+        Collection<PID> pids = pidsCaptor.getValue();
+        assertEquals(1, pids.size());
+        assertTrue(pids.contains(pid));
+
+        Document updatedDoc = getUpdatedDescriptionDocument();
+        assertTrue(hasTitleValue(updatedDoc, title));
+    }
+
+    @Test(expected = AccessRestrictionException.class)
+    public void insufficientAccessTest() throws Exception {
+        String title = "new title";
+        document.addContent(new Element("mods", MODS_V3_NS)
+                .addContent(new Element("titleInfo", MODS_V3_NS)
+                        .addContent(new Element("title", MODS_V3_NS).setText("original title"))));
+        when(binaryObj.getBinaryStream()).thenReturn(convertDocumentToStream(document));
+
+        doThrow(new AccessRestrictionException()).when(aclService)
+                .assertHasAccess(anyString(), eq(pid), any(AccessGroupSet.class), any(Permission.class));
+
+        service.editTitle(agent, pid, title);
+    }
+
+    @Test
+    public void noModsTest() throws Exception {
+        String title = "new title";
+        when(contentObj.getDescription()).thenReturn(null);
+
+        service.editTitle(agent, pid, title);
+
+        verify(operationsMessageSender).sendUpdateDescriptionOperation(anyString(), pidsCaptor.capture());
+        Collection<PID> pids = pidsCaptor.getValue();
+        assertEquals(1, pids.size());
+        assertTrue(pids.contains(pid));
+
+        Document updatedDoc = getUpdatedDescriptionDocument();
+        assertTrue(hasTitleValue(updatedDoc, title));
+    }
+
+    @Test
+    public void noTitleInMods() throws Exception {
+        String title = "new title";
+        document.addContent(new Element("mods", MODS_V3_NS)
+                .addContent(new Element("language", MODS_V3_NS)
+                        .addContent(new Element("languageTerm", MODS_V3_NS)
+                                .setText("eng")
+                                .setAttribute("authority", "iso639-2b")
+                                .setAttribute("type", "code"))));
+        when(binaryObj.getBinaryStream()).thenReturn(convertDocumentToStream(document));
+
+        service.editTitle(agent, pid, title);
+
+        verify(operationsMessageSender).sendUpdateDescriptionOperation(anyString(), pidsCaptor.capture());
+        Collection<PID> pids = pidsCaptor.getValue();
+        assertEquals(1, pids.size());
+        assertTrue(pids.contains(pid));
+
+        Document updatedDoc = getUpdatedDescriptionDocument();
+        assertTrue(hasTitleValue(updatedDoc, title));
+    }
+
+    @Test
+    public void multipleTitlesInMods() throws Exception {
+        String title = "new title";
+        document.addContent(new Element("mods", MODS_V3_NS)
+                .addContent(new Element("titleInfo", MODS_V3_NS)
+                        .addContent(new Element("title", MODS_V3_NS).setText("original title")))
+                .addContent(new Element("titleInfo", MODS_V3_NS)
+                        .addContent(new Element("title", MODS_V3_NS).setText("a second title"))));
+        when(binaryObj.getBinaryStream()).thenReturn(convertDocumentToStream(document));
+
+        service.editTitle(agent, pid, title);
+
+        verify(operationsMessageSender).sendUpdateDescriptionOperation(anyString(), pidsCaptor.capture());
+        Collection<PID> pids = pidsCaptor.getValue();
+        assertEquals(1, pids.size());
+        assertTrue(pids.contains(pid));
+
+
+        Document updatedDoc = getUpdatedDescriptionDocument();
+        assertTrue(hasTitleValue(updatedDoc, title));
+        // check that first title is no longer in mods
+        assertFalse(hasTitleValue(updatedDoc, "original title"));
+        // check that second title is unchanged
+        assertTrue(hasTitleValue(updatedDoc, "a second title"));
+    }
+
+    private InputStream convertDocumentToStream(Document doc) throws IOException {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        new XMLOutputter().output(doc, outStream);
+        return new ByteArrayInputStream(outStream.toByteArray());
+    }
+
+    private Document getUpdatedDescriptionDocument() throws IOException, JDOMException {
+        verify(contentObj).setDescription(inputStreamCaptor.capture());
+        SAXBuilder sb = new SAXBuilder(new XMLReaderSAX2Factory(false));
+        return sb.build(inputStreamCaptor.getValue());
+    }
+
+    private boolean hasTitleValue(Document document, String expectedTitle) {
+        return document.getRootElement()
+                .getChildren("titleInfo", MODS_V3_NS)
+                .stream()
+                .anyMatch(e -> (e.getChild("title", MODS_V3_NS).getValue().contentEquals(expectedTitle)));
+    }
+}
