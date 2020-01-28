b4b47fe8364116ce9c3b9659d504f42d72bdcd0a
lfarrell <lfarrell@email.unc.edu>
2020-01-28 09:13:54 -0500

    Implement user interface to edit work titles

---
+++ services/src/test/java/edu/unc/lib/dl/cdr/services/rest/modify/EditTitleIT.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dl.cdr.services.rest.modify;
+
+import static edu.unc.lib.dl.xml.JDOMNamespaceUtil.MODS_V3_NS;
+import static edu.unc.lib.dl.xml.SecureXMLFactory.createSAXBuilder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.util.Map;
+
+import edu.unc.lib.dl.fcrepo4.BinaryObject;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.web.servlet.MvcResult;
+
+import edu.unc.lib.dl.acl.exception.AccessRestrictionException;
+import edu.unc.lib.dl.acl.util.AccessGroupSet;
+import edu.unc.lib.dl.acl.util.Permission;
+import edu.unc.lib.dl.fcrepo4.WorkObject;
+import edu.unc.lib.dl.fedora.PID;
+import edu.unc.lib.dl.rdf.DcElements;
+
+/**
+ *
+ * @author lfarrell
+ *
+ */
+@ContextHierarchy({
+        @ContextConfiguration("/spring-test/test-fedora-container.xml"),
+        @ContextConfiguration("/spring-test/cdr-client-container.xml"),
+        @ContextConfiguration("/edit-title-it-servlet.xml")
+})
+public class EditTitleIT extends AbstractAPIIT {
+    @Test
+    public void testCreateTitleWhereNoneExists() throws Exception {
+        PID pid = makePid();
+        WorkObject work = repositoryObjectFactory.createWorkObject(pid, null);
+        String title = "work_title";
+
+        MvcResult result = mvc.perform(put("/edit/title/" + pid.getUUID())
+                .param("title", title))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+        // Verify response from api
+        Map<String, Object> respMap = getMapFromResponse(result);
+        assertEquals(pid.getUUID(), respMap.get("pid"));
+        assertEquals("editTitle", respMap.get("action"));
+        hasTitleValue(getUpdatedDescriptionDocument(work.getDescription()), title);
+    }
+
+    @Test
+    public void testReplaceTitle() throws Exception {
+        PID pid = makePid();
+        String oldTitle = "old_work_title";
+        Model workModel = ModelFactory.createDefaultModel();
+        workModel.add(workModel.createResource(pid.getRepositoryPath()), DcElements.title,
+                oldTitle);
+        WorkObject work = repositoryObjectFactory.createWorkObject(pid, workModel);
+
+        String newTitle = "new_work_title";
+        MvcResult result = mvc.perform(put("/edit/title/" + pid.getUUID())
+                .param("title", newTitle))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+        // Verify response from api
+        Map<String, Object> respMap = getMapFromResponse(result);
+        assertEquals(pid.getUUID(), respMap.get("pid"));
+        assertEquals("editTitle", respMap.get("action"));
+        hasTitleValue(getUpdatedDescriptionDocument(work.getDescription()), newTitle);
+    }
+
+    @Test
+    public void testAuthorizationFailure() throws Exception {
+        PID pid = makePid();
+        repositoryObjectFactory.createFolderObject(pid, null);
+
+        doThrow(new AccessRestrictionException()).when(aclService)
+                .assertHasAccess(anyString(), eq(pid), any(AccessGroupSet.class), eq(Permission.editDescription));
+
+        String title = "folder_title";
+        MvcResult result = mvc.perform(put("/edit/title/" + pid.getUUID())
+                .param("title", title))
+                .andExpect(status().isForbidden())
+                .andReturn();
+
+        // Verify response from api
+        Map<String, Object> respMap = getMapFromResponse(result);
+        assertEquals(pid.getUUID(), respMap.get("pid"));
+        assertEquals("editTitle", respMap.get("action"));
+        assertTrue(respMap.containsKey("error"));
+    }
+
+    private Document getUpdatedDescriptionDocument(BinaryObject binary) throws IOException, JDOMException {
+        SAXBuilder sb = createSAXBuilder();
+        return sb.build(binary.getBinaryStream());
+    }
+
+    private boolean hasTitleValue(Document document, String expectedTitle) {
+        return document.getRootElement()
+                .getChildren("titleInfo", MODS_V3_NS)
+                .stream()
+                .anyMatch(e -> (e.getChild("title", MODS_V3_NS).getValue().contentEquals(expectedTitle)));
+    }
+}
---
+++ static/js/admin/src/EditTitleForm.js
@@ -0,0 +1,54 @@
+define('EditTitleForm', [ 'jquery', 'jquery-ui', 'underscore', 'RemoteStateChangeMonitor', 'tpl!../templates/admin/editTitleForm',
+        'ModalLoadingOverlay', 'AbstractForm', 'AlertHandler'],
+    function($, ui, _, RemoteStateChangeMonitor, editTitleForm, ModalLoadingOverlay, AbstractForm) {
+
+        var defaultOptions = {
+            title : 'Edit Title',
+            createFormTemplate : editTitleForm,
+            submitMethod: 'PUT'
+        };
+
+        function EditTitleForm(options) {
+            this.options = $.extend({}, defaultOptions, options);
+        };
+
+        EditTitleForm.prototype.constructor = EditTitleForm;
+        EditTitleForm.prototype = Object.create( AbstractForm.prototype);
+
+        EditTitleForm.prototype.preprocessForm = function(resultObject) {
+            var newTitle = $("input[name='title']", this.$form).val();
+            var pid = resultObject.metadata.id;
+
+            this.action_url = "/services/api/edit/title/" + pid + "?title=" + newTitle;
+        };
+
+        EditTitleForm.prototype.validationErrors = function() {
+            var errors = [];
+            var title = $("input[name='title']", this.$form).val();
+            // Validate input
+            if (!title)
+                errors.push("You must specify a title.");
+            return errors;
+        };
+
+        EditTitleForm.prototype.getSuccessMessage = function(data) {
+            return "Title has been successfully edited.";
+        };
+
+        EditTitleForm.prototype.getErrorMessage = function(data) {
+            return "An error occurred while editing the title";
+        };
+
+        EditTitleForm.prototype.remove = function() {
+            AbstractForm.prototype.remove.apply(this);
+            if (this.submitSuccessful) {
+                this.options.actionHandler.addEvent({
+                    action : 'RefreshResult',
+                    target : this.resultObject,
+                    waitForUpdate : true
+                });
+            }
+        };
+
+        return EditTitleForm;
+    });
