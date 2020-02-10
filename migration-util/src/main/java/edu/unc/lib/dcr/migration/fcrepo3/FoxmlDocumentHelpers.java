ffd2701c0925f5a22d14a45a1e3b5dd5792be73e
Ben Pennell <bbpennel@email.unc.edu>
2020-02-10 15:03:17 -0500

    Add helper utility for extract information from foxml documents, including datastream versions. Restore some of ContentModelHelper to provide constants for bxc3 constants during the migration.

---
+++ migration-util/src/main/java/edu/unc/lib/dcr/migration/fcrepo3/FoxmlDocumentHelpers.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.dcr.migration.fcrepo3;
+
+import static edu.unc.lib.dl.util.RDFModelUtil.createModel;
+import static edu.unc.lib.dl.xml.JDOMNamespaceUtil.FOXML_NS;
+import static edu.unc.lib.dl.xml.JDOMNamespaceUtil.RDF_NS;
+import static java.util.stream.Collectors.toSet;
+import static org.apache.jena.rdf.model.ResourceFactory.createProperty;
+import static org.apache.jena.util.ResourceUtils.renameResource;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.XMLOutputter;
+
+import edu.unc.lib.dl.exceptions.RepositoryException;
+import edu.unc.lib.dl.fcrepo4.PIDs;
+import edu.unc.lib.dl.fedora.PID;
+
+/**
+ * Utility methods for pulling data from foxml documents
+ *
+ * @author bbpennel
+ */
+public class FoxmlDocumentHelpers {
+    public static final String RELS_EXT_DS = "RELS-EXT";
+    public static final String MODS_DS = "MD_DESCRIPTIVE";
+    public static final String FITS_DS = "MD_TECHNICAL";
+    public static final String DC_DS = "DC";
+    public static final String PREMIS_DS = "MD_EVENTS";
+    public static final String MANIFEST_DS = "DATA_MANIFEST";
+    public static final String ORIGINAL_DS = "DATA_FILE";
+
+    /**
+     * List the names of all datastreams present in the foxml
+     *
+     * @param foxml
+     * @return list of datastream names
+     */
+    public static Set<String> listDatastreams(Document foxml) {
+        return foxml.getRootElement().getChildren("datastream", FOXML_NS).stream()
+            .map(ds -> ds.getAttributeValue("ID"))
+            .collect(toSet());
+    }
+
+    /**
+     * Retrieve the properties and relationships for the object represented by
+     * the provided foxml as a model.
+     *
+     * @param foxml foxml document
+     * @return model containing properties, with the repository URI of the object as the resource.
+     */
+    public static Model getObjectModel(Document foxml) {
+        // Load the relations from RELS-EXT into the model
+        Element relsContentEl = getDatastreamElByName(foxml, RELS_EXT_DS)
+            .getChild("datastreamVersion", FOXML_NS)
+            .getChild("xmlContent", FOXML_NS)
+            .getChild("RDF", RDF_NS);
+
+        Model model;
+        try {
+            model = createModel(convertElementToStream(relsContentEl), "RDF/XML");
+        } catch (IOException e) {
+            throw new RepositoryException("Failed to serialize RELS-EXT", e);
+        }
+
+        // Swap out the fedora3 uri for the current form
+        String foxmlPid = getFoxmlPid(foxml);
+        String relsRescUri = "info:fedora/" + foxmlPid;
+        Resource resc = model.getResource(relsRescUri);
+        PID pid = PIDs.get(foxmlPid);
+        renameResource(resc, pid.getRepositoryPath());
+
+        // Add the object properties to the model
+        for (Element propEl : foxml.getRootElement().getChild("objectProperties", FOXML_NS).getChildren()) {
+            String name = propEl.getAttributeValue("NAME");
+            String value = propEl.getAttributeValue("VALUE");
+
+            resc.addLiteral(createProperty(name), value);
+        }
+
+        return model;
+    }
+
+    /**
+     * Get a datastream element by name field from the provided foxml
+     *
+     * @param foxml
+     * @param name
+     * @return
+     */
+    public static Element getDatastreamElByName(Document foxml, String name) {
+        return foxml.getRootElement().getChildren("datastream", FOXML_NS).stream()
+                .filter(ds -> name.equals(ds.getAttributeValue("ID")))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * Retrieve the id of the object in this foxml.
+     *
+     * @param foxml
+     * @return
+     */
+    public static String getFoxmlPid(Document foxml) {
+        return foxml.getRootElement().getAttributeValue("PID");
+    }
+
+    public static InputStream convertElementToStream(Element el) throws IOException {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        new XMLOutputter().output(el, outStream);
+        return new ByteArrayInputStream(outStream.toByteArray());
+    }
+
+    /**
+     * Generate a list of datastream versions for the specified datastream,
+     * ordered from oldest to newest.
+     *
+     * @param foxml
+     * @param name
+     * @return
+     */
+    public static List<DatastreamVersion> listDatastreamVersions(Document foxml, String name) {
+        Element dsEl = getDatastreamElByName(foxml, name);
+        if (dsEl == null) {
+            return null;
+        }
+
+        List<DatastreamVersion> result = new ArrayList<>();
+        for (Element dsvEl: dsEl.getChildren("datastreamVersion", FOXML_NS)) {
+            String versionName = dsvEl.getAttributeValue("ID");
+            String mimeType = dsvEl.getAttributeValue("MIMETYPE");
+            String size = dsvEl.getAttributeValue("SIZE");
+            String created = dsvEl.getAttributeValue("CREATED");
+            Element digestEl = dsvEl.getChild("contentDigest", FOXML_NS);
+            String md5 = digestEl == null ? null : digestEl.getAttributeValue("DIGEST");
+
+            result.add(new DatastreamVersion(md5, name, versionName, created, size, mimeType));
+        }
+
+        result.sort((dsV1, dsV2) -> dsV1.getCreated().compareTo(dsV2.getCreated()));
+
+        return result;
+    }
+}
