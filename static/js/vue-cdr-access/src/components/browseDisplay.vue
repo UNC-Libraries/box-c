783e6402b029f9dcd3904f2f909a2ee933baddd2
lfarrell <lfarrell@email.unc.edu>
2020-04-01 11:14:06 -0400

    Switch to word-break. Word-wrap not always working

--- static/js/vue-cdr-access/src/components/browseDisplay.vue
+++ static/js/vue-cdr-access/src/components/browseDisplay.vue
@@ -101,7 +101,7 @@
 
         .record-title {
             margin-top: 25px;
-            word-wrap: break-word;
+            word-break: break-all;
         }
 
         .thumbnail + .record-title {
