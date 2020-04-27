b1c681dea6abfb394ac90c199b66af418ae39759
lfarrell <lfarrell@email.unc.edu>
2020-04-27 12:00:48 -0400

    Remove unnecessary loop

--- access-common/src/main/java/edu/unc/lib/dl/ui/util/ModsUtil.java
+++ access-common/src/main/java/edu/unc/lib/dl/ui/util/ModsUtil.java
@@ -31,32 +31,28 @@ public class ModsUtil {
 
     public static Document removeEmptyNodes(Document doc) {
         Element docRoot = doc.getRootElement();
-        List<Element> modsRoot = docRoot.getChildren();
-        int i = modsRoot.size();
-
-        while (i > 0) {
-            recursiveRemoveEmptyContent(modsRoot);
-            i--;
-        }
-
+        recursiveRemoveEmptyContent(docRoot);
         return doc;
     }
 
-    private static void recursiveRemoveEmptyContent(List<Element> content) {
-        int i = content.size();
+    private static void recursiveRemoveEmptyContent(Element content) {
+        List<Element> children = content.getChildren();
+        if (children.size() > 0) {
+            int i = children.size();
 
-        while (i > 0) {
-            Element node = content.get(i - 1);
+            while (i > 0) {
+                Element node = children.get(i - 1);
 
-            if (node.getChildren().size() > 0) {
-                recursiveRemoveEmptyContent(node.getChildren());
-            } else if (node.getTextTrim().equals("")) {
-                Element parent = node.getParentElement();
-                parent.removeContent(node);
-                emptyParent(parent);
+                if (node.getChildren().size() > 0) {
+                    recursiveRemoveEmptyContent(node);
+                } else if (node.getTextTrim().equals("")) {
+                    Element parent = node.getParentElement();
+                    parent.removeContent(node);
+                    emptyParent(parent);
+                }
+
+                i--;
             }
-
-            i--;
         }
     }
 
