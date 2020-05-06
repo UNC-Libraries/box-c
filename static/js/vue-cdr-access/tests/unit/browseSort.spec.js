1d3205cfe54de3b49ad01cdc7d9eb31285b38dbc
Dean Farrell <lfarrell@email.unc.edu>
2020-05-06 08:56:06 -0400

    Keep sort type displayed (#981)

--- static/js/vue-cdr-access/tests/unit/browseSort.spec.js
+++ static/js/vue-cdr-access/tests/unit/browseSort.spec.js
@@ -16,45 +16,51 @@ const router = new VueRouter({
         }
     ]
 });
+let wrapper;
+let wrapper_search;
 
 describe('browseSort.vue', () => {
-    it("updates the url when the dropdown changes for browsing", () => {
-        let wrapper = shallowMount(browseSort, {
+    beforeEach(() => {
+        wrapper = shallowMount(browseSort, {
             localVue,
             router,
             propsData: {
                 browseType: 'display'
             }
         });
+
+        wrapper_search = shallowMount(browseSort, {
+            localVue,
+            router,
+            propsData: {
+                browseType: 'search'
+            }
+        });
+    });
+
+    it("updates the url when the dropdown changes for browsing", () => {
         wrapper.findAll('option').at(2).setSelected();
         expect(wrapper.vm.$router.currentRoute.query.sort).toEqual('title,reverse');
+        expect(wrapper.vm.sort_order).toEqual('title,reverse');
     });
 
     it("updates the url when the dropdown changes for a search", () => {
-        let wrapper = shallowMount(browseSort, {
-            localVue,
-            router,
-            propsData: {
-                browseType: 'search'
-            }
-        });
-        wrapper.findAll('option').at(2).setSelected();
-        expect(wrapper.vm.$router.currentRoute.query.sort).toEqual('title,reverse');
+        wrapper_search.findAll('option').at(2).setSelected();
+        expect(wrapper_search.vm.$router.currentRoute.query.sort).toEqual('title,reverse');
+        expect(wrapper_search.vm.sort_order).toEqual('title,reverse');
     });
 
-    it("maintains the base path when changing search pages", () => {
-        let wrapper = shallowMount(browseSort, {
-            localVue,
-            router,
-            propsData: {
-                browseType: 'search'
-            }
-        });
-
-        wrapper.vm.$router.push('/search/d77fd8c9-744b-42ab-8e20-5ad9bdf8194e?collection_name=testCollection&sort=title,normal');
+    it("maintains the base path when changing search pages", async () => {
+        wrapper_search.vm.$router.push('/search/d77fd8c9-744b-42ab-8e20-5ad9bdf8194e?collection_name=testCollection&sort=title,normal');
 
         // Change sort types
-        wrapper.findAll('option').at(2).setSelected();
-        expect(wrapper.vm.$route.path).toEqual('/search/d77fd8c9-744b-42ab-8e20-5ad9bdf8194e');
+        wrapper_search.findAll('option').at(2).setSelected();
+        expect(wrapper_search.vm.sort_order).toEqual('title,reverse');
+        expect(wrapper_search.vm.$route.path).toEqual('/search/d77fd8c9-744b-42ab-8e20-5ad9bdf8194e');
+    });
+
+    afterEach(() => {
+        wrapper.setData({ sort_order: '' });
+        wrapper_search.setData({ sort_order: '' });
     });
 });
