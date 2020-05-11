037c883185967826f1d86914843753ec19cb0c14
lfarrell <lfarrell@email.unc.edu>
2020-05-11 11:29:53 -0400

    Allow admins to pause/resume deposits

--- services/src/main/java/edu/unc/lib/dl/cdr/services/rest/DepositController.java
+++ services/src/main/java/edu/unc/lib/dl/cdr/services/rest/DepositController.java
@@ -250,6 +250,9 @@ public class DepositController {
         result.put("jobsURI", "/api/status/deposit/" + depositUUID + "/jobs");
         result.put("eventsURI", "/api/status/deposit/" + depositUUID + "/eventsXML");
 
+        AccessGroupSet groups = getAgentPrincipals().getPrincipals();
+        result.put("isAdmin", groups.contains("unc:app:lib:cdr:admin"));
+
         return result;
     }
 
@@ -274,7 +277,7 @@ public class DepositController {
     /**
      * Aborts the deposit, reversing any ingests and scheduling a cleanup job.
      *
-     * @param depositUUID
+     * @param uuid
      */
     @RequestMapping(value = { "{uuid}", "/{uuid}" }, method = RequestMethod.DELETE)
     public void destroy(@PathVariable String uuid) {
@@ -288,7 +291,7 @@ public class DepositController {
      * deposit such that it can be resumed later. The deposit destroy action cleans up the submitted deposit package,
      * leaving staged files alone.
      *
-     * @param depositUUID
+     * @param uuid
      *           the unique identifier of the deposit
      * @param action
      *           the action to take on the deposit (pause, resume, cancel, destroy)
@@ -305,8 +308,10 @@ public class DepositController {
         String username = GroupsThreadStore.getUsername();
         Map<String, String> status = depositStatusFactory.get(uuid);
         AccessGroupSet principals = getAgentPrincipals().getPrincipals();
+
         if (!globalPermissionEvaluator.hasGlobalPermission(principals, Permission.ingest)) {
-            if (username == null ||  !username.equals(status.get(DepositField.depositorName.name()))) {
+            if (username == null || (!username.equals(status.get(DepositField.depositorName.name())) &&
+                    !globalPermissionEvaluator.hasGlobalPermission(principals, Permission.createAdminUnit))) {
                 response.setStatus(403);
                 return;
             }
