747cfd4004e9a53c7dd8822b8433b325c1f4fa4b
lfarrell <lfarrell@email.unc.edu>
2020-06-12 14:08:32 -0400

    trigger unchecking all selected items when running a batch action

--- static/js/admin/src/action/AjaxCallbackAction.js
+++ static/js/admin/src/action/AjaxCallbackAction.js
@@ -125,9 +125,21 @@ define('AjaxCallbackAction', [ 'jquery', 'jquery-ui', 'RemoteStateChangeMonitor'
 					op.context.target.setState("idle");
 				op.complete(data);
 			}
-		}).fail(function(data) {
-			op.alertHandler.alertHandler('error', textStatus + ", " + error);
-			console.error(textStatus, error);
+
+			// Trigger unchecking all checked objects when action completes
+			$(".select_all").trigger('click', function(){
+				var checkbox = $(this).children("input").prop("checked", false);
+				var toggleFn = checkbox.prop("checked") ? "select" : "unselect";
+				var resultObjects = op.resultObjectList.resultObjects;
+				for (var index in resultObjects) {
+					resultObjects[index][toggleFn]();
+				}
+				op.selectionUpdated();
+			}).children("input").prop("checked", false);
+
+		}).fail(function(data, error) {
+			op.alertHandler.alertHandler('error', data.responseText + ", " + error);
+			console.error(data.responseText, error);
 		});
 	};
 	
