5e8849a07255e145439088d55f48d69bd17d60e7
Ben Pennell <ben.pennell@gmail.com>
2020-10-14 08:43:30 -0400

    Update PIDs.java

--- fcrepo-clients/src/main/java/edu/unc/lib/dl/fcrepo4/PIDs.java
+++ fcrepo-clients/src/main/java/edu/unc/lib/dl/fcrepo4/PIDs.java
@@ -44,8 +44,6 @@ public class PIDs {
 
     private static final Logger log = LoggerFactory.getLogger(PIDs.class);
 
-    //[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}
-
     private PIDs() {
     }
 
