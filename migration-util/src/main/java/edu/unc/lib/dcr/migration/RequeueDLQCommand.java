32764246abb89cddc8c6177c8125b89c4240470a
Ben Pennell <bbpennel@email.unc.edu>
2020-12-03 18:09:14 -0500

    Add command for requeuing solr messages. Add dry run option

--- migration-util/src/main/java/edu/unc/lib/dcr/migration/RequeueDLQCommand.java
+++ migration-util/src/main/java/edu/unc/lib/dcr/migration/RequeueDLQCommand.java
@@ -18,19 +18,25 @@ package edu.unc.lib.dcr.migration;
 import static edu.unc.lib.dcr.migration.MigrationConstants.OUTPUT_LOGGER;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.util.function.Predicate;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 
 import org.slf4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import edu.unc.lib.dl.exceptions.RepositoryException;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.ParentCommand;
 
 /**
@@ -46,11 +52,49 @@ public class RequeueDLQCommand {
     @ParentCommand
     private MigrationCLI parentCommand;
 
+    @Option(names = {"-n", "--dry-run"},
+            description = "Perform inspect queue without changing it")
+    private boolean dryRun;
+
     private String applicationContextPath = "spring/activemq-context.xml";
 
     @Command(name = "requeue_enhancements",
             description = "Requeue failed enhancement messages")
-    public int enhancement() throws Exception {
+    public int requeueEnhancements() throws Exception {
+        moveMessages("ActiveMQ.DLQ",
+                "activemq:queue:repository.enhancements",
+                "CdrEnhancementSet IS NOT NULL",
+                null);
+        return 0;
+    }
+
+    @Command(name = "requeue_solr",
+            description = "Requeue solr messages")
+    public int requeueSolr() throws Exception {
+        moveMessages("ActiveMQ.DLQ",
+                "activemq:queue:repository.solrupdate",
+                null,
+                m -> {
+                    try {
+                        if (m instanceof TextMessage) {
+                            String text = ((TextMessage) m).getText();
+                            return text.contains("http://cdr.unc.edu/schema/message/solr/");
+                        }
+                    } catch (JMSException e) {
+                        throw new RepositoryException(e);
+                    }
+                    return false;
+                });
+        return 0;
+    }
+
+    private void moveMessages(String dlqName, String destName, String filter, Predicate<Message> filterPred)
+            throws Exception {
+        if (dryRun) {
+            output.info("Dry run -- No changes will be made");
+        }
+        output.info("Preparing to move messages from {} to {}", dlqName, destName);
+
         long start = System.currentTimeMillis();
         int cnt = 0;
 
@@ -63,18 +107,27 @@ public class RequeueDLQCommand {
 
             Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
 
-            Queue dlq = session.createQueue("ActiveMQ.DLQ");
-            MessageConsumer dlqConsumer = session.createConsumer(dlq, "CdrEnhancementSet IS NOT NULL");
+            Queue dlq = session.createQueue(dlqName);
+            MessageConsumer dlqConsumer;
+            if (filter == null) {
+                dlqConsumer = session.createConsumer(dlq);
+            } else {
+                dlqConsumer = session.createConsumer(dlq, filter);
+            }
 
-            Queue enhQueue = session.createQueue("activemq:queue:repository.enhancements");
-            MessageProducer producer = session.createProducer(enhQueue);
+            Queue destQueue = session.createQueue(destName);
+            MessageProducer producer = session.createProducer(destQueue);
 
             while (true) {
                 Message m = dlqConsumer.receive(500);
                 if (m != null) {
-                    output.info("Moving message {}", ++cnt);
-                    producer.send(m);
-                    m.acknowledge();
+                    if (filterPred == null || filterPred.test(m)) {
+                        output.info("Moving message {}", ++cnt);
+                        if (!dryRun) {
+                            producer.send(m);
+                            m.acknowledge();
+                        }
+                    }
                 } else {
                     break;
                 }
@@ -89,7 +142,7 @@ public class RequeueDLQCommand {
             }
         }
 
-        output.info("Finished moving {} enhancement messages in {}ms", cnt, System.currentTimeMillis() - start);
-        return 0;
+        output.info("Finished moving {} messages from {} to {} in {}ms",
+                cnt, dlqName, destName, System.currentTimeMillis() - start);
     }
 }
