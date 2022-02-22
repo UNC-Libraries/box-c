98195fe7a6eb392768283b5d2b19d981e4996ec2
lfarrell <lfarrell@email.unc.edu>
2022-02-22 16:30:50 -0500

    Add subjects to displayed facets and tags

--- static/js/vue-cdr-access/src/components/filterTags.vue
+++ static/js/vue-cdr-access/src/components/filterTags.vue
@@ -24,7 +24,7 @@ const TYPES = {
     created: 'Date Created',
     contributorIndex: 'Contributor',
     format: 'Format',
-    subjectIndex: 'Subject',
+    subject: 'Subject',
     titleIndex: 'Title',
     genre: 'Genre'
 }
