be3c39b8bc920ecf2e297d8be30a5dbb4a623ee9
lfarrell <lfarrell@email.unc.edu>
2022-03-22 15:29:34 -0400

    Add display order back in

--- indexing-solr/src/main/java/edu/unc/lib/boxc/indexing/solr/filter/SetObjectTypeFilter.java
+++ indexing-solr/src/main/java/edu/unc/lib/boxc/indexing/solr/filter/SetObjectTypeFilter.java
@@ -45,6 +45,7 @@ public class SetObjectTypeFilter implements IndexDocumentFilter {
         }
 
         idb.setResourceType(resourceType.name());
+        idb.setResourceTypeSort(resourceType.getDisplayOrder());
     }
 
 }
