2f7269da3118a32871047be5c2d4e687b0dbd1b4
Ben Pennell <bbpennel@email.unc.edu>
2022-03-24 16:03:14 -0400

    Move common styles into asset folder

--- static/js/vue-cdr-access/src/App.vue
+++ static/js/vue-cdr-access/src/App.vue
@@ -3,28 +3,3 @@
     <router-view/>
   </div>
 </template>
-<style lang="scss">
-$cdr-blue: #1A698C;
-.button.is-link {
-    background-color: $cdr-blue;
-    border-radius: 5px;
-    font-size: .85rem;
-    margin-bottom: 10px;
-    margin-top: 10px;
-    padding: 1.1em 0.8em 1.1em 1.2em;
-}
-
-a.button.is-link {
-    width: initial;
-
-    &.disabled {
-        background-color: #fff;
-        border-color: #dbdbdb;
-        color: #555;
-    }
-
-    span {
-        padding-right: 10px;
-    }
-}
-</style>
