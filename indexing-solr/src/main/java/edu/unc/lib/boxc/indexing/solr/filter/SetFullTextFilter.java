c3e59fd592d6d2797ba662b71f6314a2adacece1
lfarrell <lfarrell@email.unc.edu>
2022-03-30 14:11:38 -0400

    Remove unused import

--- indexing-solr/src/main/java/edu/unc/lib/boxc/indexing/solr/filter/SetFullTextFilter.java
+++ indexing-solr/src/main/java/edu/unc/lib/boxc/indexing/solr/filter/SetFullTextFilter.java
@@ -26,7 +26,6 @@ import edu.unc.lib.boxc.indexing.solr.exception.IndexingException;
 import edu.unc.lib.boxc.indexing.solr.indexing.DocumentIndexingPackage;
 import edu.unc.lib.boxc.model.api.objects.ContentObject;
 import edu.unc.lib.boxc.model.api.objects.FileObject;
-import edu.unc.lib.boxc.model.api.objects.WorkObject;
 import edu.unc.lib.boxc.model.fcrepo.services.DerivativeService;
 import edu.unc.lib.boxc.model.fcrepo.services.DerivativeService.Derivative;
 
