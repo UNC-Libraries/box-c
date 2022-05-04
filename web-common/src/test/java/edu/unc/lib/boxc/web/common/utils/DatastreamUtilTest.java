bd43195dac2ffb9b930d4a70f0d88e7c212488de
lfarrell <lfarrell@email.unc.edu>
2022-05-04 11:50:04 -0400

    Update filetype text based on how many and what filetypes are returned

--- web-common/src/test/java/edu/unc/lib/boxc/web/common/utils/DatastreamUtilTest.java
+++ web-common/src/test/java/edu/unc/lib/boxc/web/common/utils/DatastreamUtilTest.java
@@ -29,7 +29,10 @@ import org.junit.Test;
 
 import edu.unc.lib.boxc.model.api.ids.PID;
 import edu.unc.lib.boxc.search.solr.models.ContentObjectSolrRecord;
-import edu.unc.lib.boxc.web.common.utils.DatastreamUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  *
@@ -45,6 +48,7 @@ public class DatastreamUtilTest {
     private final static String FITS_DS = TECHNICAL_METADATA.getId() + "|text/xml|fits.xml|xml|5555||";
     private final static String THUMB_SMALL_DS = THUMBNAIL_SMALL.getId() + "|image/png|small|png|3333||";
     private final static String THUMB_LARGE_DS = THUMBNAIL_LARGE.getId() + "|image/png|small|png|10000||";
+    private final static ArrayList<String> emptyList = new ArrayList<>();
 
     @Before
     public void setup() {
@@ -123,5 +127,82 @@ public class DatastreamUtilTest {
         var id = DatastreamUtil.getThumbnailOwnerId(mdObj);
         assertNull(id);
         assertNull(DatastreamUtil.constructThumbnailUrl(id));
+    }
+
+    @Test
+    public void testNoFileFormats() {
+        PID pid = makePid();
+        ContentObjectSolrRecord mdObj = new ContentObjectSolrRecord();
+        mdObj.setId(pid.getId());
+        mdObj.setFileFormatDescription(emptyList);
+        mdObj.setFileFormatType(emptyList);
+        String fileType = DatastreamUtil.getFileType(mdObj);
+        assertEquals("", fileType);
+    }
+
+    @Test
+    public void testFileFormatsWithDescription() {
+        PID pid = makePid();
+        ContentObjectSolrRecord mdObj = new ContentObjectSolrRecord();
+        mdObj.setId(pid.getId());
+        mdObj.setFileFormatDescription(Collections.singletonList("Portable Network Graphics"));
+        mdObj.setFileFormatType(emptyList);
+        String fileType = DatastreamUtil.getFileType(mdObj);
+        assertEquals("Portable Network Graphics", fileType);
+    }
+
+    @Test
+    public void testFileFormatsWithMultipleDescription() {
+        PID pid = makePid();
+        ContentObjectSolrRecord mdObj = new ContentObjectSolrRecord();
+        mdObj.setId(pid.getId());
+        mdObj.setFileFormatDescription(Arrays.asList("Portable Network Graphics", "Joint Photographic Experts Group"));
+        mdObj.setFileFormatType(emptyList);
+        String fileType = DatastreamUtil.getFileType(mdObj);
+        assertEquals("Various", fileType);
+    }
+
+    @Test
+    public void testFileFormatsWithSameMultipleDescription() {
+        PID pid = makePid();
+        ContentObjectSolrRecord mdObj = new ContentObjectSolrRecord();
+        mdObj.setId(pid.getId());
+        mdObj.setFileFormatDescription(Arrays.asList("Portable Network Graphics", "Portable Network Graphics"));
+        mdObj.setFileFormatType(emptyList);
+        String fileType = DatastreamUtil.getFileType(mdObj);
+        assertEquals("Portable Network Graphics", fileType);
+    }
+
+    @Test
+    public void testFileFormatsWithNoDescription() {
+        PID pid = makePid();
+        ContentObjectSolrRecord mdObj = new ContentObjectSolrRecord();
+        mdObj.setId(pid.getId());
+        mdObj.setFileFormatDescription(emptyList);
+        mdObj.setFileFormatType(Collections.singletonList("image/png"));
+        String fileType = DatastreamUtil.getFileType(mdObj);
+        assertEquals("image/png", fileType);
+    }
+
+    @Test
+    public void testFileFormatsWithNoDescriptionMultipleTypes() {
+        PID pid = makePid();
+        ContentObjectSolrRecord mdObj = new ContentObjectSolrRecord();
+        mdObj.setId(pid.getId());
+        mdObj.setFileFormatDescription(emptyList);
+        mdObj.setFileFormatType(Arrays.asList("image/png", "image/jpeg"));
+        String fileType = DatastreamUtil.getFileType(mdObj);
+        assertEquals("Various", fileType);
+    }
+
+    @Test
+    public void testFileFormatsWithNoDescriptionSameMultipleTypes() {
+        PID pid = makePid();
+        ContentObjectSolrRecord mdObj = new ContentObjectSolrRecord();
+        mdObj.setId(pid.getId());
+        mdObj.setFileFormatDescription(emptyList);
+        mdObj.setFileFormatType(Arrays.asList("image/png", "image/png"));
+        String fileType = DatastreamUtil.getFileType(mdObj);
+        assertEquals("image/png", fileType);
     }
 }
