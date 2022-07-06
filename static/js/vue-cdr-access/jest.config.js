8d3e5c21f4b74bfc26eb4aea3c653bab7d3d3817
lfarrell <lfarrell@email.unc.edu>
2022-07-06 08:28:41 -0400

    Update dependencies and test settings

--- static/js/vue-cdr-access/jest.config.js
+++ static/js/vue-cdr-access/jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   automock: false,
-  testURL: 'https://localhost/record/73bc003c-9603-4cd9-8a65-93a22520ef6a',
   setupFiles: ['jest-localstorage-mock'],
   moduleFileExtensions: ['vue', 'js', 'json'],
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+    url: 'https://localhost/record/73bc003c-9603-4cd9-8a65-93a22520ef6a'
+  },
   transform: {
     "^.+\\.vue$": "@vue/vue3-jest",
     "^.+\\js$": "babel-jest"
