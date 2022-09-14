7304946edc8b51d908ed79ae7900d4a39f0fca78
sharonl <6546457+sharonluong@users.noreply.github.com>
2022-09-14 17:10:05 -0400

    Bxc 3787 send email set order update (#1470)
    * BXC-3787 build email and send
    
    * BXC-3787 setting up initial files for notification email
    
    * BXC-3787 adding assertions
    
    * BXC-3787 add test and finish up
    
    * BXC-3787 update subject heading of email
    
    Co-authored-by: Sharon Luong <snluong@email.lib.unc.edu>

--- services-camel-app/src/main/java/edu/unc/lib/boxc/services/camel/order/OrderNotificationService.java
+++ services-camel-app/src/main/java/edu/unc/lib/boxc/services/camel/order/OrderNotificationService.java
@@ -16,6 +16,7 @@
 package edu.unc.lib.boxc.services.camel.order;
 
 import edu.unc.lib.boxc.model.api.ids.PID;
+import edu.unc.lib.boxc.operations.impl.utils.EmailHandler;
 import edu.unc.lib.boxc.operations.jms.order.MultiParentOrderRequest;
 
 import java.util.List;
@@ -26,13 +27,29 @@ import java.util.List;
  * @author bbpennel
  */
 public class OrderNotificationService {
+    private EmailHandler emailHandler;
+    private OrderNotificationBuilder orderNotificationBuilder;
     /**
-     * Send a notification about the results of an order operation caused by a MultiParentOrderRequest
+     * Send a notification about the results of a MultiParentOrderRequest
      * @param request
      * @param successes list of pids for objects that successfully updated
-     * @param errors
+     * @param errors list of errors for unsuccessful order operations
      */
     public void sendResults(MultiParentOrderRequest request, List<PID> successes, List<String> errors) {
-        // TODO implement this
+        if (request.getEmail() == null) {
+            return;
+        }
+
+        var emailBody = orderNotificationBuilder.construct(request, successes, errors);
+
+        emailHandler.sendEmail(request.getEmail(), "DCR Member Order update completed", emailBody, null, null);
+    }
+
+    public void setEmailHandler(EmailHandler emailHandler) {
+        this.emailHandler = emailHandler;
+    }
+
+    public void setOrderNotificationBuilder(OrderNotificationBuilder orderNotificationBuilder) {
+        this.orderNotificationBuilder = orderNotificationBuilder;
     }
 }
---
+++ services-camel-app/src/test/java/edu/unc/lib/boxc/services/camel/order/OrderNotificationServiceTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2008 The University of North Carolina at Chapel Hill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.unc.lib.boxc.services.camel.order;
+
+import edu.unc.lib.boxc.auth.api.models.AgentPrincipals;
+import edu.unc.lib.boxc.auth.fcrepo.models.AccessGroupSetImpl;
+import edu.unc.lib.boxc.auth.fcrepo.models.AgentPrincipalsImpl;
+import edu.unc.lib.boxc.model.api.ids.PID;
+import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
+import edu.unc.lib.boxc.operations.impl.utils.EmailHandler;
+import edu.unc.lib.boxc.operations.jms.order.MultiParentOrderRequest;
+import edu.unc.lib.boxc.operations.jms.order.OrderOperationType;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.isNull;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.util.Arrays;
+
+
+/**
+ * @author snluong
+ */
+public class OrderNotificationServiceTest {
+    private static final String PARENT1_UUID = "f277bb38-272c-471c-a28a-9887a1328a1f";
+    private static final String PARENT2_UUID = "75e29173-2e4d-418d-a9a6-caa68413edaf";
+    private static final String EMAIL = "user1@example.com";
+    private static final String USERNAME = "user1";
+    private PID parentPid1;
+    private PID parentPid2;
+    private AgentPrincipals agent = new AgentPrincipalsImpl(USERNAME, new AccessGroupSetImpl("agroup"));
+    private MultiParentOrderRequest request = new MultiParentOrderRequest();
+    private OrderNotificationService orderNotificationService;
+
+    @Mock
+    private EmailHandler emailHandler;
+    @Mock
+    private OrderNotificationBuilder orderNotificationBuilder;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        request.setAgent(agent);
+        request.setOperation(OrderOperationType.SET);
+        parentPid1 = PIDs.get(PARENT1_UUID);
+        parentPid2 = PIDs.get(PARENT2_UUID);
+        orderNotificationService = new OrderNotificationService();
+        orderNotificationService.setOrderNotificationBuilder(orderNotificationBuilder);
+        orderNotificationService.setEmailHandler(emailHandler);
+    }
+    @Test
+    public void sendResultsSendsEmail() {
+        when(orderNotificationBuilder.construct(any(), any(), any())).thenReturn("Hi there");
+        request.setEmail(EMAIL);
+        var successes = Arrays.asList(parentPid1, parentPid2);
+        var errors = Arrays.asList("First error", "Another error oh no");
+        orderNotificationService.sendResults(request, successes, errors);
+        assertEmailSent();
+    }
+    @Test
+    public void doNotSendResultsEmailIfNoEmailAddress() {
+        var successes = Arrays.asList(parentPid1, parentPid2);
+        var errors = Arrays.asList("First error", "Another error oh no");
+        orderNotificationService.sendResults(request, successes, errors);
+        assertEmailNotSent();
+    }
+
+    private void assertEmailSent() {
+        verify(emailHandler, times(1)).sendEmail(
+                eq(EMAIL), any(), eq("Hi there"), isNull(String.class), isNull(File.class));
+    }
+
+    private void assertEmailNotSent() {
+        verify(emailHandler, never()).sendEmail(any(), any(), any(), any(), any());
+    }
+}
