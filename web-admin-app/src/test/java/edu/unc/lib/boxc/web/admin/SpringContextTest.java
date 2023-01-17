5bd104affe8772b9541ec03c31b147086ef2eb64
krwong <krwong@email.unc.edu>
2023-01-17 14:02:38 -0500

    upgrade web-admin-app (junit 5 dependencies, modify imports/annotations)

--- web-admin-app/src/test/java/edu/unc/lib/boxc/web/admin/SpringContextTest.java
+++ web-admin-app/src/test/java/edu/unc/lib/boxc/web/admin/SpringContextTest.java
@@ -1,18 +1,18 @@
 package edu.unc.lib.boxc.web.admin;
 
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/solr-search-context.xml",
         "file:src/main/webapp/WEB-INF/uiapp-servlet.xml",
         "file:src/main/webapp/WEB-INF/service-context.xml",
         "file:src/main/webapp/WEB-INF/access-fedora-context.xml" })
 public class SpringContextTest {
-    @BeforeClass
+    @BeforeAll
     public static void setup() {
         System.setProperty("acl.properties.uri", "acl-config.properties");
     }
