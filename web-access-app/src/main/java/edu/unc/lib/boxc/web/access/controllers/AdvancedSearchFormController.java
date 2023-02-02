0df95cb147e98a315a94291c0d06ce6c74991700
lfarrell <lfarrell@email.unc.edu>
2023-02-02 15:40:35 -0500

    Switch advanced search to vue js

--- web-access-app/src/main/java/edu/unc/lib/boxc/web/access/controllers/AdvancedSearchFormController.java
+++ web-access-app/src/main/java/edu/unc/lib/boxc/web/access/controllers/AdvancedSearchFormController.java
@@ -1,5 +1,7 @@
 package edu.unc.lib.boxc.web.access.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.unc.lib.boxc.auth.api.models.AccessGroupSet;
 import edu.unc.lib.boxc.auth.fcrepo.services.GroupsThreadStore;
 import edu.unc.lib.boxc.search.api.ContentCategory;
@@ -7,15 +9,19 @@ import edu.unc.lib.boxc.search.api.requests.SearchState;
 import edu.unc.lib.boxc.search.solr.responses.SearchResultResponse;
 import edu.unc.lib.boxc.search.solr.utils.SearchStateUtil;
 import edu.unc.lib.boxc.web.common.controllers.AbstractErrorHandlingSearchController;
+import edu.unc.lib.boxc.web.common.utils.SerializationUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
  * Handles requests to the advanced search page, sending users to the form if there are no
@@ -24,31 +30,35 @@ import java.util.stream.Collectors;
  * @author bbpennel
  */
 @Controller
-@RequestMapping("/advancedSearch")
 public class AdvancedSearchFormController extends AbstractErrorHandlingSearchController {
-    private List<String> FORMAT_VALUES = Arrays.stream(ContentCategory.values())
+    private final List<String> FORMAT_VALUES = Arrays.stream(ContentCategory.values())
             .map(ContentCategory::getDisplayName)
             .sorted()
             .collect(Collectors.toList());
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(path = "/collections", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+    public @ResponseBody String getCollections() {
+        AccessGroupSet accessGroups = GroupsThreadStore.getPrincipals();
+        SearchResultResponse collectionResultResponse = queryLayer.getCollectionList(accessGroups);
+        return SerializationUtil.resultsToJSON(collectionResultResponse, accessGroups);
+    }
+
+    @RequestMapping(path = "/formats", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+    public @ResponseBody String getFormats() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(FORMAT_VALUES);
+    }
+
+    @RequestMapping(path = "/advancedSearch", method = RequestMethod.GET)
     public String handleRequest(Model model, HttpServletRequest request) {
         //If the user is coming to this servlet without any parameters set then send them to form.
         if (request.getQueryString() == null || request.getQueryString().length() == 0) {
-            //Populate the list of collections for the advanced search page drop down
-            AccessGroupSet accessGroups = GroupsThreadStore.getPrincipals();
-
-            SearchResultResponse collectionResultResponse = queryLayer.getCollectionList(accessGroups);
-            model.addAttribute("collectionList", collectionResultResponse.getResultList());
-            model.addAttribute("pageSubtitle", "Advanced Search");
-            model.addAttribute("formats", FORMAT_VALUES);
             return "advancedSearch";
         }
 
         // If the user has submitted the search form, then generate a search state
         // and forward them to the search servlet.
         SearchState searchState = searchStateFactory.createSearchStateAdvancedSearch(request.getParameterMap());
-
         model.addAllAttributes(SearchStateUtil.generateStateParameters(searchState));
 
         return "redirect:/search";
