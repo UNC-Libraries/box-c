f97df71705a7d5a60dbbe55f72db038bc35e4b77
krwong <krwong@email.unc.edu>
2023-02-15 16:56:24 -0500

    fix cdrFooter tests

--- static/js/vue-cdr-access/tests/unit/cdrFooter.spec.js
+++ static/js/vue-cdr-access/tests/unit/cdrFooter.spec.js
@@ -1,37 +1,18 @@
-import { mount } from '@vue/test-utils';
-import { createRouter, createWebHistory } from 'vue-router';
+import { shallowMount } from '@vue/test-utils';
 import cdrFooter from '@/components/cdrFooter.vue';
-import moxios from "moxios";
-
-let wrapper, router;
 
 describe('cdrFooter.vue', () => {
 
-    beforeEach(() => {
-        moxios.install();
-
-        router = createRouter({
-            history: createWebHistory(process.env.BASE_URL),
-            routes: [
-                {
-                    component: cdrFooter
-                }
-            ]
-        });
-        wrapper = mount(cdrFooter, {
-            global: {
-                plugins: [router]
-            }
-        });
-    });
-
-    afterEach(function () {
-        moxios.uninstall();
-    });
-
     it("loads the cdrFooter", () => {
-        wrapper.find('cdrFooter');
-        expect(wrapper.find('cdrFooter').exists()).toBe(true);
+        const wrapper = shallowMount(cdrFooter);
+        wrapper.find('footer');
+        expect(wrapper.find('footer').exists()).toBe(true);
+    });
+
+    it("verify Home url", () => {
+        const homeUrl = "https://dcr.lib.unc.edu/";
+        const wrapper = shallowMount(cdrFooter);
+        expect(wrapper.html()).toContain(homeUrl);
     });
 
 });
