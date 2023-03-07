e2827320357e4927e49e474f3a4a7241288be91a
Sharon Luong <snluong@email.lib.unc.edu>
2023-03-07 13:50:31 -0500

    BXC-4009 working more on tests

--- web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/DownloadImageControllerIT.java
+++ web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/DownloadImageControllerIT.java
@@ -1,20 +1,16 @@
 
 package edu.unc.lib.boxc.web.services.rest;
 
-import edu.unc.lib.boxc.auth.api.Permission;
 import edu.unc.lib.boxc.auth.api.exceptions.AccessRestrictionException;
 import edu.unc.lib.boxc.auth.api.services.AccessControlService;
 import edu.unc.lib.boxc.auth.fcrepo.models.AccessGroupSetImpl;
 import edu.unc.lib.boxc.model.api.ids.PID;
-import edu.unc.lib.boxc.model.api.objects.FolderObject;
-import edu.unc.lib.boxc.model.api.rdf.Premis;
-import edu.unc.lib.boxc.model.fcrepo.ids.AgentPids;
+import edu.unc.lib.boxc.search.api.models.Datastream;
 import edu.unc.lib.boxc.search.api.requests.SimpleIdRequest;
 import edu.unc.lib.boxc.search.solr.models.ContentObjectSolrRecord;
 import edu.unc.lib.boxc.web.common.services.SolrQueryLayerService;
 import edu.unc.lib.boxc.web.services.rest.modify.AbstractAPIIT;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -22,21 +18,19 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.util.NestedServletException;
 
-import static edu.unc.lib.boxc.auth.api.Permission.viewHidden;
-import static edu.unc.lib.boxc.auth.api.Permission.viewMetadata;
 import static edu.unc.lib.boxc.auth.api.Permission.viewOriginal;
-import static edu.unc.lib.boxc.model.api.DatastreamType.MD_EVENTS;
 import static edu.unc.lib.boxc.web.common.services.FedoraContentService.CONTENT_DISPOSITION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -57,11 +51,13 @@ public class DownloadImageControllerIT extends AbstractAPIIT {
     public void testGetImageAtFullSize() throws Exception {
         PID filePid = makePid();
         ContentObjectSolrRecord record = mock(ContentObjectSolrRecord.class);
+        Datastream datastream = mock(Datastream.class);
 
         when(solrSearchService.getObjectById(any(SimpleIdRequest.class))).thenReturn(record);
-        when(record.getDatastreamObject(filePid.getId()).getExtent()).thenReturn("800x1200");
+        when(record.getDatastreamObject(filePid.getId())).thenReturn(datastream);
+        when(datastream.getExtent()).thenReturn("800x1200");
 
-        MvcResult result = mvc.perform(get("/file/" + filePid.getId() + "/full"))
+        MvcResult result = mvc.perform(get("/downloadImage/" + filePid.getId() + "/full"))
                 .andExpect(status().is2xxSuccessful())
                 .andReturn();
 
@@ -76,14 +72,19 @@ public class DownloadImageControllerIT extends AbstractAPIIT {
     public void testFullSizeAccessImageNoPermissions() throws Exception {
         PID filePid = makePid();
         ContentObjectSolrRecord record = mock(ContentObjectSolrRecord.class);
+        when(solrSearchService.getObjectById(any(SimpleIdRequest.class))).thenReturn(record);
         doThrow(new AccessRestrictionException()).when(accessControlService)
                 .assertHasAccess(anyString(), eq(filePid), any(AccessGroupSetImpl.class), eq(viewOriginal));
 
-        MvcResult result = mvc.perform(get("/file/" + filePid.getId() + "/full"))
+        MvcResult result = mvc.perform(get("/downloadImage/" + filePid.getId() + "/full"))
                 .andExpect(status().isForbidden())
                 .andReturn();
 
         MockHttpServletResponse response = result.getResponse();
         assertEquals("", response.getContentAsString(), "Content must not be returned");
+//        Throwable exception = assertThrows(NestedServletException.class, () -> mvc.perform(get("/downloadImage/" + filePid.getId() + "/full"))
+//                .andExpect(status().isForbidden())
+//                .andReturn());
+        assertInstanceOf(AccessRestrictionException.class, result.getResolvedException().getClass());
     }
 }
