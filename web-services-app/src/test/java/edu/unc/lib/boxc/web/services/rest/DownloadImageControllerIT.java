6aa40c7a05e4e143f18c121c97f1f36dbe826a29
Sharon Luong <snluong@email.lib.unc.edu>
2023-03-08 17:00:14 -0500

    BXC-4009 working on test server connections

--- web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/DownloadImageControllerIT.java
+++ web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/DownloadImageControllerIT.java
@@ -1,6 +1,7 @@
 
 package edu.unc.lib.boxc.web.services.rest;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import edu.unc.lib.boxc.auth.api.exceptions.AccessRestrictionException;
 import edu.unc.lib.boxc.auth.api.services.AccessControlService;
 import edu.unc.lib.boxc.auth.fcrepo.models.AccessGroupSetImpl;
@@ -9,22 +10,24 @@ import edu.unc.lib.boxc.search.api.models.Datastream;
 import edu.unc.lib.boxc.search.api.requests.SimpleIdRequest;
 import edu.unc.lib.boxc.search.solr.models.ContentObjectSolrRecord;
 import edu.unc.lib.boxc.web.common.services.SolrQueryLayerService;
+import edu.unc.lib.boxc.web.services.processing.DownloadImageService;
 import edu.unc.lib.boxc.web.services.rest.modify.AbstractAPIIT;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.util.NestedServletException;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static edu.unc.lib.boxc.auth.api.Permission.viewOriginal;
 import static edu.unc.lib.boxc.web.common.services.FedoraContentService.CONTENT_DISPOSITION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -33,6 +36,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
 /**
  * @author snluong
@@ -41,17 +45,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 @ContextConfiguration("/download-image-it-servlet.xml")
 
+@WireMockTest(httpPort = 46887)
 public class DownloadImageControllerIT extends AbstractAPIIT {
     @Autowired
     private AccessControlService accessControlService;
     @Autowired
     private SolrQueryLayerService solrSearchService;
 
+    private String iiifBasePath = "http://localhost:46887/";
+
     @Test
     public void testGetImageAtFullSize() throws Exception {
         PID filePid = makePid();
         ContentObjectSolrRecord record = mock(ContentObjectSolrRecord.class);
         Datastream datastream = mock(Datastream.class);
+
+        stubFor(WireMock.get(urlMatching(iiifBasePath + "*"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())));
 
         when(solrSearchService.getObjectById(any(SimpleIdRequest.class))).thenReturn(record);
         when(record.getDatastreamObject(filePid.getId())).thenReturn(datastream);
@@ -81,10 +92,6 @@ public class DownloadImageControllerIT extends AbstractAPIIT {
                 .andReturn();
 
         MockHttpServletResponse response = result.getResponse();
-        assertEquals("", response.getContentAsString(), "Content must not be returned");
-//        Throwable exception = assertThrows(NestedServletException.class, () -> mvc.perform(get("/downloadImage/" + filePid.getId() + "/full"))
-//                .andExpect(status().isForbidden())
-//                .andReturn());
-        assertInstanceOf(AccessRestrictionException.class, result.getResolvedException().getClass());
+        assertEquals("Insufficient permissions", response.getContentAsString());
     }
 }
