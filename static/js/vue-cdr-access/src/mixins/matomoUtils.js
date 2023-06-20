c693a9959cbf4b9473f63354799a3a56bced8e60
lfarrell <lfarrell@email.unc.edu>
2023-06-20 14:13:23 -0400

    Get the url from ansible/vite

--- static/js/vue-cdr-access/src/mixins/matomoUtils.js
+++ static/js/vue-cdr-access/src/mixins/matomoUtils.js
@@ -1,10 +1,12 @@
 export default {
     methods: {
         matomoSetup() {
+            const matomoTrackingUrl = import.meta.env.VITE_MATOMO_API_URL || '';
+
             let _mtm = window._mtm = window._mtm || [];
             _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
             let d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src='https://analytics-qa.lib.unc.edu/js/container_tNSvp9t8.js'; s.parentNode.insertBefore(g,s);
+            g.async=true; g.src=matomoTrackingUrl; s.parentNode.insertBefore(g,s);
         }
     }
 }
