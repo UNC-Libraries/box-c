14baae0a6b862982649fae698a1e957a2afd221a
lfarrell <lfarrell@email.unc.edu>
2024-01-09 13:29:24 -0500

    Move about page text to translations file

--- static/js/vue-cdr-access/src/translations.js
+++ static/js/vue-cdr-access/src/translations.js
@@ -113,6 +113,71 @@ export default {
         works_only: {
             show: "Show only works",
             show_tip: "Show all files without organizational folders."
+        },
+        about: {
+            contact_us: "You can also contact us:",
+            headers: {
+                main: "About this Repository",
+                more_info: "How can I get more information or assistance?",
+                preservation: "How We Preserve Digital Materials",
+                related_collections: "Related Wilson Special Collection Library Collections",
+                responsibilities: "What are my responsibilities when using the digital files available through this website?",
+                usage: "Policies Governing Use"
+            },
+            description: "The University of North Carolina at Chapel Hill Library’s Digital Collections Repository contains a portion of" +
+                " the Wilson Special Collection’s Library digital manuscript collections, university records, and publications." +
+                " The Repository also contains select scholarly projects from university departments.",
+            preservation: {
+                text_one: "Much of the material available in the Digital Collections Repository were acquired by the Library on various" +
+                    " types of digital storage devices and via cloud storage. The storage devices range from floppy disks to DVDs" +
+                    " to USB drives to Dropbox. In order to preserve the materials effectively we must carefully migrate the digital" +
+                    " files to storage that is contemporary and optimized for long-term preservation. We employ best practices to" +
+                    " analyze and migrate digital files from donated storage devices to the Digital Collections Repository" +
+                    " for preservation and access.",
+                text_two: "As a result of the legacy nature of some of the digital materials we receive, some digital files are difficult" +
+                    " to access with contemporary software. This also means that some files may be inaccessible at present. As it" +
+                    " becomes possible to do so, we will expand and improve access to legacy file formats based on emerging best" +
+                    " practices and tool development.",
+                text_three: "If you have questions about access to files, file formats, or storage devices used to transfer the materials" +
+                    " to the Library, please contact us for more information."
+            },
+            responsibilities: {
+                text: "Many collection materials in the DCR are made available openly or by ONYEN authentication without requiring" +
+                    " registration with the Wilson Special Collections Library. However, the materials are still governed by the" +
+                    " Wilson Special Collections Library's Policies Governing Use. When you are use materials from the Repository," +
+                    " you should acknowledge and follow the following policies:"
+            },
+            sub_headers: {
+                description: "Descriptions of collections (digital or physical) can be searched online at:",
+                docsouth: "DocSouth Data:",
+                scans: "Scanned images of collection material, curated online exhibits, and virtual museum content:",
+                websites: "Archived websites:"
+            },
+            usage: {
+                text_one: "Researchers agree to abide by restrictions placed on the library’s collections.",
+                text_two: "Collection materials including unpublished documents may be subject to laws governing copyright" +
+                    " (Title 17, U.S. Code). Digital files are made for private study and reference. Permission of the copyright" +
+                    " holder may be required for uses exceeding \"fair use.\" Researchers acknowledge that they are solely responsible" +
+                    " for determining the copyright status of the materials they use.",
+                text_three: "Manuscript collections and archival records that include twentieth and twenty-first century materials may" +
+                    " contain sensitive or confidential information that is protected under federal or state right to privacy laws" +
+                    " and regulations, the North Carolina Public Records Act (N.C.G.S. &sect; 132 1 et seq.), and Article 7 of the North" +
+                    " Carolina State Personnel Act (Privacy of State Employee Personnel Records, N.C.G.S. &sect; 126-22 et seq.)." +
+                    " Researchers are advised that the disclosure of certain information pertaining to identifiable living" +
+                    " individuals without the consent of those individuals may have legal ramifications (e.g., a cause of action" +
+                    " under common law for invasion of privacy may arise if facts concerning an individual's private life are" +
+                    " published that would be deemed highly offensive to a reasonable person).",
+                text_four: "The University Archives at the University of North Carolina at Chapel Hill operates in accordance with the" +
+                    " North Carolina Public Records Act (N.C.G.S. &sect; 132 1 et seq.) providing unrestricted access to university" +
+                    " records not covered by state and federal right to privacy acts.",
+                text_five: "The University of North Carolina at Chapel Hill, and their libraries, assume no responsibility for" +
+                    " infringement of literary property rights and copyright or for liability to any person for defamation or" +
+                    " invasion of privacy that results from a researcher's use of collections.",
+                text_six: "Researchers agree to indemnify and hold harmless the University of North Carolina at Chapel Hill, and their" +
+                    " officers, employees, and agents from and against all suits, claims, actions, and expenses arising out of use" +
+                    " of collections held by the libraries."
+            },
+            website: "You can visit our website:"
         }
     }
 }
