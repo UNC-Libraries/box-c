508d7d54a31b186b909aa28609a4e047a80383f3
sharonl <6546457+sharonluong@users.noreply.github.com>
2024-02-22 10:46:49 -0500

    BXC-4430 working on tests

--- web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/modify/ViewSettingIT.java
+++ web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/modify/ViewSettingIT.java
@@ -2,14 +2,19 @@ package edu.unc.lib.boxc.web.services.rest.modify;
 
 import edu.unc.lib.boxc.auth.api.Permission;
 import edu.unc.lib.boxc.auth.api.exceptions.AccessRestrictionException;
+import edu.unc.lib.boxc.auth.api.models.AccessGroupSet;
 import edu.unc.lib.boxc.auth.api.services.AccessControlService;
+import edu.unc.lib.boxc.auth.fcrepo.models.AccessGroupSetImpl;
 import edu.unc.lib.boxc.auth.fcrepo.services.GroupsThreadStore;
-import edu.unc.lib.boxc.model.api.objects.RepositoryObjectLoader;
-import edu.unc.lib.boxc.model.api.objects.WorkObject;
+import edu.unc.lib.boxc.model.api.ResourceType;
+import edu.unc.lib.boxc.model.api.ids.PID;
+import edu.unc.lib.boxc.model.api.services.RepositoryObjectFactory;
+import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
+import edu.unc.lib.boxc.search.solr.models.ContentObjectSolrRecord;
 import edu.unc.lib.boxc.web.services.rest.exceptions.RestResponseEntityExceptionHandler;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.http.MediaType;
@@ -23,14 +28,18 @@ import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ViewSettingIT extends AbstractAPIIT{
+public class ViewSettingIT {
     @InjectMocks
     private ViewSettingController controller;
     @Mock
     private AccessControlService accessControlService;
     private MockMvc mockMvc;
     private AutoCloseable closeable;
-    private WorkObject workObject;
+    private ContentObjectSolrRecord workObject;
+    private final static String USERNAME = "test_user";
+    private final static AccessGroupSet GROUPS = new AccessGroupSetImpl("adminGroup");
+    private static final String OBJECT_ID = "f277bb38-272c-471c-a28a-9887a1328a1f";
+    private static final PID OBJECT_PID = PIDs.get(OBJECT_ID);
 
     @BeforeEach
     public void setup() {
@@ -38,9 +47,12 @@ public class ViewSettingIT extends AbstractAPIIT{
         mockMvc = MockMvcBuilders.standaloneSetup(controller)
                 .setControllerAdvice(new RestResponseEntityExceptionHandler())
                 .build();
+        workObject = new ContentObjectSolrRecord();
+        workObject.setId(OBJECT_ID);
+        workObject.setResourceType(ResourceType.Work.name());
+        workObject.setTitle("Test Work");
         GroupsThreadStore.storeUsername(USERNAME);
         GroupsThreadStore.storeGroups(GROUPS);
-        workObject = repositoryObjectFactory.createWorkObject(null);
     }
 
     @AfterEach
@@ -58,7 +70,7 @@ public class ViewSettingIT extends AbstractAPIIT{
         doThrow(new AccessRestrictionException()).when(accessControlService)
                 .assertHasAccess(eq(workObject.getPid()), any(), eq(Permission.viewHidden));
 
-        mockMvc.perform(get("/edit/viewSettings/" + workObject.getPid())
+        mockMvc.perform(get("/edit/viewSettings/" + OBJECT_ID)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isForbidden());
     }
