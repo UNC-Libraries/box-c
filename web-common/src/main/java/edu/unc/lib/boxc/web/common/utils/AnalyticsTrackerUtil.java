71f58652effeff8e834809666e29a4d7671c9cf1
Ben Pennell <bbpennel@email.unc.edu>
2024-03-08 11:44:53 -0500

    BXC-4497 - Temporarily disabling setting of a random user id (#1695)
    * Temporarily disabling setting of a random user id
    
    * Add comment

--- web-common/src/main/java/edu/unc/lib/boxc/web/common/utils/AnalyticsTrackerUtil.java
+++ web-common/src/main/java/edu/unc/lib/boxc/web/common/utils/AnalyticsTrackerUtil.java
@@ -167,7 +167,8 @@ public class AnalyticsTrackerUtil {
 
             // if it cannot be found in the cookie, generate a random 16 character hex user ID
             if (StringUtils.isBlank(uid)) {
-                uid = generateUserId();
+                // Commented out temporarily while we debug a resource exhaustion issue
+//                uid = generateUserId();
             }
 
             userAgent = request.getHeader("User-Agent");
