f16687e27c15501ce9a6806c41625a875c9a33a0
sharonl <6546457+sharonluong@users.noreply.github.com>
2024-04-03 16:36:03 -0400

    BXC-4531 add GET for streaming properties

--- web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/modify/StreamingPropertiesIT.java
+++ web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/modify/StreamingPropertiesIT.java
@@ -7,27 +7,39 @@ import edu.unc.lib.boxc.auth.api.services.AccessControlService;
 import edu.unc.lib.boxc.auth.fcrepo.models.AccessGroupSetImpl;
 import edu.unc.lib.boxc.auth.fcrepo.services.GroupsThreadStore;
 import edu.unc.lib.boxc.model.api.ids.PID;
+import edu.unc.lib.boxc.model.api.objects.FileObject;
+import edu.unc.lib.boxc.model.api.objects.RepositoryObjectLoader;
+import edu.unc.lib.boxc.model.api.objects.WorkObject;
+import edu.unc.lib.boxc.model.api.rdf.Cdr;
+import edu.unc.lib.boxc.model.api.rdf.CdrView;
 import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
 import edu.unc.lib.boxc.operations.jms.streaming.StreamingPropertiesRequestSender;
 import edu.unc.lib.boxc.web.services.rest.MvcTestHelpers;
 import edu.unc.lib.boxc.web.services.rest.exceptions.RestResponseEntityExceptionHandler;
+import org.apache.hadoop.fs.DU;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Map;
 
+import static edu.unc.lib.boxc.operations.jms.streaming.StreamingPropertiesRequest.DURACLOUD;
 import static edu.unc.lib.boxc.operations.jms.streaming.StreamingPropertiesRequest.OPEN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,7 +49,21 @@ public class StreamingPropertiesIT {
     @Mock
     private AccessControlService accessControlService;
     @Mock
+    private RepositoryObjectLoader repositoryObjectLoader;
+    @Mock
     private StreamingPropertiesRequestSender streamingPropertiesRequestSender;
+    @Mock
+    private WorkObject workObject;
+    @Mock
+    private FileObject fileObject;
+    @Mock
+    private Resource resource;
+    @Mock
+    private Statement filenameStatement;
+    @Mock
+    private Statement hostStatement;
+    @Mock
+    private Statement folderStatement;
     private MockMvc mockMvc;
     private AutoCloseable closeable;
     private final static String USERNAME = "test_user";
@@ -57,6 +83,49 @@ public class StreamingPropertiesIT {
     @AfterEach
     void closeService() throws Exception {
         closeable.close();
+    }
+
+    @Test
+    public void testGetStreamingPropertiesNoPermission() throws Exception {
+        doThrow(new AccessRestrictionException()).when(accessControlService)
+                .assertHasAccess(anyString(), eq(FILE_PID), any(), eq(Permission.viewHidden));
+
+        mockMvc.perform(get("/edit/streamingProperties/" + FILE_ID)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void testGetStreamingPropertiesNotAFileObject() throws Exception {
+        var workId = "ba70a1ee-fa7c-437f-a979-cc8b16599652";
+        when(repositoryObjectLoader.getRepositoryObject(eq(PIDs.get(workId)))).thenReturn(workObject);
+
+        mockMvc.perform(get("/edit/streamingProperties/" + workId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetStreamingPropertiesSuccess() throws Exception {
+        var filename = "banjo-playlist.m3u8";
+        when(repositoryObjectLoader.getRepositoryObject(eq(FILE_PID))).thenReturn(fileObject);
+        when(fileObject.getResource()).thenReturn(resource);
+        when(resource.getProperty(eq(Cdr.streamingFile))).thenReturn(filenameStatement);
+        when(filenameStatement.getString()).thenReturn(filename);
+        when(resource.getProperty(eq(Cdr.streamingFolder))).thenReturn(folderStatement);
+        when(folderStatement.getString()).thenReturn(OPEN);
+        when(resource.getProperty(eq(Cdr.streamingHost))).thenReturn(hostStatement);
+        when(hostStatement.getString()).thenReturn(DURACLOUD);
+
+        var result = mockMvc.perform(get("/edit/streamingProperties/" + FILE_ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn();
+
+        Map<String, Object> respMap = MvcTestHelpers.getMapFromResponse(result);
+        assertEquals(filename, respMap.get("filename"));
+        assertEquals(OPEN, respMap.get("folder"));
+        assertEquals(DURACLOUD, respMap.get("host"));
     }
 
     @Test
