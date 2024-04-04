023ae52b1b17a40239253d41a31e73179dbcd79c
sharonl <6546457+sharonluong@users.noreply.github.com>
2024-04-04 11:46:33 -0400

    Bxc 4531 get endpoint (#1716)
    * BXC-4531 add GET for streaming properties
    
    * BXC-4531 remove unused imports
    
    * Update StreamingPropertiesController.java
    
    ---------
    
    Co-authored-by: Ben Pennell <bbpennel@email.unc.edu>

--- web-services-app/src/main/java/edu/unc/lib/boxc/web/services/rest/modify/StreamingPropertiesController.java
+++ web-services-app/src/main/java/edu/unc/lib/boxc/web/services/rest/modify/StreamingPropertiesController.java
@@ -4,15 +4,24 @@ import com.apicatalog.jsonld.StringUtils;
 import edu.unc.lib.boxc.auth.api.Permission;
 import edu.unc.lib.boxc.auth.api.models.AccessGroupSet;
 import edu.unc.lib.boxc.auth.api.services.AccessControlService;
+import edu.unc.lib.boxc.model.api.exceptions.InvalidOperationForObjectType;
+import edu.unc.lib.boxc.model.api.ids.PID;
+import edu.unc.lib.boxc.model.api.objects.FileObject;
+import edu.unc.lib.boxc.model.api.objects.RepositoryObjectLoader;
+import edu.unc.lib.boxc.model.api.rdf.Cdr;
 import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
 import edu.unc.lib.boxc.operations.jms.streaming.StreamingPropertiesRequest;
 import edu.unc.lib.boxc.operations.jms.streaming.StreamingPropertiesRequestSender;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -35,6 +44,30 @@ public class StreamingPropertiesController {
     private AccessControlService accessControlService;
     @Autowired
     private StreamingPropertiesRequestSender streamingPropertiesRequestSender;
+    @Autowired
+    private RepositoryObjectLoader repositoryObjectLoader;
+
+    @GetMapping(value = "/edit/streamingProperties/{id}", produces = APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<Object> getStreamingProperties(@PathVariable("id") String id) {
+        PID pid = PIDs.get(id);
+        // Check if the user is allowed to view this object
+        AccessGroupSet principals = getAgentPrincipals().getPrincipals();
+        accessControlService.assertHasAccess("Insufficient permissions to get streaming properties for " + id,
+                pid, principals, Permission.viewHidden);
+
+        // check if object is a FileObject
+        var repositoryObject = repositoryObjectLoader.getRepositoryObject(pid);
+        if (!(repositoryObject instanceof FileObject)) {
+            throw new InvalidOperationForObjectType("Cannot get streaming properties of type " +
+                    repositoryObject.getClass().getName() + ", as only FileObjects have streaming properties");
+        }
+
+        var resource = repositoryObject.getResource();
+        var properties = getProperties(id, resource);
+
+        return new ResponseEntity<>(properties, HttpStatus.OK);
+    }
 
     @PutMapping(value = "/edit/streamingProperties", produces = APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -77,5 +110,22 @@ public class StreamingPropertiesController {
         }
 
         return false;
+    }
+
+    private String getValue(Resource resource, Property property) {
+        var propValue = resource.getProperty(property);
+        return propValue == null ? null : propValue.getString();
+    }
+    private Map<String, String> getProperties(String id, Resource resource) {
+        Map<String, String> result = new HashMap<>();
+        var host = getValue(resource, Cdr.streamingHost);
+        var filename = getValue(resource, Cdr.streamingFile);
+        var folder = getValue(resource, Cdr.streamingFolder);
+
+        result.put("id", id);
+        result.put("host", host);
+        result.put("filename", filename);
+        result.put("folder", folder);
+        return result;
     }
 }
