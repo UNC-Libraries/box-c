a5a44b8134afa51033c205e9813cc2d20fbdeafe
Ben Pennell <bbpennel@email.unc.edu>
2024-04-29 12:10:38 -0400

    Update destroy route

--- services-camel-app/src/main/java/edu/unc/lib/boxc/services/camel/destroy/DestroyObjectsRouter.java
+++ services-camel-app/src/main/java/edu/unc/lib/boxc/services/camel/destroy/DestroyObjectsRouter.java
@@ -1,12 +1,13 @@
 package edu.unc.lib.boxc.services.camel.destroy;
 
-import static org.apache.camel.LoggingLevel.DEBUG;
-import static org.slf4j.LoggerFactory.getLogger;
-
 import org.apache.camel.BeanInject;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
+
+import static org.apache.camel.LoggingLevel.DEBUG;
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Route to execute requests to destroy repository objects
@@ -20,25 +21,72 @@ public class DestroyObjectsRouter extends RouteBuilder {
     @BeanInject(value = "destroyObjectsProcessor")
     private DestroyObjectsProcessor destroyObjectsProcessor;
 
+    private String cdrDestroyStreamCamel;
+    private String cdrDestroyPostStreamCamel;
+    private String cdrDestroyDerivativesStreamCamel;
+    private String longleafDeregisterEndpoint;
+
+    private long errorRetryDelay;
+    private int errorMaxRedeliveries;
+    private int errorBackOffMultiplier;
+
     @Override
     public void configure() throws Exception {
         onException(Exception.class)
-                .redeliveryDelay("{{error.retryDelay}}")
-                .maximumRedeliveries("{{error.maxRedeliveries}}")
-                .backOffMultiplier("{{error.backOffMultiplier}}")
+                .redeliveryDelay(errorRetryDelay)
+                .maximumRedeliveries(errorMaxRedeliveries)
+                .backOffMultiplier(errorBackOffMultiplier)
                 .retryAttemptedLogLevel(LoggingLevel.WARN);
 
-        from("{{cdr.destroy.stream.camel}}")
+        from(cdrDestroyStreamCamel)
             .routeId("CdrDestroyObjects")
             .log(DEBUG, log, "Received destroy objects message")
             .bean(destroyObjectsProcessor);
 
-        from("{{cdr.destroy.post.stream.camel}}")
+        from(cdrDestroyPostStreamCamel)
             .routeId("CdrDestroyObjectsCleanup")
             .log(DEBUG, log, "Received destroy objects cleanup message")
-            .multicast()
-            .parallelProcessing()
-            .to("activemq://activemq:queue:filter.longleaf.deregister",
-                    "{{cdr.destroy.derivatives.stream.camel}}");
+            .multicast().parallelProcessing()
+                .to(longleafDeregisterEndpoint)
+                .to(cdrDestroyDerivativesStreamCamel);
+    }
+
+    @PropertyInject("error.retryDelay:500")
+    public void setErrorRetryDelay(long errorRetryDelay) {
+        this.errorRetryDelay = errorRetryDelay;
+    }
+
+    @PropertyInject("error.maxRedeliveries:10")
+    public void setErrorMaxRedeliveries(int errorMaxRedeliveries) {
+        this.errorMaxRedeliveries = errorMaxRedeliveries;
+    }
+
+    @PropertyInject("error.backOffMultiplier:2")
+    public void setErrorBackOffMultiplier(int errorBackOffMultiplier) {
+        this.errorBackOffMultiplier = errorBackOffMultiplier;
+    }
+
+    public void setDestroyObjectsProcessor(DestroyObjectsProcessor destroyObjectsProcessor) {
+        this.destroyObjectsProcessor = destroyObjectsProcessor;
+    }
+
+    @PropertyInject("cdr.destroy.stream.camel")
+    public void setCdrDestroyStreamCamel(String cdrDestroyStreamCamel) {
+        this.cdrDestroyStreamCamel = cdrDestroyStreamCamel;
+    }
+
+    @PropertyInject("cdr.destroy.post.stream.camel")
+    public void setCdrDestroyPostStreamCamel(String cdrDestroyPostStreamCamel) {
+        this.cdrDestroyPostStreamCamel = cdrDestroyPostStreamCamel;
+    }
+
+    @PropertyInject("cdr.destroy.derivatives.stream.camel")
+    public void setCdrDestroyDerivativesStreamCamel(String cdrDestroyDerivativesStreamCamel) {
+        this.cdrDestroyDerivativesStreamCamel = cdrDestroyDerivativesStreamCamel;
+    }
+
+    @PropertyInject("longleaf.filter.deregister")
+    public void setLongleafDeregisterEndpoint(String longleafDeregisterEndpoint) {
+        this.longleafDeregisterEndpoint = longleafDeregisterEndpoint;
     }
 }
--- services-camel-app/src/test/java/edu/unc/lib/boxc/services/camel/destroy/DestroyObjectsRouterTest.java
+++ services-camel-app/src/test/java/edu/unc/lib/boxc/services/camel/destroy/DestroyObjectsRouterTest.java
@@ -1,38 +1,8 @@
 package edu.unc.lib.boxc.services.camel.destroy;
 
-import static edu.unc.lib.boxc.operations.jms.destroy.DestroyObjectsHelper.serializeDestroyRequest;
-import static org.apache.jena.rdf.model.ModelFactory.createDefaultModel;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
-
-import java.util.Collections;
-import java.util.UUID;
-
-import edu.unc.lib.boxc.model.api.rdf.Premis;
-import edu.unc.lib.boxc.operations.api.events.PremisEventBuilder;
-import edu.unc.lib.boxc.operations.api.events.PremisLogger;
-import edu.unc.lib.boxc.operations.api.events.PremisLoggerFactory;
-import org.apache.camel.BeanInject;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.builder.AdviceWith;
-import org.apache.camel.test.spring.CamelSpringTestSupport;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.vocabulary.RDF;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.mockito.Mock;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import edu.unc.lib.boxc.auth.api.models.AccessGroupSet;
 import edu.unc.lib.boxc.auth.api.models.AgentPrincipals;
+import edu.unc.lib.boxc.auth.api.services.AccessControlService;
 import edu.unc.lib.boxc.auth.fcrepo.models.AccessGroupSetImpl;
 import edu.unc.lib.boxc.auth.fcrepo.models.AgentPrincipalsImpl;
 import edu.unc.lib.boxc.auth.fcrepo.services.InheritedAclFactory;
@@ -42,11 +12,43 @@ import edu.unc.lib.boxc.model.api.ids.PID;
 import edu.unc.lib.boxc.model.api.objects.RepositoryObjectLoader;
 import edu.unc.lib.boxc.model.api.objects.WorkObject;
 import edu.unc.lib.boxc.model.api.rdf.Cdr;
+import edu.unc.lib.boxc.model.api.rdf.Premis;
 import edu.unc.lib.boxc.model.api.services.RepositoryObjectFactory;
 import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
+import edu.unc.lib.boxc.operations.api.events.PremisEventBuilder;
+import edu.unc.lib.boxc.operations.api.events.PremisLogger;
+import edu.unc.lib.boxc.operations.api.events.PremisLoggerFactory;
+import edu.unc.lib.boxc.operations.jms.MessageSender;
 import edu.unc.lib.boxc.operations.jms.destroy.DestroyObjectsRequest;
+import edu.unc.lib.boxc.operations.jms.indexing.IndexingMessageSender;
 import edu.unc.lib.boxc.search.api.models.ObjectPath;
 import edu.unc.lib.boxc.search.solr.services.ObjectPathFactory;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWith;
+import org.apache.camel.builder.NotifyBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDF;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static edu.unc.lib.boxc.operations.jms.destroy.DestroyObjectsHelper.serializeDestroyRequest;
+import static org.apache.jena.rdf.model.ModelFactory.createDefaultModel;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * See edu.unc.lib.dl.persist.services.destroy.DestroyObjectsJobIT for related integration test
@@ -54,32 +56,37 @@ import edu.unc.lib.boxc.search.solr.services.ObjectPathFactory;
  * @author bbpennel
  *
  */
-public class DestroyObjectsRouterTest extends CamelSpringTestSupport {
+@ExtendWith(MockitoExtension.class)
+public class DestroyObjectsRouterTest extends CamelTestSupport {
     private static final String DESTROY_ROUTE = "CdrDestroyObjects";
 
     private static final String USER_NAME = "user";
     private static final String USER_GROUPS = "edu:lib:staff_grp";
-
-    private AutoCloseable closeable;
 
     @Produce(uri = "direct:start")
     protected ProducerTemplate template;
 
     private AgentPrincipals agent;
 
-    @BeanInject(value = "repositoryObjectLoader")
+    @Mock
     private RepositoryObjectLoader repoObjLoader;
-    @BeanInject(value = "repositoryObjectFactory")
+    @Mock
     private RepositoryObjectFactory repoObjFactory;
-    @BeanInject(value = "inheritedAclFactory")
+    @Mock
     private InheritedAclFactory inheritedAclFactory;
-    @BeanInject(value = "objectPathFactory")
+    @Mock
+    private AccessControlService aclService;
+    @Mock
     private ObjectPathFactory objectPathFactory;
-    @BeanInject(value = "premisLoggerFactory")
+    @Mock
     private PremisLoggerFactory premisLoggerFactory;
-
-    @BeanInject(value = "transactionManager")
+    @Mock
+    private MessageSender binaryDestroyedMessageSender;
+    @Mock
+    private IndexingMessageSender indexingMessageSender;
+    @Mock
     private TransactionManager txManager;
+    private DestroyObjectsProcessor destroyObjectsProcessor;
 
     @Mock
     private WorkObject workObj;
@@ -92,27 +99,38 @@ public class DestroyObjectsRouterTest extends CamelSpringTestSupport {
     @Mock
     private PremisEventBuilder eventBuilder;
 
-    @Before
-    public void setup() {
-        closeable = openMocks(this);
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
         AccessGroupSet testPrincipals = new AccessGroupSetImpl(USER_GROUPS);
         agent = new AgentPrincipalsImpl(USER_NAME, testPrincipals);
 
-        when(txManager.startTransaction()).thenReturn(tx);
-    }
+        destroyObjectsProcessor = new DestroyObjectsProcessor();
+        destroyObjectsProcessor.setRepositoryObjectFactory(repoObjFactory);
+        destroyObjectsProcessor.setRepositoryObjectLoader(repoObjLoader);
+        destroyObjectsProcessor.setInheritedAclFactory(inheritedAclFactory);
+        destroyObjectsProcessor.setObjectPathFactory(objectPathFactory);
+        destroyObjectsProcessor.setPremisLoggerFactory(premisLoggerFactory);
+        destroyObjectsProcessor.setTransactionManager(txManager);
+        destroyObjectsProcessor.setAclService(aclService);
+        destroyObjectsProcessor.setBinaryDestroyedMessageSender(binaryDestroyedMessageSender);
+        destroyObjectsProcessor.setIndexingMessageSender(indexingMessageSender);
 
-    @AfterEach
-    void closeService() throws Exception {
-        closeable.close();
-    }
-
-    @Override
-    protected AbstractApplicationContext createApplicationContext() {
-        return new ClassPathXmlApplicationContext("/service-context.xml", "/destroy-objects-context.xml");
+        var router = new DestroyObjectsRouter();
+        router.setCdrDestroyStreamCamel("direct:destroyObjects");
+        router.setErrorRetryDelay(0);
+        router.setErrorBackOffMultiplier(1);
+        router.setErrorMaxRedeliveries(1);
+        router.setCdrDestroyDerivativesStreamCamel("mock:direct:destroyDerivatives");
+        router.setCdrDestroyPostStreamCamel("direct:destroyPost");
+        router.setLongleafDeregisterEndpoint("mock:direct:longleafDeregister");
+        router.setDestroyObjectsProcessor(destroyObjectsProcessor);
+        return router;
     }
 
     @Test
     public void destroyObject() throws Exception {
+        when(txManager.startTransaction()).thenReturn(tx);
+
         createContext(DESTROY_ROUTE, "direct:start");
         when(premisLoggerFactory.createPremisLogger(any())).thenReturn(premisLogger);
         when(premisLogger.buildEvent(Premis.Deletion)).thenReturn(eventBuilder);
@@ -144,11 +162,24 @@ public class DestroyObjectsRouterTest extends CamelSpringTestSupport {
         verify(repoObjFactory).createOrTransformObject(eq(pid.getRepositoryUri()), any(Model.class));
     }
 
+    @Test
+    public void destroyObjectsCleanupTest() throws Exception {
+        createContext("CdrDestroyObjectsCleanup", "direct:start");
+
+        NotifyBuilder notify = new NotifyBuilder(context)
+                .whenCompleted(3)
+                .create();
+
+        template.sendBody("");
+
+        boolean result1 = notify.matches(5l, TimeUnit.SECONDS);
+        assertTrue("Register route not satisfied", result1);
+        // The mocked routes don't match when using parallel processing
+    }
+
     private void createContext(String routeName, String currentRoute) throws Exception {
         AdviceWith.adviceWith(context, routeName, a -> {
             a.replaceFromWith(currentRoute);
-            a.mockEndpointsAndSkip("*");
         });
-        context.start();
     }
 }
