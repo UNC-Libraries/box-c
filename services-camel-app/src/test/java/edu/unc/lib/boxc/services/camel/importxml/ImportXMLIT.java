efe24b4a5c42be99d84be84caddf144c5e8cea29
Ben Pennell <bbpennel@email.unc.edu>
2024-06-27 16:36:16 -0400

    Updated ImportXMLIT

--- services-camel-app/src/test/java/edu/unc/lib/boxc/services/camel/importxml/ImportXMLIT.java
+++ services-camel-app/src/test/java/edu/unc/lib/boxc/services/camel/importxml/ImportXMLIT.java
@@ -1,5 +1,39 @@
 package edu.unc.lib.boxc.services.camel.importxml;
 
+import com.samskivert.mustache.Template;
+import edu.unc.lib.boxc.auth.api.models.AgentPrincipals;
+import edu.unc.lib.boxc.model.api.objects.ContentObject;
+import edu.unc.lib.boxc.model.api.objects.FolderObject;
+import edu.unc.lib.boxc.model.api.objects.RepositoryObjectLoader;
+import edu.unc.lib.boxc.model.api.services.RepositoryObjectFactory;
+import edu.unc.lib.boxc.model.fcrepo.test.TestHelper;
+import edu.unc.lib.boxc.model.fcrepo.test.TestRepositoryDeinitializer;
+import edu.unc.lib.boxc.operations.impl.importxml.ImportXMLService;
+import edu.unc.lib.boxc.operations.test.ModsTestHelper;
+import edu.unc.lib.boxc.persist.api.storage.StorageLocationManager;
+import edu.unc.lib.boxc.persist.impl.transfer.BinaryTransferServiceImpl;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.NotifyBuilder;
+import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
+import org.fcrepo.client.FcrepoClient;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mock;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import javax.mail.internet.MimeMessage;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
+
 import static edu.unc.lib.boxc.model.api.xml.JDOMNamespaceUtil.MODS_V3_NS;
 import static edu.unc.lib.boxc.persist.impl.storage.StorageLocationTestHelper.newStorageLocationTestHelper;
 import static org.junit.Assert.assertEquals;
@@ -8,109 +42,69 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
-import java.io.InputStream;
-import java.util.concurrent.TimeUnit;
-
-import javax.mail.internet.MimeMessage;
-
-import edu.unc.lib.boxc.model.fcrepo.test.TestRepositoryDeinitializer;
-import org.apache.camel.CamelContext;
-import org.apache.camel.builder.NotifyBuilder;
-import org.fcrepo.client.FcrepoClient;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.input.SAXBuilder;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.rules.TemporaryFolder;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.ContextHierarchy;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.samskivert.mustache.Template;
-
-import edu.unc.lib.boxc.auth.api.models.AgentPrincipals;
-import edu.unc.lib.boxc.model.api.objects.ContentObject;
-import edu.unc.lib.boxc.model.api.objects.FolderObject;
-import edu.unc.lib.boxc.model.api.objects.RepositoryObjectLoader;
-import edu.unc.lib.boxc.model.api.services.RepositoryObjectFactory;
-import edu.unc.lib.boxc.model.fcrepo.test.TestHelper;
-import edu.unc.lib.boxc.operations.impl.importxml.ImportXMLService;
-import edu.unc.lib.boxc.operations.test.ModsTestHelper;
-import edu.unc.lib.boxc.persist.api.storage.StorageLocationManager;
-import edu.unc.lib.boxc.persist.impl.transfer.BinaryTransferServiceImpl;
-
 /**
  * @author bbpennel
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextHierarchy({
-    @ContextConfiguration("/spring-test/cdr-client-container.xml"),
-    @ContextConfiguration("/spring-test/jms-context.xml"),
-    @ContextConfiguration("/import-xml-it-context.xml")
-})
-public class ImportXMLIT {
-
+public class ImportXMLIT extends CamelSpringTestSupport {
     private final static String USER_EMAIL = "user@example.com";
-
     private final static String UPDATED_TITLE = "Updated Work Title";
     private final static String UPDATED_DATE = "2018-04-06";
 
     private AutoCloseable closeable;
 
-    @Rule
-    public final TemporaryFolder tmpFolder = new TemporaryFolder();
+    @TempDir
+    public Path tmpFolder;
 
-    @Autowired
     private String baseAddress;
 
-    @Autowired
     private RepositoryObjectFactory repoObjectFactory;
-    @javax.annotation.Resource(name = "repositoryObjectLoader")
     private RepositoryObjectLoader repoObjectLoader;
 
-    @Autowired
     private CamelContext cdrImportXML;
 
-    @Autowired
     private JmsTemplate importXmlJmsTemplate;
 
     private ImportXMLService importXmlService;
 
     private StorageLocationManager locationManager;
-    @Autowired
     private BinaryTransferServiceImpl binaryTransferService;
-    @Autowired
     private ImportXMLProcessor importXMLProcessor;
-    @Autowired
     private JavaMailSender mailSender;
     @Mock
     private MimeMessage mimeMessage;
-    @Autowired
     private Template updateCompleteTemplate;
-    @Autowired
     private FcrepoClient fcrepoClient;
 
     @Mock
     private AgentPrincipals agent;
 
-    @Before
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("spring-test/cdr-client-container.xml",
+                "spring-test/jms-context.xml",
+                "import-xml-it-context.xml");
+    }
+
+    @BeforeEach
     public void setup() throws Exception {
         closeable = openMocks(this);
+        baseAddress = applicationContext.getBean("baseAddress", String.class);
+        fcrepoClient = applicationContext.getBean(FcrepoClient.class);
+        repoObjectFactory = applicationContext.getBean(RepositoryObjectFactory.class);
+        repoObjectLoader = applicationContext.getBean("repositoryObjectLoader", RepositoryObjectLoader.class);
+        cdrImportXML = applicationContext.getBean("cdrImportXML", CamelContext.class);
+        importXmlJmsTemplate = applicationContext.getBean("importXmlJmsTemplate", JmsTemplate.class);
+        importXMLProcessor = applicationContext.getBean(ImportXMLProcessor.class);
+        mailSender = applicationContext.getBean(JavaMailSender.class);
+        binaryTransferService = applicationContext.getBean(BinaryTransferServiceImpl.class);
+        updateCompleteTemplate = applicationContext.getBean("updateCompleteTemplate", Template.class);
 
         TestHelper.setContentBase(baseAddress);
 
         importXmlService = new ImportXMLService();
         importXmlService.setJmsTemplate(importXmlJmsTemplate);
-        importXmlService.setDataDir(tmpFolder.getRoot().getAbsolutePath());
+        importXmlService.setDataDir(tmpFolder.toString());
         importXmlService.init();
 
         locationManager = newStorageLocationTestHelper()
