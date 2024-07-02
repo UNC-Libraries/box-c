8a0f33d5ee27fc4a391943a83c45729878278642
Ben Pennell <bbpennel@email.unc.edu>
2024-07-02 09:19:36 -0400

    Restoring EnhancementRouterIT

--- services-camel-app/src/main/java/edu/unc/lib/boxc/services/camel/enhancements/EnhancementRouter.java
+++ services-camel-app/src/main/java/edu/unc/lib/boxc/services/camel/enhancements/EnhancementRouter.java
@@ -8,6 +8,7 @@ import static org.apache.camel.LoggingLevel.INFO;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import org.apache.camel.BeanInject;
+import org.apache.camel.ExchangePattern;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.PropertyInject;
 import org.apache.camel.builder.RouteBuilder;
@@ -38,11 +39,8 @@ public class EnhancementRouter extends RouteBuilder {
     @BeanInject(value = "nonBinaryEnhancementProcessor")
     private NonBinaryEnhancementProcessor nbProcessor;
 
+    @PropertyInject(value = "cdr.enhancement.processingThreads")
     private Integer enhancementThreads;
-
-    private String enhancementStreamCamel;
-    private String enhancementPerformCamel;
-    private String fcrepoBaseUrl;
 
     private static final String DEFAULT_ENHANCEMENTS = "thumbnails,imageAccessCopy,extractFulltext";
     private static final String THUMBNAIL_ENHANCEMENTS = "thumbnails";
@@ -50,11 +48,11 @@ public class EnhancementRouter extends RouteBuilder {
     public void configure() throws Exception {
 
         // Queue which interprets fedora messages into enhancement requests
-        from(enhancementStreamCamel)
+        from("{{cdr.enhancement.stream.camel}}")
             .routeId("ProcessEnhancementQueue")
             .startupOrder(110)
             .process(enProcessor)
-            .to("fcrepo:" + fcrepoBaseUrl + "?preferInclude=ServerManaged&accept=text/turtle")
+            .to("fcrepo:{{fcrepo.baseUrl}}?preferInclude=ServerManaged&accept=text/turtle")
             .choice()
                 .when(simple("${headers[org.fcrepo.jms.resourceType]} contains '" + Cdr.Tombstone.getURI() + "'"))
                     .log(DEBUG, log, "Ignoring tombstone object for enhancements ${headers[CamelFcrepoUri]}")
@@ -73,7 +71,7 @@ public class EnhancementRouter extends RouteBuilder {
                     .log(DEBUG, log, "Processing enhancements for non-binary ${headers[CamelFcrepoUri]}")
                     .process(nbProcessor)
                     .setHeader(CdrEnhancementSet, constant(THUMBNAIL_ENHANCEMENTS))
-                    .to(enhancementPerformCamel)
+                    .to("{{cdr.enhancement.perform.camel}}")
                 .otherwise()
                     .log(DEBUG, log, "Ignoring resource ${headers[CamelFcrepoUri]}")
             .end();
@@ -85,10 +83,10 @@ public class EnhancementRouter extends RouteBuilder {
             .setHeader(CdrEnhancementSet, constant(DEFAULT_ENHANCEMENTS))
             .process(mdProcessor)
             .filter(header(CdrBinaryPath).isNotNull())
-                .to(enhancementPerformCamel);
+                .to("{{cdr.enhancement.perform.camel}}");
 
-        // Queue for executing enhancement operations
-        from(enhancementPerformCamel)
+        // Queue for executing enhancnement operations
+        from("{{cdr.enhancement.perform.camel}}")
             .routeId("PerformEnhancementsQueue")
             .startupOrder(107)
             .choice()
@@ -117,39 +115,8 @@ public class EnhancementRouter extends RouteBuilder {
             .doCatch(IllegalStateException.class)
                 .log(LoggingLevel.WARN, log, "Shutdown interrupted processing of ${headers[CdrBinaryPath]}, requeuing")
                 .setHeader("AMQ_SCHEDULED_DELAY", constant("10000"))
-                .inOnly(enhancementPerformCamel);
+                .to("{{cdr.enhancement.perform.camel}}")
+                .to(ExchangePattern.InOnly, "{{cdr.enhancement.perform.camel}}");
 
-    }
-
-    @PropertyInject("cdr.enhancement.stream.camel")
-    public void setEnhancementStreamCamel(String enhancementStreamCamel) {
-        this.enhancementStreamCamel = enhancementStreamCamel;
-    }
-
-    @PropertyInject("cdr.enhancement.perform.camel")
-    public void setEnhancementPerformCamel(String enhancementPerformCamel) {
-        this.enhancementPerformCamel = enhancementPerformCamel;
-    }
-
-    public void setBinaryEnhancementProcessor(BinaryEnhancementProcessor enProcessor) {
-        this.enProcessor = enProcessor;
-    }
-
-    public void setBinaryMetadataProcessor(BinaryMetadataProcessor mdProcessor) {
-        this.mdProcessor = mdProcessor;
-    }
-
-    public void setNonBinaryEnhancementProcessor(NonBinaryEnhancementProcessor nbProcessor) {
-        this.nbProcessor = nbProcessor;
-    }
-
-    @PropertyInject("fcrepo.baseUrl")
-    public void setFcrepoBaseUrl(String fcrepoBaseUrl) {
-        this.fcrepoBaseUrl = fcrepoBaseUrl;
-    }
-
-    @PropertyInject("cdr.enhancement.processingThreads")
-    public void setEnhancementThreads(Integer enhancementThreads) {
-        this.enhancementThreads = enhancementThreads;
     }
 }
---
+++ services-camel-app/src/test/java/edu/unc/lib/boxc/services/camel/enhancements/EnhancementRouterIT.java
@@ -0,0 +1,345 @@
+package edu.unc.lib.boxc.services.camel.enhancements;
+
+import static edu.unc.lib.boxc.fcrepo.FcrepoJmsConstants.EVENT_TYPE;
+import static edu.unc.lib.boxc.fcrepo.FcrepoJmsConstants.IDENTIFIER;
+import static edu.unc.lib.boxc.fcrepo.FcrepoJmsConstants.RESOURCE_TYPE;
+import static edu.unc.lib.boxc.model.api.ids.RepositoryPathConstants.HASHED_PATH_DEPTH;
+import static edu.unc.lib.boxc.model.api.ids.RepositoryPathConstants.HASHED_PATH_SIZE;
+import static edu.unc.lib.boxc.model.api.rdf.Fcrepo4Repository.Binary;
+import static edu.unc.lib.boxc.model.api.rdf.Fcrepo4Repository.Container;
+import static edu.unc.lib.boxc.model.fcrepo.ids.DatastreamPids.getTechnicalMetadataPid;
+import static edu.unc.lib.boxc.model.fcrepo.ids.RepositoryPaths.idToPath;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import edu.unc.lib.boxc.model.fcrepo.ids.DatastreamPids;
+import edu.unc.lib.boxc.model.fcrepo.test.TestRepositoryDeinitializer;
+import edu.unc.lib.boxc.persist.impl.storage.StorageLocationTestHelper;
+import org.apache.camel.BeanInject;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.NotifyBuilder;
+import org.apache.camel.test.spring.junit5.CamelSpringTestSupport;
+import org.apache.commons.io.FileUtils;
+import org.fcrepo.client.FcrepoClient;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import edu.unc.lib.boxc.auth.fcrepo.models.AgentPrincipalsImpl;
+import edu.unc.lib.boxc.model.api.ids.PID;
+import edu.unc.lib.boxc.model.api.objects.BinaryObject;
+import edu.unc.lib.boxc.model.api.objects.CollectionObject;
+import edu.unc.lib.boxc.model.api.objects.DepositRecord;
+import edu.unc.lib.boxc.model.api.objects.FileObject;
+import edu.unc.lib.boxc.model.api.rdf.Cdr;
+import edu.unc.lib.boxc.model.api.services.RepositoryObjectFactory;
+import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
+import edu.unc.lib.boxc.model.fcrepo.test.TestHelper;
+import edu.unc.lib.boxc.operations.impl.edit.UpdateDescriptionService;
+import edu.unc.lib.boxc.operations.impl.edit.UpdateDescriptionService.UpdateDescriptionRequest;
+import edu.unc.lib.boxc.services.camel.BinaryMetadataProcessor;
+import edu.unc.lib.boxc.services.camel.NonBinaryEnhancementProcessor;
+import edu.unc.lib.boxc.services.camel.fulltext.FulltextProcessor;
+import edu.unc.lib.boxc.services.camel.images.AddDerivativeProcessor;
+import edu.unc.lib.boxc.services.camel.solr.SolrIngestProcessor;
+
+/**
+ *
+ * @author bbpennel
+ *
+ */
+public class EnhancementRouterIT extends CamelSpringTestSupport {
+    private final static String FILE_CONTENT = "content";
+
+    private final static long ALLOW_WAIT = 5000;
+
+    private AutoCloseable closeable;
+
+    private String baseAddress;
+
+    private RepositoryObjectFactory repoObjectFactory;
+    private StorageLocationTestHelper storageLocationTestHelper;
+    private FcrepoClient fcrepoClient;
+
+    private CamelContext cdrEnhancements;
+
+    @Produce(uri = "{{cdr.enhancement.stream.camel}}")
+    private ProducerTemplate template;
+
+    private AddDerivativeProcessor addSmallThumbnailProcessor;
+
+    private AddDerivativeProcessor addLargeThumbnailProcessor;
+
+    private AddDerivativeProcessor addAccessCopyProcessor;
+
+    private SolrIngestProcessor solrIngestProcessor;
+
+    private FulltextProcessor fulltextProcessor;
+
+//    private BinaryMetadataProcessor binaryMetadataProcessor;
+
+    private UpdateDescriptionService updateDescriptionService;
+
+    @TempDir
+    public Path tmpFolder;
+
+    private File tempDir;
+
+    private NonBinaryEnhancementProcessor nbh;
+
+    @Override
+    protected AbstractApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext(
+                "spring-test/cdr-client-container.xml",
+                "spring-test/jms-context.xml",
+                "enhancement-router-it-context.xml");
+    }
+
+    @BeforeEach
+    public void init() throws Exception {
+        closeable = openMocks(this);
+        baseAddress = applicationContext.getBean("baseAddress", String.class);
+        repoObjectFactory = applicationContext.getBean(RepositoryObjectFactory.class);
+        storageLocationTestHelper = applicationContext.getBean(StorageLocationTestHelper.class);
+        fcrepoClient = applicationContext.getBean(FcrepoClient.class);
+        cdrEnhancements = applicationContext.getBean(CamelContext.class);
+        addSmallThumbnailProcessor = applicationContext.getBean("addSmallThumbnailProcessor", AddDerivativeProcessor.class);
+        addLargeThumbnailProcessor = applicationContext.getBean("addLargeThumbnailProcessor", AddDerivativeProcessor.class);
+        addAccessCopyProcessor = applicationContext.getBean("addAccessCopyProcessor", AddDerivativeProcessor.class);
+        solrIngestProcessor = applicationContext.getBean("solrIngestProcessor", SolrIngestProcessor.class);
+        fulltextProcessor = applicationContext.getBean("fulltextProcessor", FulltextProcessor.class);
+//        binaryMetadataProcessor = applicationContext.getBean("binaryMetadataProcessor", BinaryMetadataProcessor.class);
+        updateDescriptionService = applicationContext.getBean(UpdateDescriptionService.class);
+        nbh = applicationContext.getBean(NonBinaryEnhancementProcessor.class);
+
+
+        reset(solrIngestProcessor);
+        reset(addSmallThumbnailProcessor);
+        reset(addLargeThumbnailProcessor);
+        reset(addAccessCopyProcessor);
+
+        when(addSmallThumbnailProcessor.needsRun(any(Exchange.class))).thenReturn(true);
+        when(addLargeThumbnailProcessor.needsRun(any(Exchange.class))).thenReturn(true);
+        when(addAccessCopyProcessor.needsRun(any(Exchange.class))).thenReturn(true);
+
+        TestHelper.setContentBase(baseAddress);
+        tempDir = Files.createDirectory(tmpFolder.resolve("target")).toFile();
+        nbh.setSourceImagesDir(tempDir.getAbsolutePath());
+
+        File thumbScriptFile = new File("target/convertScaleStage.sh");
+        FileUtils.writeStringToFile(thumbScriptFile, "exit 0", "utf-8");
+        thumbScriptFile.deleteOnExit();
+
+        File jp2ScriptFile = new File("target/convertJp2.sh");
+        FileUtils.writeStringToFile(jp2ScriptFile, "exit 0", "utf-8");
+        jp2ScriptFile.deleteOnExit();
+    }
+
+    @AfterEach
+    void closeService() throws Exception {
+        closeable.close();
+        TestRepositoryDeinitializer.cleanup(fcrepoClient);
+        storageLocationTestHelper.cleanupStorageLocations();
+    }
+
+    @Test
+    public void nonBinaryWithSourceImages() throws Exception {
+        CollectionObject collObject = repoObjectFactory.createCollectionObject(null);
+
+        String uuid = collObject.getPid().getUUID();
+        String basePath = idToPath(uuid, HASHED_PATH_DEPTH, HASHED_PATH_SIZE);
+        File uploadedFile = new File(String.valueOf(Paths.get(tempDir.getAbsolutePath(), basePath, uuid)));
+
+        FileInputStream input = new FileInputStream("src/test/resources/uploaded-files/burndown.png");
+        FileUtils.copyInputStreamToFile(input, uploadedFile);
+
+        final Map<String, Object> headers = createEvent(collObject.getPid(),
+                Cdr.Collection.getURI(), Container.getURI());
+        template.sendBodyAndHeaders("", headers);
+
+        verify(addSmallThumbnailProcessor, timeout(ALLOW_WAIT)).process(any(Exchange.class));
+        verify(addLargeThumbnailProcessor, timeout(ALLOW_WAIT)).process(any(Exchange.class));
+        verify(addAccessCopyProcessor, never()).process(any(Exchange.class));
+        verify(solrIngestProcessor, timeout(ALLOW_WAIT)).process(any(Exchange.class));
+    }
+
+    @Test
+    public void nonBinaryNoSourceImages() throws Exception {
+        CollectionObject collObject = repoObjectFactory.createCollectionObject(null);
+        final Map<String, Object> headers = createEvent(collObject.getPid(),
+                Cdr.Collection.getURI(), Container.getURI());
+        template.sendBodyAndHeaders("", headers);
+
+        verify(solrIngestProcessor, timeout(ALLOW_WAIT)).process(any(Exchange.class));
+        verify(addSmallThumbnailProcessor, never()).process(any(Exchange.class));
+        verify(addLargeThumbnailProcessor, never()).process(any(Exchange.class));
+    }
+
+    @Test
+    public void testBinaryImageFile() throws Exception {
+        FileObject fileObj = repoObjectFactory.createFileObject(null);
+        var storageUri = storageLocationTestHelper.makeTestStorageUri(DatastreamPids.getOriginalFilePid(fileObj.getPid()));
+        FileUtils.writeStringToFile(new File(storageUri), FILE_CONTENT, "UTF-8");
+        BinaryObject binObj = fileObj.addOriginalFile(storageUri,
+                null, "image/png", null, null);
+
+        // Separate exchanges when multicasting
+        NotifyBuilder notify1 = new NotifyBuilder(cdrEnhancements)
+                .whenCompleted(12)
+                .create();
+
+        final Map<String, Object> headers = createEvent(binObj.getPid(), Binary.getURI());
+        template.sendBodyAndHeaders("", headers);
+
+        boolean result1 = notify1.matches(5l, TimeUnit.SECONDS);
+        assertTrue("Enhancement route not satisfied", result1);
+
+        verify(addSmallThumbnailProcessor, timeout(ALLOW_WAIT)).process(any(Exchange.class));
+        verify(addLargeThumbnailProcessor, timeout(ALLOW_WAIT)).process(any(Exchange.class));
+        verify(addAccessCopyProcessor, timeout(ALLOW_WAIT)).process(any(Exchange.class));
+        // Indexing triggered for binary parent
+        verify(solrIngestProcessor, timeout(ALLOW_WAIT)).process(any(Exchange.class));
+    }
+
+    @Test
+    public void testBinaryMetadataFile() throws Exception {
+        FileObject fileObj = repoObjectFactory.createFileObject(null);
+        var storageUri = storageLocationTestHelper.makeTestStorageUri(fileObj.getPid());
+        FileUtils.writeStringToFile(new File(storageUri), FILE_CONTENT, "UTF-8");
+        BinaryObject binObj = fileObj.addOriginalFile(storageUri,
+                null, "image/png", null, null);
+
+        String mdId = binObj.getPid().getRepositoryPath() + "/fcr:metadata";
+        PID mdPid = PIDs.get(mdId);
+
+        NotifyBuilder notify = new NotifyBuilder(cdrEnhancements)
+                .whenCompleted(1)
+                .create();
+
+        final Map<String, Object> headers = createEvent(mdPid, Binary.getURI());
+        template.sendBodyAndHeaders("", headers);
+
+        boolean result = notify.matches(5l, TimeUnit.SECONDS);
+
+        assertTrue("Processing message did not match expectations", result);
+
+        verify(addSmallThumbnailProcessor, never()).process(any(Exchange.class));
+        verify(addLargeThumbnailProcessor, never()).process(any(Exchange.class));
+        verify(addAccessCopyProcessor, never()).process(any(Exchange.class));
+        verify(solrIngestProcessor, never()).process(any(Exchange.class));
+    }
+
+    @Test
+    public void testInvalidFile() throws Exception {
+        FileObject fileObj = repoObjectFactory.createFileObject(null);
+        PID fitsPid = getTechnicalMetadataPid(fileObj.getPid());
+        var storageUri = storageLocationTestHelper.makeTestStorageUri(fitsPid);
+        FileUtils.writeStringToFile(new File(storageUri), FILE_CONTENT, "UTF-8");
+        BinaryObject binObj = fileObj.addBinary(fitsPid, storageUri,
+                "fits.xml", "text/xml", null, null, null);
+
+        NotifyBuilder notify = new NotifyBuilder(cdrEnhancements)
+                .whenCompleted(1)
+                .create();
+
+        final Map<String, Object> headers = createEvent(binObj.getPid(), Binary.getURI());
+        template.sendBodyAndHeaders("", headers);
+
+        boolean result = notify.matches(5l, TimeUnit.SECONDS);
+
+        assertTrue("Processing message did not match expectations", result);
+
+        verify(addSmallThumbnailProcessor, never()).process(any(Exchange.class));
+        verify(fulltextProcessor,  never()).process(any(Exchange.class));
+        verify(solrIngestProcessor, never()).process(any(Exchange.class));
+    }
+
+    @Test
+    public void testProcessFilterOutDescriptiveMDSolr() throws Exception {
+        FileObject fileObj = repoObjectFactory.createFileObject(null);
+        var modsStream = Files.newInputStream(Path.of("src/test/resources/datastreams/simpleMods.xml"));
+        BinaryObject descObj = updateDescriptionService.updateDescription(new UpdateDescriptionRequest(
+                mock(AgentPrincipalsImpl.class), fileObj.getPid(), modsStream));
+
+        NotifyBuilder notify = new NotifyBuilder(cdrEnhancements)
+                .whenCompleted(1)
+                .create();
+
+        Map<String, Object> headers = createEvent(descObj.getPid(),
+                Binary.getURI(), Cdr.DescriptiveMetadata.getURI());
+        template.sendBodyAndHeaders("", headers);
+
+        boolean result = notify.matches(5l, TimeUnit.SECONDS);
+
+        assertTrue("Processing message did not match expectations", result);
+
+        verify(solrIngestProcessor, never()).process(any(Exchange.class));
+    }
+
+    @Test
+    public void testDepositManifestFileMetadata() throws Exception {
+        DepositRecord recObj = repoObjectFactory.createDepositRecord(null);
+        var storageUri = storageLocationTestHelper.makeTestStorageUri(recObj.getPid());
+        FileUtils.writeStringToFile(new File(storageUri), FILE_CONTENT, "UTF-8");
+        BinaryObject manifestBin = recObj.addManifest(storageUri, "manifest", "text/plain", null, null);
+
+        String mdId = manifestBin.getPid().getRepositoryPath() + "/fcr:metadata";
+        PID mdPid = PIDs.get(mdId);
+
+        NotifyBuilder notify = new NotifyBuilder(cdrEnhancements)
+                .whenCompleted(1)
+                .create();
+
+        final Map<String, Object> headers = createEvent(mdPid, Binary.getURI());
+        template.sendBodyAndHeaders("", headers);
+
+        boolean result = notify.matches(5l, TimeUnit.SECONDS);
+
+        assertTrue("Processing message did not match expectations", result);
+
+        verify(addSmallThumbnailProcessor, never()).process(any(Exchange.class));
+        verify(addLargeThumbnailProcessor, never()).process(any(Exchange.class));
+        verify(addAccessCopyProcessor, never()).process(any(Exchange.class));
+        verify(solrIngestProcessor, never()).process(any(Exchange.class));
+    }
+
+    private Map<String, Object> createEvent(PID pid, String... type) {
+        final Map<String, Object> headers = new HashMap<>();
+        headers.put(IDENTIFIER, pid.getRepositoryPath());
+        headers.put(EVENT_TYPE, "ResourceCreation");
+        headers.put("CamelFcrepoUri", pid.getRepositoryPath());
+        headers.put(RESOURCE_TYPE, String.join(",", type));
+
+        return headers;
+    }
+}
