6537aee1dcaa225aa105c41682f5040af7c7d1b7
lfarrell <lfarrell@email.unc.edu>
2024-08-06 10:13:29 -0400

    Fix issue with with moving around in audio/video files

--- web-common/src/main/java/edu/unc/lib/boxc/web/common/services/FedoraContentService.java
+++ web-common/src/main/java/edu/unc/lib/boxc/web/common/services/FedoraContentService.java
@@ -108,7 +108,7 @@ public class FedoraContentService {
         try (FcrepoResponse fedoraResponse = getFedoraResponse(binObj, range)) {
             response.setHeader(ACCEPT_RANGES, BYTES);
             response.setHeader(CONTENT_LENGTH, fedoraResponse.getHeaderValue(CONTENT_LENGTH));
-           // response.setHeader(CONTENT_RANGE, fedoraResponse.getHeaderValue(CONTENT_RANGE));
+            response.setHeader(CONTENT_RANGE, fedoraResponse.getHeaderValue(CONTENT_RANGE));
             InputStream binStream = fedoraResponse.getBody();
             OutputStream outStream = response.getOutputStream();
             IOUtils.copy(binStream, outStream, BUFFER_SIZE);
