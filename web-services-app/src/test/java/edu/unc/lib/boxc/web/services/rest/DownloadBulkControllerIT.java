ee56dc0110675e9710da1893ec581e30b2256f9d
sharonl <6546457+sharonluong@users.noreply.github.com>
2024-08-19 11:16:23 -0400

    BXC-4673 finish bulk download controller test

--- web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/DownloadBulkControllerIT.java
+++ web-services-app/src/test/java/edu/unc/lib/boxc/web/services/rest/DownloadBulkControllerIT.java
@@ -1,12 +1,15 @@
 package edu.unc.lib.boxc.web.services.rest;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import edu.unc.lib.boxc.auth.api.Permission;
 import edu.unc.lib.boxc.auth.api.exceptions.AccessRestrictionException;
 import edu.unc.lib.boxc.auth.api.models.AccessGroupSet;
 import edu.unc.lib.boxc.auth.api.services.AccessControlService;
 import edu.unc.lib.boxc.auth.fcrepo.models.AccessGroupSetImpl;
 import edu.unc.lib.boxc.auth.fcrepo.services.GroupsThreadStore;
 import edu.unc.lib.boxc.model.api.ids.PID;
+import edu.unc.lib.boxc.model.api.objects.BinaryObject;
+import edu.unc.lib.boxc.model.api.objects.FileObject;
 import edu.unc.lib.boxc.model.api.objects.RepositoryObjectLoader;
 import edu.unc.lib.boxc.model.api.objects.WorkObject;
 import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
@@ -28,7 +31,10 @@ import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
@@ -40,6 +46,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,7 +55,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @WireMockTest(httpPort = 46887)
 public class DownloadBulkControllerIT {
     private static final String WORK_ID = "f277bb38-272c-471c-a28a-9887a1328a1f";
+    private static final String FILE_ID = "83c2d7f8-2e6b-4f0b-ab7e-7397969c0682";
     private static final PID WORK_PID = PIDs.get(WORK_ID);
+    private static final PID FILE_PID = PIDs.get(FILE_ID);
     private final static String USERNAME = "test_user";
     private final static AccessGroupSet GROUPS = new AccessGroupSetImpl("adminGroup");
     @Mock
@@ -57,6 +66,8 @@ public class DownloadBulkControllerIT {
     private RepositoryObjectLoader repositoryObjectLoader;
     @Mock
     private WorkObject workObject;
+    @Mock
+    private FileObject fileObject;
     @TempDir
     public Path tmpFolder;
     @InjectMocks
@@ -101,6 +112,15 @@ public class DownloadBulkControllerIT {
     @Test
     public void successTest() throws Exception {
         when(repositoryObjectLoader.getWorkObject(any())).thenReturn(workObject);
+        when(workObject.getMembers()).thenReturn(List.of(fileObject));
+        when(fileObject.getPid()).thenReturn(FILE_PID);
+        var binObj = mock(BinaryObject.class);
+        when(fileObject.getOriginalFile()).thenReturn(binObj);
+        when(binObj.getBinaryStream()).thenReturn(fileInputStream);
+        when(binObj.getFilename()).thenReturn("bunny.jpg");
+        when(aclService.hasAccess(eq(FILE_PID), any(),
+                eq(Permission.viewOriginal))).thenReturn(true);
+
         var result = mvc.perform(get("/bulkDownload/" + WORK_ID))
                 .andExpect(status().is2xxSuccessful())
                 .andReturn();
@@ -109,21 +129,20 @@ public class DownloadBulkControllerIT {
         var bytes = response.getContentAsByteArray();
         var zipInputStream =  new ZipInputStream(new ByteArrayInputStream(bytes));
 
-        assertZipFiles(zipInputStream);
-        //assertEquals("application/zip", response.getHeader("Content-Type"));
+        assertZipFiles(zipInputStream, List.of("bunny.jpg"));
+        assertEquals("application/zip", response.getHeader("Content-Type"));
     }
 
-    private void assertZipFiles(ZipInputStream stream) throws IOException {
-        ZipEntry entry;
-        while((entry = stream.getNextEntry())!=null)
-        {
-
+    private void assertZipFiles(ZipInputStream stream, List<String> filenames) throws IOException {
+        var actualFilenames = new ArrayList<String>();
+        try (stream) {
+            ZipEntry entry;
+            while ((entry = stream.getNextEntry()) != null) {
+                actualFilenames.add(entry.getName());
+                var actualContent = IOUtils.toByteArray(stream);
+                DownloadTestHelper.assertCorrectImageReturned(actualContent);
+            }
         }
-//        while (entries.hasMoreElements()) {
-//            var entry = entries.nextElement();
-//            var zipInputStream = zipFile.getInputStream(entry);
-//            var actualContent = IOUtils.toByteArray(zipInputStream);
-//            DownloadTestHelper.assertCorrectImageReturned(actualContent);
-//        }
+        assertEquals(filenames, actualFilenames);
     }
 }
