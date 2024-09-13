1fb179f7bb3a50163a2bd02eb61817679e540fcf
Ben Pennell <bbpennel@email.unc.edu>
2024-09-13 15:50:20 -0400

    Update properties to make response from chompb. Use datatable ajax to load data

--- static/js/admin/vue-cdr-admin/tests/unit/chompb/preIngest.spec.js
+++ static/js/admin/vue-cdr-admin/tests/unit/chompb/preIngest.spec.js
@@ -5,29 +5,33 @@ let wrapper;
 
 const project_info = [
     {
-        "name": "file_source_test",
-        "createdDate": "2024-07-16T15:20:08.579384Z",
-        "creator": "bbpennel",
-        "indexedDate": "2024-08-15T12:38:28.662654Z",
-        "sourceFilesUpdatedDate": "2024-08-14T17:06:34.182417Z",
-        "groupMappingsUpdatedDate": "2024-08-14T17:42:49.626664Z",
-        "sipsSubmitted": [],
-        "bxcEnvironmentId": "test",
-        "projectSource": "files",
+        "projectProperties" : {
+            "name": "file_source_test",
+            "createdDate": "2024-07-16T15:20:08.579384Z",
+            "creator": "bbpennel",
+            "indexedDate": "2024-08-15T12:38:28.662654Z",
+            "sourceFilesUpdatedDate": "2024-08-14T17:06:34.182417Z",
+            "groupMappingsUpdatedDate": "2024-08-14T17:42:49.626664Z",
+            "sipsSubmitted": [],
+            "bxcEnvironmentId": "test",
+            "projectSource": "files"
+        },
         "status": "sources_mapped",
         "allowedActions": ["color_bar_crop", "color_bar_report"],
         "projectPath": "/test_path_one/file_source_test"
     },
     {
-        "name": "ncmaps_urls",
-        "cdmCollectionId": "ncmaps",
-        "createdDate": "2024-04-05T13:17:51.975599Z",
-        "creator": "goslen",
-        "exportedDate": "2024-04-05T13:18:30.212723Z",
-        "indexedDate": "2024-04-05T13:20:32.083546Z",
-        "cdmEnvironmentId": "dc-prod",
-        "bxcEnvironmentId": "test",
-        "projectSource": "CDM",
+        "projectProperties" : {
+            "name": "ncmaps_urls",
+            "cdmCollectionId": "ncmaps",
+            "createdDate": "2024-04-05T13:17:51.975599Z",
+            "creator": "goslen",
+            "exportedDate": "2024-04-05T13:18:30.212723Z",
+            "indexedDate": "2024-04-05T13:20:32.083546Z",
+            "cdmEnvironmentId": "dc-prod",
+            "bxcEnvironmentId": "test",
+            "projectSource": "CDM"
+        },
         "status": "sources_mapped",
         "allowedActions": [],
         "projectPath": "/test_path_two/file_source_test"
