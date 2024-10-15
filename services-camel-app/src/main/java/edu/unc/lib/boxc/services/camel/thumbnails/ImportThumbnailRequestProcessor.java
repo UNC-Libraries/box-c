a0816f73ece1c4ca6b3bfeb23b96f1594611255c
sharonl <6546457+sharonluong@users.noreply.github.com>
2024-10-15 14:45:56 -0400

    BXC-4737 cleanup temp imported thumbnail (#1817)
    * BXC-4737
    
    * BXC-4737 make exception more specific

--- services-camel-app/src/main/java/edu/unc/lib/boxc/services/camel/thumbnails/ImportThumbnailRequestProcessor.java
+++ services-camel-app/src/main/java/edu/unc/lib/boxc/services/camel/thumbnails/ImportThumbnailRequestProcessor.java
@@ -3,9 +3,15 @@ package edu.unc.lib.boxc.services.camel.thumbnails;
 import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
 import edu.unc.lib.boxc.operations.jms.thumbnails.ImportThumbnailRequestSerializationHelper;
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static edu.unc.lib.boxc.services.camel.util.CdrFcrepoHeaders.CdrBinaryMimeType;
 import static edu.unc.lib.boxc.services.camel.util.CdrFcrepoHeaders.CdrBinaryPath;
@@ -17,6 +23,7 @@ import static org.fcrepo.camel.FcrepoHeaders.FCREPO_URI;
  * @author snluong
  */
 public class ImportThumbnailRequestProcessor implements Processor {
+    private static final Logger log = LoggerFactory.getLogger(ImportThumbnailRequestProcessor.class);
     @Override
     public void process(Exchange exchange) throws IOException {
         var in = exchange.getIn();
@@ -29,5 +36,21 @@ public class ImportThumbnailRequestProcessor implements Processor {
         in.setHeader(CdrBinaryPath, storagePath.toString());
         in.setHeader(CdrBinaryMimeType, mimetype);
         in.setHeader(FCREPO_URI, repoPath);
+    }
+
+    /**
+     * Deletes the temporarily stored uploaded thumbnail file
+     * @param exchange
+     * @throws IOException
+     */
+    public void cleanupTempThumbnailFile(Exchange exchange) throws IOException {
+        final Message in = exchange.getIn();
+        String tempValue = (String) in.getHeader(CdrBinaryPath);
+        Path tempPath = Paths.get(tempValue);
+
+        boolean deleted = Files.deleteIfExists(tempPath);
+        if (deleted) {
+            log.debug("Cleaned up leftover temp file {}", tempPath);
+        }
     }
 }
