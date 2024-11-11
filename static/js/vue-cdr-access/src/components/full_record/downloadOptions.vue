f5c3a6f5fd54c84834f3ba4c08b18fa06ce6b1e8
lfarrell <lfarrell@email.unc.edu>
2024-11-11 15:04:42 -0500

    Us

--- static/js/vue-cdr-access/src/components/full_record/downloadOptions.vue
+++ static/js/vue-cdr-access/src/components/full_record/downloadOptions.vue
@@ -179,7 +179,7 @@ export default {
 
         showImageDownload(brief_object) {
             return (this.hasPermission(brief_object, 'viewReducedResImages')
-                    || this.hasPermission(brief_object, 'viewReducedResImages')) &&
+                    || this.hasPermission(brief_object, 'viewOriginals')) &&
                 brief_object.format.includes('Image') && this.getOriginalFile(brief_object) !== undefined
         },
 
