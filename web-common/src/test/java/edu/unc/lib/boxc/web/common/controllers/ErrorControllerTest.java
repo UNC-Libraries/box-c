379c06cdf18496094d87b4f7dfbab2bb2a80051a
sharonl <6546457+sharonluong@users.noreply.github.com>
2025-03-12 15:42:43 -0400

    BXC-4853 finish error controller tests

--- web-common/src/test/java/edu/unc/lib/boxc/web/common/controllers/ErrorControllerTest.java
+++ web-common/src/test/java/edu/unc/lib/boxc/web/common/controllers/ErrorControllerTest.java
@@ -1,27 +1,17 @@
 package edu.unc.lib.boxc.web.common.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.unc.lib.boxc.operations.jms.viewSettings.ViewSettingRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.servlet.http.HttpServletRequest;
-
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ErrorControllerIT {
+public class ErrorControllerTest {
     @InjectMocks
     private ErrorController controller;
     private MockMvc mvc;
@@ -54,20 +44,12 @@ public class ErrorControllerIT {
 
     @Test
     public void testException() throws Exception {
-//        var request = mock(HttpServletRequest.class);
-//        when(request.getAttribute(eq("javax.servlet.error.exception"))).thenReturn(IllegalArgumentException.class);
-//        when(request.getAttribute(eq("javax.servlet.forward.request_uri"))).thenReturn("uri");
-
-        var request = new MockHttpServletRequest();
-        request.setAttribute("javax.servlet.error.exception", IllegalArgumentException.class);
-        request.setAttribute("javax.servlet.forward.request_uri", "uri");
-
-        ObjectMapper mapper = new ObjectMapper();
-        var writer = mapper.writerFor(MockHttpServletRequest.class);
-        var content = writer.writeValueAsString(request);
-        mvc.perform(post("/exception")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content))
+        mvc.perform(get("/exception")
+                .with(request -> {
+                    request.setAttribute("javax.servlet.error.exception", new IllegalStateException("Test Exception"));
+                    request.setAttribute("javax.servlet.forward.request_uri", "/test-uri");
+                    return request;
+                }))
                 .andExpect(status().isInternalServerError())
                 .andReturn();
     }
