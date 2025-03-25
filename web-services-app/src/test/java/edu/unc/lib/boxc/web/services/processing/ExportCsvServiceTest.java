1343c5306787174fa3b482b67774ca416412f0a5
sharonl <6546457+sharonluong@users.noreply.github.com>
2025-03-25 10:11:01 -0400

    BXC-4846 remove unused import

--- web-services-app/src/test/java/edu/unc/lib/boxc/web/services/processing/ExportCsvServiceTest.java
+++ web-services-app/src/test/java/edu/unc/lib/boxc/web/services/processing/ExportCsvServiceTest.java
@@ -38,7 +38,6 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
