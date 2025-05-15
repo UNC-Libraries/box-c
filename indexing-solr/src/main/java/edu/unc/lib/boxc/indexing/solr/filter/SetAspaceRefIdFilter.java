8a1601765c52216507a200d5f8eeaf41c562d5eb
krwong <krwong@email.unc.edu>
2025-05-15 14:40:02 -0400

    switch order of null check and empty check

--- indexing-solr/src/main/java/edu/unc/lib/boxc/indexing/solr/filter/SetAspaceRefIdFilter.java
+++ indexing-solr/src/main/java/edu/unc/lib/boxc/indexing/solr/filter/SetAspaceRefIdFilter.java
@@ -33,7 +33,7 @@ public class SetAspaceRefIdFilter implements IndexDocumentFilter {
                 resource.getProperty(CdrAspace.refId).getString() : null;
 
         List<String> identifiers = new ArrayList<>();
-        if (!doc.getIdentifier().isEmpty() && doc.getIdentifier() != null) {
+        if (doc.getIdentifier() != null && !doc.getIdentifier().isEmpty()) {
             identifiers = doc.getIdentifier();
         }
         identifiers.add("aspaceRefId|" + refId);
