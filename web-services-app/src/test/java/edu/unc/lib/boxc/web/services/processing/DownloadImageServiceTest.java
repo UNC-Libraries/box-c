adae02068892f1b565692a7efadcfac93cdcea5d
sharonl <6546457+sharonluong@users.noreply.github.com>
2025-07-18 12:28:21 -0400

    Bxc 4845 download image service tests (#1966)
    * BXC-4845 starting downloadImageService unit tests
    
    * BXC-4845 streaming tests
    
    * BXC-4845 finish downloadImageService unit tests
    
    * BXC-4845 fix tests

---
+++ web-services-app/src/test/java/edu/unc/lib/boxc/web/services/processing/DownloadImageServiceTest.java
@@ -0,0 +1,258 @@
+package edu.unc.lib.boxc.web.services.processing;
+
+import edu.unc.lib.boxc.model.api.DatastreamType;
+import edu.unc.lib.boxc.model.api.exceptions.NotFoundException;
+import edu.unc.lib.boxc.operations.api.images.ImageServerUtil;
+import edu.unc.lib.boxc.search.api.models.ContentObjectRecord;
+import edu.unc.lib.boxc.search.api.models.Datastream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static edu.unc.lib.boxc.model.fcrepo.test.TestHelper.makePid;
+import static edu.unc.lib.boxc.operations.api.images.ImageServerUtil.FULL_SIZE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+public class DownloadImageServiceTest {
+    private static final String IIIF_BASE = "http://example.com/iiif/v3/";
+    private DownloadImageService downloadImageService;
+    private AutoCloseable closeable;
+    @Mock
+    private ContentObjectRecord contentObjectRecord;
+    @Mock
+    private Datastream datastream, jp2Datastream;
+
+    @BeforeEach
+    public void setup() {
+        closeable = openMocks(this);
+        downloadImageService = new DownloadImageService();
+        downloadImageService.setIiifBasePath(IIIF_BASE);
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    public void testIsInvalidJP2DatastreamWithValidDataStream() {
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(jp2Datastream);
+        when(jp2Datastream.getFilesize()).thenReturn(2048L);
+        assertFalse(downloadImageService.isInvalidJP2Datastream(contentObjectRecord));
+    }
+
+    @Test
+    public void testIsInvalidJP2DatastreamWithNullDataStream() {
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(null);
+        assertTrue(downloadImageService.isInvalidJP2Datastream(contentObjectRecord));
+    }
+
+    @Test
+    public void testIsInvalidJP2DatastreamWithEmptyDataStream() {
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(jp2Datastream);
+        when(jp2Datastream.getFilesize()).thenReturn(0L);
+        assertTrue(downloadImageService.isInvalidJP2Datastream(contentObjectRecord));
+    }
+
+    @Test
+    public void testGetDownloadFilenameFullSizeFromOriginalFile() {
+        var filename = "original_file";
+        var extension = ".png";
+        when(contentObjectRecord.getDatastreamObject(any())).thenReturn(datastream);
+        when(datastream.getFilename()).thenReturn(filename + extension);
+
+        assertEquals( filename + "_" + FULL_SIZE + ".jpg",
+                downloadImageService.getDownloadFilename(contentObjectRecord, FULL_SIZE));
+    }
+
+    @Test
+    public void testGetDownloadFilenameFormattedSizeFromJP2() {
+        var filename = "derivative_file";
+        var extension = ".png";
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.ORIGINAL_FILE.getId())))
+                .thenReturn(null);
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(jp2Datastream);
+        when(jp2Datastream.getFilename()).thenReturn(filename + extension);
+
+        assertEquals( "derivative_file_800px.jpg",
+                downloadImageService.getDownloadFilename(contentObjectRecord, "800"));
+    }
+
+    @Test
+    public void testParseSizeNoErrors() {
+        assertEquals(800, downloadImageService.parseSize("800"));
+    }
+
+    @Test
+    public void testParseSizeNotANumber() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            downloadImageService.parseSize("not a number");
+        });
+    }
+
+    @Test
+    public void testGetSizeBiggerThanFull() {
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(jp2Datastream);
+        when(jp2Datastream.getExtent()).thenReturn("800x800");
+        assertEquals(FULL_SIZE, downloadImageService.getSize(contentObjectRecord, "1200"));
+    }
+
+    @Test
+    public void testGetSizeDatastreamExtentsAreNull() {
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(jp2Datastream);
+        when(jp2Datastream.getExtent()).thenReturn(null);
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.ORIGINAL_FILE.getId())))
+                .thenReturn(datastream);
+        when(datastream.getExtent()).thenReturn(null);
+        assertNull(downloadImageService.getSize(contentObjectRecord, "800"));
+    }
+
+    @Test
+    public void testGetSizeNoDatastreamObject() {
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                    .thenReturn(null);
+            when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.ORIGINAL_FILE.getId())))
+                    .thenReturn(null);
+            downloadImageService.getSize(contentObjectRecord, "800");
+        });
+    }
+
+    @Test
+    public void testGetSizeFullSize() {
+        assertEquals(FULL_SIZE, downloadImageService.getSize(contentObjectRecord, FULL_SIZE));
+    }
+
+    @Test
+    public void testGetSizeMax() {
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(jp2Datastream);
+        when(jp2Datastream.getExtent()).thenReturn("800x500");
+        assertEquals(FULL_SIZE, downloadImageService.getSize(contentObjectRecord, "800"));
+    }
+
+    @Test
+    public void testGetSize() {
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(jp2Datastream);
+        when(jp2Datastream.getExtent()).thenReturn("500x500");
+        assertEquals("125", downloadImageService.getSize(contentObjectRecord, "125"));
+    }
+
+    @Test
+    public void testGetSizeOriginalFileDatastream() {
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId())))
+                .thenReturn(null);
+        when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.ORIGINAL_FILE.getId())))
+                .thenReturn(datastream);
+        when(datastream.getExtent()).thenReturn("500x500");
+        assertEquals("125", downloadImageService.getSize(contentObjectRecord, "125"));
+
+    }
+
+    @Test
+    public void testStreamDownload() throws IOException {
+        var constructorArgs = new ArrayList<>();
+        try (MockedConstruction<UrlResource> urlResourceClass = Mockito.mockConstruction(UrlResource.class,
+                // get the URL sent into UrlResource constructor
+                (mock, context) -> constructorArgs.add(context.arguments().get(0)))){
+            var contentDispositionHeader = "attachment; filename=original_file_max.jpg";
+            var contentType = MediaType.IMAGE_JPEG;
+            var pid = makePid();
+            var url = ImageServerUtil.buildURL(IIIF_BASE, pid.getId(), FULL_SIZE);
+            when(contentObjectRecord.getDatastreamObject(any())).thenReturn(datastream);
+            when(datastream.getFilename()).thenReturn("original_file.png");
+            when(contentObjectRecord.getPid()).thenReturn(pid);
+
+            var downloadStream = downloadImageService.streamDownload(contentObjectRecord, FULL_SIZE);
+            var mockedUrlResource = urlResourceClass.constructed().get(0);
+
+            var expectedResponse = ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, contentDispositionHeader)
+                    .contentType(contentType)
+                    .body(mockedUrlResource);
+            assertEquals(expectedResponse, downloadStream);
+            assertEquals(url, constructorArgs.get(0));
+        }
+    }
+
+    @Test
+    public void testStreamThumbnail() throws IOException {
+        var constructorArgs = new ArrayList<>();
+        try (MockedConstruction<UrlResource> urlResourceClass = Mockito.mockConstruction(UrlResource.class,
+                // get the URL sent into UrlResource constructor
+                (mock, context) -> constructorArgs.add(context.arguments().get(0))))
+        {
+            var contentDispositionHeader = "inline;";
+            var contentType = MediaType.IMAGE_JPEG;
+            var pid = makePid();
+            var url = ImageServerUtil.buildURL(IIIF_BASE, pid.getId(), "128");
+            when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId()))).thenReturn(jp2Datastream);
+            when(jp2Datastream.getFilename()).thenReturn("derivative.png");
+            when(jp2Datastream.getFilesize()).thenReturn(1048L);
+            when(jp2Datastream.getExtent()).thenReturn("300x300");
+            when(contentObjectRecord.getPid()).thenReturn(pid);
+
+            var thumbnailStream = downloadImageService.streamThumbnail(contentObjectRecord, "128");
+            var mockedUrlResource = urlResourceClass.constructed().get(0);
+
+            var expectedResponse = ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, contentDispositionHeader)
+                    .contentType(contentType)
+                    .body(mockedUrlResource);
+            assertEquals(expectedResponse, thumbnailStream);
+            // make sure URl matches what is expected
+            assertEquals(url, constructorArgs.get(0));
+        }
+    }
+
+    @Test
+    public void testStreamThumbnailPlaceholder() throws IOException {
+        var constructorArgs = new ArrayList<>();
+        try (MockedConstruction<UrlResource> urlResourceClass = Mockito.mockConstruction(UrlResource.class,
+                // get the URL sent into UrlResource constructor
+                (mock, context) -> constructorArgs.add(context.arguments().get(0)))){
+            var contentDispositionHeader = "inline;";
+            var contentType = MediaType.IMAGE_PNG;
+            var pid = makePid();
+            var url = downloadImageService.getPlaceholderUrl("128");
+            when(contentObjectRecord.getDatastreamObject(eq(DatastreamType.JP2_ACCESS_COPY.getId()))).thenReturn(null);
+            when(contentObjectRecord.getPid()).thenReturn(pid);
+
+            var thumbnailStream = downloadImageService.streamThumbnail(contentObjectRecord, "128");
+            var mockedUrlResource = urlResourceClass.constructed().get(0);
+
+            var expectedResponse = ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, contentDispositionHeader)
+                    .contentType(contentType)
+                    .body(mockedUrlResource);
+            assertEquals(expectedResponse, thumbnailStream);
+            assertEquals(url, constructorArgs.get(0));
+        }
+    }
+
+}
