160ddcfb39d106b66c5d96a4713e9e419aad71eb
lfarrell <lfarrell@email.unc.edu>
2025-12-11 08:47:20 -0500

    tesy

--- operations/src/main/java/edu/unc/lib/boxc/operations/impl/acl/ExpireEmbargoService.java
+++ operations/src/main/java/edu/unc/lib/boxc/operations/impl/acl/ExpireEmbargoService.java
@@ -79,7 +79,7 @@ public class ExpireEmbargoService {
     }
 
     // run service every day 1 minute after midnight
-    @Scheduled(cron = "0 1 0 * * *")
+    @Scheduled(cron = "0 0/3 0 * * *")
     public void expireEmbargoes() {
         log.info("running ExpireEmbargoService");
         // get list of expired embargoes
