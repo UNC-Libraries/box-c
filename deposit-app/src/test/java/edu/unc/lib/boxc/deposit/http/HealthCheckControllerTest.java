a0b722917775c6626f49acb16422f2f838dc0d27
sharonl <6546457+sharonluong@users.noreply.github.com>
2026-02-06 16:47:11 -0500

    BXC-5571 update test and pom file

--- deposit-app/src/test/java/edu/unc/lib/boxc/deposit/http/HealthCheckControllerTest.java
+++ deposit-app/src/test/java/edu/unc/lib/boxc/deposit/http/HealthCheckControllerTest.java
@@ -3,10 +3,16 @@ package edu.unc.lib.boxc.deposit.http;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,11 +22,20 @@ public class HealthCheckControllerTest {
     private HealthCheckController controller;
     private MockMvc mvc;
     private AutoCloseable closeable;
+    private Path fitsCommand;
+    @TempDir
+    public Path tmpFolder;
 
     @BeforeEach
-    public void init() {
-        closeable = MockitoAnnotations.openMocks(this);
+    public void init() throws IOException {
+        Path fitsHomePath = tmpFolder.resolve("fits");
+        Files.createDirectory(fitsHomePath);
+        File fitsHome = fitsHomePath.toFile();
+        // Create fits command and make it executable
+        fitsCommand = new File(fitsHome, "fits.sh").toPath();
 
+        controller.setFitsHomePath(fitsHomePath.toString());
+        closeable = MockitoAnnotations.openMocks(this);
         mvc = MockMvcBuilders.standaloneSetup(controller)
                 .build();
     }
