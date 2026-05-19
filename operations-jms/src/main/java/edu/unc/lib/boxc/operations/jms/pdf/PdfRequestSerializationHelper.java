95950471c4deeee94a2eabaa41dee9adeaa13d42
krwong <69482343+krwong@users.noreply.github.com>
2026-05-19 19:56:59 -0400

    BXC-5549 setup camel workflow for pdf4u (#2132)
    * add datastream type for PDF derivatives, pdf4u processor and router
    
    * fix/update tests, update pdf4u submodule
    
    * change startup order for pdf things, fix names
    
    * remove pdf4u from automatic enhancements
    
    * fix pdf4u processor tests
    
    * fix PdfEnhancementsRouterTest
    
    * pdf4u on demand camel workflow
    
    * update PdfDerivativeService and fix tests
    
    * switch PdfRequest workPid to String, add/fix tests, update service-context.xml
    
    * fix test
    
    * fix test and remove pdf stuff from destroy derivatives router
    
    * add test for PdfRequestSerializationHelper
    
    * remove DatastreamType pdf access copy, remove mimetype patterns from PdfDerivativeProcessor
    
    * rename to AggregatePdf, add init, throw ServiceException, rename methods, get original file absolute path in createInputListFile, update createTranscriptListFile, add file format filter, add AggregateFile Rdf type, update AggregatePdfProcessor workflow to store aggregate PDF as another file in work, update tests
    
    * inject ${services.tempDirectory} property, fix imports
    
    * don't call initializeTempImageFilesDir in constructor, use Files.write(), move access controls check, only load workObject once, fix test
    
    * add null check for transcript, delete all transcript files, use workPid.getId(), delete pdfTmpPath if exception thrown, initialize tmpFilesDir, defin character set, use pdfPid, fix test
    
    * fix javadoc, use pdfPid, use new ArrayList
    
    * fix tests
    
    * remove ObjectTypeMismatchException from generateAggregatePdf method

---
+++ operations-jms/src/main/java/edu/unc/lib/boxc/operations/jms/pdf/PdfRequestSerializationHelper.java
@@ -0,0 +1,44 @@
+package edu.unc.lib.boxc.operations.jms.pdf;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.io.IOException;
+
+/**
+ * Helper methods for serializing and deserializing pdf gen requests
+ * @author krwong
+ */
+public class PdfRequestSerializationHelper {
+    private static final ObjectWriter REQUEST_WRITER;
+    private static final ObjectReader REQUEST_READER;
+    static {
+        ObjectMapper mapper = new ObjectMapper();
+        REQUEST_WRITER = mapper.writerFor(PdfRequest.class);
+        REQUEST_READER = mapper.readerFor(PdfRequest.class);
+    }
+
+    private PdfRequestSerializationHelper() {
+    }
+
+    /**
+     * Transform request into a JSON string
+     * @param request pdf request
+     * @return json
+     * @throws IOException
+     */
+    public static String toJson(PdfRequest request) throws IOException {
+        return REQUEST_WRITER.writeValueAsString(request);
+    }
+
+    /**
+     * Transform JSON string to a PdfRequest
+     * @param json
+     * @return pdf request
+     * @throws IOException
+     */
+    public static PdfRequest toRequest(String json) throws IOException {
+        return REQUEST_READER.readValue(json);
+    }
+}
---
+++ operations/src/test/java/edu/unc/lib/boxc/operations/impl/pdf/AggregatePdfServiceTest.java
@@ -0,0 +1,228 @@
+package edu.unc.lib.boxc.operations.impl.pdf;
+
+import edu.unc.lib.boxc.auth.api.models.AgentPrincipals;
+import edu.unc.lib.boxc.auth.fcrepo.models.AccessGroupSetImpl;
+import edu.unc.lib.boxc.auth.fcrepo.models.AgentPrincipalsImpl;
+import edu.unc.lib.boxc.model.api.DatastreamType;
+import edu.unc.lib.boxc.model.api.ResourceType;
+import edu.unc.lib.boxc.model.api.objects.BinaryObject;
+import edu.unc.lib.boxc.model.api.objects.RepositoryObjectLoader;
+import edu.unc.lib.boxc.model.api.objects.WorkObject;
+import edu.unc.lib.boxc.model.fcrepo.ids.DatastreamPids;
+import edu.unc.lib.boxc.model.fcrepo.ids.PIDs;
+import edu.unc.lib.boxc.operations.jms.pdf.PdfRequest;
+import edu.unc.lib.boxc.search.api.models.ContentObjectRecord;
+import edu.unc.lib.boxc.search.solr.models.ContentObjectSolrRecord;
+import edu.unc.lib.boxc.search.solr.models.DatastreamImpl;
+import edu.unc.lib.boxc.search.solr.responses.SearchResultResponse;
+import edu.unc.lib.boxc.search.solr.services.SolrSearchService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import pdf4u.CLIMain;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
+
+public class AggregatePdfServiceTest {
+    private static final String PARENT_UUID = "f277bb38-272c-471c-a28a-9887a1328a1f";
+    private static final String CHILD1_UUID = "83c2d7f8-2e6b-4f0b-ab7e-7397969c0682";
+    private static final String CHILD2_UUID = "0e33ad0b-7a16-4bfa-b833-6126c262d889";
+    private static final String COLLECTION_UUID = "9cb6cc61-d88e-403e-b959-2396cd331a12";
+    private static final String ADMIN_UNIT_UUID = "5158b962-9e59-4ed8-b920-fc948213efd3";
+
+    @Mock
+    private RepositoryObjectLoader repositoryObjectLoader;
+    @Mock
+    private SolrSearchService solrSearchService;
+
+    private AgentPrincipals agent = new AgentPrincipalsImpl("user", new AccessGroupSetImpl("agroup"));
+    private AutoCloseable closeable;
+    private AggregatePdfService pdfService;
+
+    @BeforeEach
+    public void setup() {
+        closeable = openMocks(this);
+        pdfService = new AggregatePdfService();
+        pdfService.setRepositoryObjectLoader(repositoryObjectLoader);
+        pdfService.setSolrSearchService(solrSearchService);
+    }
+
+    @AfterEach
+    void closeService() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    public void generateAggregatePdfTest() throws Exception {
+        try (MockedStatic<CLIMain> mockedStatic = Mockito.mockStatic(CLIMain.class)) {
+            var parentRec = makeWorkRecord(PARENT_UUID, "Work");
+            var rec1 = makeRecord(CHILD1_UUID, PARENT_UUID, ResourceType.File, "File One",
+                    "file1.png", "image/png");
+            var rec2 = makeRecord(CHILD2_UUID, PARENT_UUID, ResourceType.File, "File Two",
+                    "file2.png", "image/png");
+
+            mockParentResults(parentRec);
+            mockChildrenResults(rec1, rec2);
+            mockOriginalFile(CHILD1_UUID, "file1.png");
+            mockOriginalFile(CHILD2_UUID, "file2.png");
+
+            var workObject = mock(WorkObject.class);
+            when(repositoryObjectLoader.getWorkObject(PIDs.get(PARENT_UUID))).thenReturn(workObject);
+
+            PdfRequest request = new PdfRequest();
+            request.setWorkPid(PARENT_UUID);
+            request.setMimetype("image/png");
+            request.setAgent(agent);
+
+            mockedStatic.when(() -> CLIMain.runCommand(any(String[].class))).thenReturn(0);
+
+            Path result = pdfService.generateAggregatePdf(request);
+
+            assertNotNull(result);
+            assertTrue(result.toString().endsWith(".pdf"));
+
+            mockedStatic.verify(() -> CLIMain.runCommand(argThat(command ->
+                    command != null
+                            && command.length == 10
+                            && "pdf4u".equals(command[0])
+                            && "add_ocr".equals(command[1])
+                            && "-i".equals(command[2])
+                            && "-o".equals(command[4])
+                            && "-t".equals(command[6])
+                            && "-tt".equals(command[8])
+                            && "HANDWRITTEN-PRINT".equals(command[9])
+            )));
+        }
+    }
+
+    @Test
+    public void createInputListFileTest() throws Exception {
+        var parentRec = makeWorkRecord(PARENT_UUID, "Work");
+        var rec1 = makeRecord(CHILD1_UUID, PARENT_UUID, ResourceType.File, "File One",
+                "file1.png", "image/png");
+        var rec2 = makeRecord(CHILD2_UUID, PARENT_UUID, ResourceType.File, "File Two",
+                "file2.png", "image/png");
+
+        mockParentResults(parentRec);
+        mockChildrenResults(rec1, rec2);
+        mockOriginalFile(CHILD1_UUID, "file1.png");
+        mockOriginalFile(CHILD2_UUID, "file2.png");
+
+        PdfRequest request = new PdfRequest();
+        request.setWorkPid(PARENT_UUID);
+        request.setMimetype("image/png");
+        request.setAgent(agent);
+
+        var inputFilePath = pdfService.createInputListFile(request);
+        List<String> lines = Files.readAllLines(inputFilePath, StandardCharsets.UTF_8);
+        assertEquals(2, lines.size());
+        assertEquals("file1.png", lines.get(0));
+        assertEquals("file2.png", lines.get(1));
+    }
+
+    @Test
+    public void createTranscriptListFileTest() throws Exception {
+        var parentRec = makeWorkRecord(PARENT_UUID, "Work");
+        var rec1 = makeRecord(CHILD1_UUID, PARENT_UUID, ResourceType.File, "File One",
+                "file1.png", "image/png");
+        var rec2 = makeRecord(CHILD2_UUID, PARENT_UUID, ResourceType.File, "File Two",
+                "file2.png", "image/png");
+
+        mockParentResults(parentRec);
+        mockChildrenResults(rec1, rec2);
+        mockOriginalFile(CHILD1_UUID, "file1.png");
+        mockOriginalFile(CHILD2_UUID, "file2.png");
+
+        PdfRequest request = new PdfRequest();
+        request.setWorkPid(PARENT_UUID);
+        request.setMimetype("image/png");
+        request.setAgent(agent);
+
+        var transcriptListFile = pdfService.createTranscriptListFile(request);
+        List<String> lines = Files.readAllLines(transcriptListFile, StandardCharsets.UTF_8);
+        assertEquals(2, lines.size());
+        assertTrue(lines.get(0).contains(CHILD1_UUID + "_transcript"));
+        assertTrue(lines.get(1).contains(CHILD2_UUID + "_transcript"));
+    }
+
+    @Test
+    public void getTextTypeTest() throws Exception {
+        // todo
+    }
+
+    public static SearchResultResponse makeResultResponse(ContentObjectRecord... results) {
+        var resp = new SearchResultResponse();
+        resp.setResultList(Arrays.asList(results));
+        resp.setResultCount(results.length);
+        return resp;
+    }
+
+    public static void mockSingleRecordResults(SolrSearchService solrSearchService, ContentObjectRecord parentRec, ContentObjectRecord... parentRecs) {
+        when(solrSearchService.getObjectById(any())).thenReturn(parentRec, parentRecs);
+    }
+
+    public static void mockSearchResults(SolrSearchService solrSearchService, ContentObjectRecord... results) {
+        when(solrSearchService.getSearchResults(any())).thenReturn(makeResultResponse(results));
+    }
+
+    private void mockChildrenResults(ContentObjectRecord... results) {
+        mockSearchResults(solrSearchService, results);
+    }
+
+    private void mockParentResults(ContentObjectRecord parentRec, ContentObjectRecord... parentRecs) {
+        mockSingleRecordResults(solrSearchService, parentRec, parentRecs);
+    }
+
+    private ContentObjectRecord makeWorkRecord(String uuid, String title) {
+        return makeRecord(uuid, COLLECTION_UUID, ResourceType.Work, title, null, "image/png");
+    }
+
+    private ContentObjectRecord makeRecord(String uuid, String parentUuid, ResourceType resourceType, String title,
+                                           String filename, String mimetype) {
+        var rec = new ContentObjectSolrRecord();
+        rec.setId(uuid);
+        rec.setAncestorPath(makeAncestorPath(parentUuid));
+        rec.setResourceType(resourceType.name());
+        rec.setTitle(title);
+        rec.setFileFormatType(Arrays.asList(mimetype));
+        rec.setTranscript("Transcript for " + title);
+
+        if (filename != null) {
+            var datastream = new DatastreamImpl(null, DatastreamType.ORIGINAL_FILE.getId(), 0l, mimetype,
+                    filename, null, null, null);
+            rec.setDatastream(Arrays.asList(datastream.toString()));
+        }
+        return rec;
+    }
+
+    private List<String> makeAncestorPath(String parentUuid) {
+        return Arrays.asList("1,collections", "2," + ADMIN_UNIT_UUID, "3," + COLLECTION_UUID, "4," + parentUuid);
+    }
+
+    private void mockOriginalFile(String childUuid, String contentUri) {
+        var childPid = PIDs.get(childUuid);
+        var originalFilePid = DatastreamPids.getOriginalFilePid(childPid);
+
+        var binaryObject = mock(BinaryObject.class);
+        when(binaryObject.getContentUri()).thenReturn(URI.create(contentUri));
+
+        when(repositoryObjectLoader.getBinaryObject(originalFilePid)).thenReturn(binaryObject);
+    }
+}
