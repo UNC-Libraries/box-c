7a0e23c4a2866456960dbd6a6cd9332229116a23
Dean Farrell <lfarrell@email.unc.edu>
2026-07-10 10:40:22 -0400

    Bxc 5715 run enhancements (#2185)
    * Update admin form to allow specific enhancements
    
    * Check that at least one target and one enhancement has been selected.
    
    * Fix issue with form not resetting after it's closed.

--- static/js/admin/src/action/RunEnhancementsBatchAction.js
+++ static/js/admin/src/action/RunEnhancementsBatchAction.js
@@ -3,29 +3,37 @@ define('RunEnhancementsBatchAction', [ 'jquery', 'AbstractBatchAction', "tpl!tem
 	function RunEnhancementsBatchAction(context) {
 		this._create(context);
 	};
-	
+
 	RunEnhancementsBatchAction.prototype.constructor = RunEnhancementsBatchAction;
-	RunEnhancementsBatchAction.prototype = Object.create( AbstractBatchAction.prototype );
-	
+	RunEnhancementsBatchAction.prototype = Object.create(AbstractBatchAction.prototype);
+
 	RunEnhancementsBatchAction.prototype.isValidTarget = function(target) {
 		return target.isSelected() && target.isEnabled() && $.inArray("reindex", target.metadata.permissions) != -1;
 	};
-	
-	RunEnhancementsBatchAction.prototype.getTargets = function(targets) {
+
+	RunEnhancementsBatchAction.prototype.getTargets = function() {
 		if (this.context.targets) {
 			return this.context.targets;
-		} 
+		}
 		return AbstractBatchAction.prototype.getTargets.call(this);
 	};
-	
+
+	RunEnhancementsBatchAction.prototype.getPids = function() {
+		var targetIdsString = this.$dialog.find("#run_enhancements_ids").val() || "";
+
+		return targetIdsString.split("\n").map(function(id) {
+			return id.trim();
+		}).filter(function(id) {
+			return id.length > 0;
+		});
+	};
+
 	RunEnhancementsBatchAction.prototype.execute = function() {
 		var self = this;
-		
-		var exportContainerMode = this.context.exportContainerMode;
-		
+
 		this.targets = this.getTargets();
 		var title;
-		if (this.targets.length == 1) {
+		if (this.targets.length === 1) {
 			title = "Run enhancements on " + this.targets[0].metadata.title.substring(0, 30);
 		} else {
 			title = "Run enhancements on " + this.targets.length + " objects";
@@ -35,44 +43,91 @@ define('RunEnhancementsBatchAction', [ 'jquery', 'AbstractBatchAction', "tpl!tem
 		for (var index in this.targets) {
 			targetIds += this.targets[index].getPid() + "\n";
 		}
-		var form = runEnhancementsTemplate({ targetIds : targetIds });
-		this.dialog = $("<div class='containingDialog'>" + form + "</div>");
-		this.dialog.dialog({
+		var form = runEnhancementsTemplate({ targetIds: targetIds });
+		this.$dialog = $("<div class='containingDialog'>" + form + "</div>");
+		this.$dialog.dialog({
 			autoOpen: true,
-			width: 'auto',
-			minWidth: '500',
+			width: "auto",
+			minWidth: "500",
 			modal: true,
-			title: title
+			title: title,
+			close: function() {
+				self.$dialog.remove();
+				self.$dialog = null;
+			}
 		});
-		this.$form = this.dialog.first();
-		
-		this.$form.submit(function(e){
-			var force = document.getElementById('run_enhancements_force').checked;
-			var recursive = document.getElementById('run_enhancements_recursive').checked;
-			var targetIdsString = document.getElementById('run_enhancements_ids').value;
 
-			var pids = targetIdsString.split("\n").map((id) => id.trim()).filter((id) => id.length > 0);
+		this.$dialog.find("form").first().off("submit").on("submit", function(e) {
+			var $form = self.$dialog.find("form").first();
+			var force = $form.find("#run_enhancements_force").prop("checked");
+			var recursive = $form.find("#run_enhancements_recursive").prop("checked");
+			// Collect all checked enhancement checkboxes.
+			// Split any comma-separated checkbox values into individual enhancement names.
+			// Convert it to a plain array and flatten the results.
+			var enhancements = [...$form.find('input[name="enhancements"]:checked')
+					.map(function() {
+						return this.value.split(",");
+					})
+					.get()
+					.flat()
+			];
+			var pids = self.getPids();
+
+			if (!self.validateForm(enhancements, pids, $form)) {
+				e.preventDefault();
+				return false;
+			}
 
 			$.ajax({
-				url : "/services/api/runEnhancements",
-				type : "POST",
+				url: "/services/api/runEnhancements",
+				type: "POST",
 				contentType: "application/json; charset=utf-8",
 				dataType: "json",
-				data : JSON.stringify({
-					force : force,
-					pids : pids,
-					recursive : recursive
+				data: JSON.stringify({
+					force: force,
+					pids: pids,
+					recursive: recursive,
+					enhancements: enhancements
 				})
 			}).done(function(response) {
 				self.context.view.$alertHandler.alertHandler("message", response.message);
-				self.dialog.dialog("destroy");
+				self.$dialog.dialog("close");
 			}).fail(function() {
 				self.context.view.$alertHandler.alertHandler("error", "Failed to run enhancements for " + self.targets.length + " objects");
 			});
-			
+
 			e.preventDefault();
 		});
-	}
-	
+	};
+
+	RunEnhancementsBatchAction.prototype.validateForm = function(enhancements, pids, $form) {
+		var $errors = $(".errors", $form);
+		var $errorStack = $(".error_stack", $form);
+		var errors = [];
+
+		$errors.hide();
+		$errorStack.empty();
+
+		if (!this.targets || this.targets.length === 0 || pids.length === 0) {
+			errors.push("Select at least one target to run enhancements on.");
+		}
+
+		if (enhancements.length === 0) {
+			errors.push("Select at least one enhancement to run.");
+		}
+
+		if (errors.length > 0) {
+			errors.forEach(function(error) {
+				$errorStack.append($("<div>").text(error));
+			});
+			$errors.show();
+			this.$dialog.dialog("option", "position", "center");
+
+			return false;
+		}
+
+		return true;
+	};
+
 	return RunEnhancementsBatchAction;
 });
