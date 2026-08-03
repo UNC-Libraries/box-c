954c5457cb596617817b507ac263ca42e2493308
lfarrell <lfarrell@email.unc.edu>
2026-08-03 11:06:25 -0400

    Basic UI for file events

---
+++ static/js/vue-cdr-access/src/components/full_record/fileHistory.vue
@@ -0,0 +1,70 @@
+<template>
+<div id="file-history">
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">View File History</p>
+            <button class="card-header-icon" aria-label="display file events" @click="toggleFileHistory">
+              <span class="icon">
+                <i class="fas" :class="toggleArrow" aria-hidden="true"></i>
+              </span>
+            </button>
+        </header>
+        <div class="card-content" :class="{'is-hidden': !showFilHistory}">
+            <div class="content">
+                <table class="table is-bordered is-striped is-fullwidth">
+                    <thead>
+                    <tr>
+                        <th scope="col">File Activity</th>
+                        <th scope="col">Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="event in fileEvents" :key="event.id">
+                        <td>{{ event.description }}</td>
+                        <td>{{ event.date }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'fileHistory',
+    
+    data() {
+        return {
+            showFilHistory: false,
+            fileEvents: [
+                { description: "New version added as file2.txt", date: Date.now() },
+                { description: "Added to rare books collection", date: Date.now() - 50000 },
+                { description: "Uploaded as file.txt", date: Date.now() - 100000 },
+            ]
+        }
+    },
+
+    computed: {
+        toggleArrow() {
+            return this.showFilHistory ? 'fa-angle-down' : 'fa-angle-right'
+        }
+    },
+    
+    methods: {
+        getFileEvents() {
+
+        },
+
+        toggleFileHistory() {
+            this.showFilHistory = !this.showFilHistory;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- static/js/vue-cdr-access/src/components/full_record/fileRecord.vue
+++ static/js/vue-cdr-access/src/components/full_record/fileRecord.vue
@@ -60,10 +60,13 @@
             <div class="container pb-5" v-if="recordData.viewerType">
                 <player :record-data="recordData"></player>
             </div>
-            <div class="container pb-6">
+            <div class="container pb-5">
                 <metadata-display :uuid="recordData.briefObject.id"
                               :can-view-metadata="hasPermission(recordData, 'viewMetadata')">
                 </metadata-display>
+            </div>
+            <div class="container pb-6">
+                <file-history></file-history>
             </div>
             <neighbor-list :current-record-id="recordData.briefObject.id" :neighbors="recordData.neighborList"></neighbor-list>
         </div>
@@ -78,11 +81,12 @@ import player from '@/components/full_record/player.vue';
 import metadataDisplay from '@/components/full_record/metadataDisplay.vue';
 import neighborList from '@/components/full_record/neighborList.vue';
 import objectActions from '@/components/full_record/objectActions.vue';
+import FileHistory from "@/components/full_record/fileHistory.vue";
 
 export default {
     name: 'fileRecord',
 
-    components: {abstract, metadataDisplay, neighborList, objectActions, player },
+    components: {FileHistory, abstract, metadataDisplay, neighborList, objectActions, player },
 
     mixins: [fileUtils, fullRecordUtils],
 
